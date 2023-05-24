7837bba73b27c7be19cfdf2dfe0a8246c6540847
JP Lorek <juan.lorek@globant.com>
2023-05-24 16:53:16 -0300

    refactor(extension): remove css class on sideMenuLabel component

--- apps/browser-extension-wallet/src/views/browser-view/components/SideMenu/SideMenuLabel.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/components/SideMenu/SideMenuLabel.tsx
@@ -8,6 +8,5 @@ interface SideMenuLabelProps {
 }
 
 export const SideMenuLabel: FC<SideMenuLabelProps> = ({ children, className }) => (
-  // to be able to target this element inside the VerticalNavigationBar.module.scss the concealable-menu-label class needs to be added
-  <p className={classnames(styles.text, className, 'concealable-menu-label')}>{children}</p>
+  <p className={classnames(styles.text, className)}>{children}</p>
 );
