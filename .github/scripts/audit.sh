68f19c028c7b1cbcd21af655d14d3566db968259
lgobbi-atix <leonel.gobbi@globant.com>
2023-05-30 10:30:35 -0300

    chore(all): update audit.sh severity level to high

--- .github/scripts/audit.sh
+++ .github/scripts/audit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn audit --groups dependencies --level critical; [[ $? -ge 16 ]] && exit 1 || exit 0;
+yarn audit --groups dependencies --level high; [[ $? -ge 8 ]] && exit 1 || exit 0;
