d30b03c93091ff9cbb762c869bb7157320c92dcd
wklos-iohk <wojciech.klos@iohk.io>
2023-06-01 11:51:21 +0200

    test(extension): add separate assertion for button on help drawer

--- packages/e2e-tests/src/assert/settings/drawerHelpSettingsAssert.ts
+++ packages/e2e-tests/src/assert/settings/drawerHelpSettingsAssert.ts
@@ -3,6 +3,15 @@ import { t } from '../../utils/translationService';
 import { expect } from 'chai';
 
 class DrawerHelpSettingsAssert {
+  async assertSeeCreateASupportTicketButton(shouldBeDisplayed: boolean) {
+    await HelpSettingsDrawer.createASupportTicketButton.waitForDisplayed({ reverse: !shouldBeDisplayed });
+    if (shouldBeDisplayed) {
+      await expect(await HelpSettingsDrawer.createASupportTicketButton.getText()).to.equal(
+        await t('browserView.settings.help.support.createASupportTicket')
+      );
+    }
+  }
+
   async assertSeeHelpDrawer(mode: 'extended' | 'popup') {
     await HelpSettingsDrawer.drawerHeaderTitle.waitForDisplayed();
     await expect(await HelpSettingsDrawer.drawerHeaderTitle.getText()).to.equal(
@@ -16,10 +25,7 @@ class DrawerHelpSettingsAssert {
     await expect(await HelpSettingsDrawer.helpZendeskTitle.getText()).to.equal(
       await t('browserView.settings.help.support.iogZenDesk')
     );
-    await HelpSettingsDrawer.createASupportTicketButton.waitForDisplayed();
-    await expect(await HelpSettingsDrawer.createASupportTicketButton.getText()).to.equal(
-      await t('browserView.settings.help.support.createASupportTicket')
-    );
+    await this.assertSeeCreateASupportTicketButton(true);
 
     const isExtended = mode === 'extended';
     await HelpSettingsDrawer.drawerNavigationTitle.waitForDisplayed({ reverse: !isExtended });
