f78d40159790a06bd21f743f36b08e9ee2087548
JP Lorek <juan.lorek@globant.com>
2023-06-02 10:39:16 -0300

    refactor(extension): improve variables naming

--- apps/browser-extension-wallet/src/features/address-book/components/AddressDetailDrawer/types/index.ts
+++ apps/browser-extension-wallet/src/features/address-book/components/AddressDetailDrawer/types/index.ts
@@ -8,6 +8,8 @@ export interface AddressDetailsSectionConfig {
   currentSection: AddressDetailsSteps;
   nextSection?: AddressDetailsSteps;
   prevSection?: AddressDetailsSteps;
+  headerTitle?: string;
+  headerSubtitle?: string;
 }
 
 export type AddressDetailsConfig = Partial<Record<AddressDetailsSteps, AddressDetailsSectionConfig>>;
