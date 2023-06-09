fcfe7ab2cf5d30c8ec6174becddd30a2a5b329fe
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-06-10 00:20:19 +0200

    chore(staking): setup ladle to mimic lace base styling

--- packages/staking/.ladle/components.tsx
+++ packages/staking/.ladle/components.tsx
@@ -1,7 +1,16 @@
-import { GlobalProvider } from '@ladle/react';
+import { GlobalProvider, ThemeState } from '@ladle/react';
+import { ThemeColorScheme, ThemeProvider } from '@lace/ui';
 import React from 'react';
 import 'antd/dist/antd.css';
 import 'normalize.css';
 import '../../../apps/browser-extension-wallet/src/styles/index.scss';
 
-export const Provider: GlobalProvider = ({ children }) => <div id={'lace-app'}>{children}</div>;
+export const Provider: GlobalProvider = ({ children, globalState }) => {
+  return (
+    <ThemeProvider
+      colorScheme={globalState.theme === ThemeState.Light ? ThemeColorScheme.Light : ThemeColorScheme.Dark}
+    >
+      <div id={'lace-app'}>{children}</div>
+    </ThemeProvider>
+  );
+};
