5d282e61ab0f5ed8be46b62535678d798f2dfd2c
januszjanus <janusz.janus@iohk.io>
2023-06-16 16:06:21 +0200

    test(extension): conflicts resolve 2

--- packages/core/src/ui/components/AssetTable/AssetTable.tsx
+++ packages/core/src/ui/components/AssetTable/AssetTable.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable unicorn/no-nested-ternary */
 import classnames from 'classnames';
-import { InfiniteScrollableTable } from '@lace/common';
+import { InfiniteScrollableTable, useHasScrollBar } from '@lace/common';
 import { ColumnsType } from 'antd/lib/table';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './AssetTable.module.scss';
 import { useTranslate } from '@src/ui/hooks/useTranslate';
 
@@ -25,18 +25,27 @@ interface IAssetColumn {
 }
 
 const renderCell = (
-  { src, title, subtitle }: { src?: string; title: string; subtitle: string },
+  {
+    src,
+    title,
+    subtitle,
+    testIdForTitle,
+    testIdForSubtitle
+  }: { src?: string; title: string; subtitle: string; testIdForTitle?: string; testIdForSubtitle?: string },
   subtitleColor?: 'red' | 'green' | 'neutral',
   popupView?: boolean
 ) => (
   <div data-testid="asset-table-cell" className={styles.cellContainer}>
     {src && <img data-testid="asset-table-cell-logo" src={src} alt="" className={styles.image} />}
     <div>
-      <p data-testid="asset-table-cell-title" className={classnames(styles.title, popupView && styles.cellTitlePopup)}>
+      <p
+        data-testid={testIdForTitle ? `token-table-cell-${testIdForTitle}` : 'asset-table-cell-title'}
+        className={classnames(styles.title, popupView && styles.cellTitlePopup)}
+      >
         {title}
       </p>
       <p
-        data-testid="asset-table-cell-subtitle"
+        data-testid={testIdForSubtitle ? `token-table-cell-${testIdForSubtitle}` : 'asset-table-cell-subtitle'}
         className={classnames(
           styles.subtitle,
           {
@@ -56,16 +65,37 @@ const renderCell = (
 const renderRows = (rows: IRow[], popupView: boolean): IAssetColumn[] =>
   rows.map((row) => ({
     key: row.id,
-    token: renderCell({ src: row.logo, title: row.name, subtitle: row.ticker || '-' }, undefined, popupView),
+    token: renderCell(
+      {
+        src: row.logo,
+        title: row.name,
+        subtitle: row.ticker || '-',
+        testIdForTitle: 'name',
+        testIdForSubtitle: 'ticker'
+      },
+      undefined,
+      popupView
+    ),
     price: renderCell(
       {
         title: row.price,
-        subtitle: row.variation
+        subtitle: row.variation,
+        testIdForTitle: 'price',
+        testIdForSubtitle: 'price-variation'
       },
       row.variation === '0' || row.variation === '-' ? 'neutral' : row.variation.includes('+') ? 'green' : 'red',
       popupView
     ),
-    balance: renderCell({ title: row.balance, subtitle: row.fiatBalance }, undefined, popupView)
+    balance: renderCell(
+      {
+        title: row.balance,
+        subtitle: row.fiatBalance,
+        testIdForTitle: 'balance',
+        testIdForSubtitle: 'fiat-balance'
+      },
+      undefined,
+      popupView
+    )
   }));
 
 export type AssetTableProps = {
@@ -107,14 +137,19 @@ export const AssetTable = ({
       key: 'price'
     });
 
+  const [hasScrollBar, setHasScrollBar] = useState<boolean>(false);
+  useHasScrollBar({ current: document.querySelector(`#${scrollableTargetId}`) }, (withScroll) =>
+    setHasScrollBar(withScroll && popupView)
+  );
+
   return (
     <InfiniteScrollableTable
       data-testid="asset-table"
       columns={columns}
-      className={popupView && styles.negativeMarginTable}
       dataSource={renderRows(rows, popupView)}
       onRow={onRowClick ? handleRowClick : undefined}
-      infitineScrollProps={{
+      infiniteScrollContainerClass={hasScrollBar && styles.scrollContainer}
+      infiniteScrollProps={{
         dataLength: rows?.length || 0,
         next: onLoad,
         hasMore: rows?.length < (totalItems || 0),
