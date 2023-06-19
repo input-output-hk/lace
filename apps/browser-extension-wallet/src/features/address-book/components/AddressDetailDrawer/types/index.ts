1e960599013150c854c135cdb0f0ae0eabc1cb08
jplorek-atix <65184652+jplorek-atix@users.noreply.github.com>
2023-06-19 13:57:46 -0300

    Implement add new address form in side drawer (#57)
    * feat(extension): move address book new address form to side drawer
    
    * refactor(extension): remove unnecessary check on address detail drawing
    
    * feat(extension): add enum to handle initial step in address drawer
    
    * refactor(extension): improve variables naming
    
    * chore(extension): refactor address book button handling
    
    * chore(extension): remove address book sub headline
    
    * fix(extension): fix edit address enter key action
    
    ---------
    
    Co-authored-by: Dominik Guzei <dominik.guzei@gmail.com>

--- apps/browser-extension-wallet/src/features/address-book/components/AddressDetailDrawer/types/index.ts
+++ apps/browser-extension-wallet/src/features/address-book/components/AddressDetailDrawer/types/index.ts
@@ -1,12 +1,15 @@
 export enum AddressDetailsSteps {
-  DETAIL = 'detail',
-  FORM = 'form'
+  DETAILS,
+  CREATE,
+  EDIT
 }
 
 export interface AddressDetailsSectionConfig {
   currentSection: AddressDetailsSteps;
   nextSection?: AddressDetailsSteps;
   prevSection?: AddressDetailsSteps;
+  headerTitle?: string;
+  headerSubtitle?: string;
 }
 
 export type AddressDetailsConfig = Partial<Record<AddressDetailsSteps, AddressDetailsSectionConfig>>;
