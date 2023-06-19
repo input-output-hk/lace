4ed24fce6d4a3a48107a17e1bd970ac4985db88c
Renan Ferreira <renan.ferreira@iohk.io>
2023-06-19 17:57:08 -0300

    feat(ui): create <SearchBox /> component

---
+++ packages/ui/src/design-system/search-box/search-box-clear-button.component.tsx
@@ -0,0 +1,39 @@
+import React, { forwardRef } from 'react';
+import type { MouseEvent, Ref } from 'react';
+
+import classNames from 'classnames';
+
+import { ReactComponent as CloseIcon } from '../../assets/icons/close.component.svg';
+
+import * as cx from './search-box-clear-button.css';
+
+import type { OmitClassName } from '../../types';
+
+type Props = Omit<OmitClassName<'button'>, 'onChange'> & {
+  onClick?: (event: Readonly<MouseEvent<HTMLButtonElement>>) => void;
+  id?: string;
+  disabled?: boolean;
+  active?: boolean;
+};
+
+const PureClearButton = (
+  { onClick, id, disabled, active, ...props }: Readonly<Props>,
+  ref: Ref<HTMLButtonElement>,
+): JSX.Element => (
+  <div className={cx.container}>
+    <button
+      {...props}
+      onClick={onClick}
+      className={classNames(cx.button, {
+        [cx.active]: active,
+      })}
+      id={id}
+      disabled={disabled}
+      ref={ref}
+    >
+      <CloseIcon />
+    </button>
+  </div>
+);
+
+export const ClearButton = forwardRef(PureClearButton);
