16bc6983aaed64397955e070ff3956ee51cbc93f
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-06-19 23:55:17 +0200

    chore(staking): ix ladle and tsup builds regarding the vanila exract

--- packages/staking/tsup.config.ts
+++ packages/staking/tsup.config.ts
@@ -9,8 +9,11 @@ const tsupConfig = defineConfig([
     clean: true,
     dts: true,
     entry: ['./src/index.ts'],
-    // eslint-disable-next-line new-cap
-    esbuildPlugins: [ScssModulesPlugin() as never, vanillaExtractPlugin()],
+    esbuildPlugins: [
+      // eslint-disable-next-line new-cap
+      ScssModulesPlugin() as never,
+      vanillaExtractPlugin({ esbuildOptions: { loader: { '.css': 'empty' } } }),
+    ],
     external: Object.keys(peerDependencies),
     format: ['esm', 'cjs'],
     name: 'lace/staking',
