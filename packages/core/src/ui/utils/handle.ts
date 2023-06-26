a8604ce60a28a489fdc6f5d23fadadf5298ecc3d
shawnbusuttil <shawn-buzu@hotmail.co.uk>
2023-06-26 20:25:24 +0100

    feat(core): bind feature flag for editing addresses

--- packages/core/src/ui/utils/handle.ts
+++ packages/core/src/ui/utils/handle.ts
@@ -2,4 +2,7 @@ const ADA_HANDLE_PREFIX = '$';
 const ADA_HANDLE_THRESHOLD = 2;
 
 export const isHandle = (value: string): boolean =>
-  value && value.charAt(0) === ADA_HANDLE_PREFIX && value.length > ADA_HANDLE_THRESHOLD;
+  process.env.USE_ADA_HANDLE === 'true' &&
+  value &&
+  value.charAt(0) === ADA_HANDLE_PREFIX &&
+  value.length > ADA_HANDLE_THRESHOLD;
