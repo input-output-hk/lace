9861e1dbaff99c633f37c829c8d1ad73a83cc07e
wklos-iohk <wojciech.klos@iohk.io>
2023-06-27 12:29:10 +0200

    test(extension): add new POM for Show Public Key drawer

--- packages/e2e-tests/src/assert/settings/publicKeyDrawerAssert.ts
+++ packages/e2e-tests/src/assert/settings/publicKeyDrawerAssert.ts
@@ -1,47 +1,47 @@
 import { WalletConfig } from '../../support/walletConfiguration';
 import { t } from '../../utils/translationService';
 import { expect } from 'chai';
-import WalletAddressPage from '../../elements/walletAddressPage';
+import ShowPublicKeyDrawer from '../../elements/settings/ShowPublicKeyDrawer';
 
 class WalletPublicKeyDrawerAssert {
   async assertSeePublicKey(wallet: WalletConfig) {
-    await WalletAddressPage.walletAddress.waitForDisplayed();
-    await expect(await WalletAddressPage.walletAddress.getText()).to.equal(wallet.publicKey);
+    await ShowPublicKeyDrawer.walletAddress.waitForDisplayed();
+    await expect(await ShowPublicKeyDrawer.walletAddress.getText()).to.equal(wallet.publicKey);
   }
 
   async assertSeeQrCode() {
-    await WalletAddressPage.qrCode.waitForDisplayed();
+    await ShowPublicKeyDrawer.qrCode.waitForDisplayed();
   }
 
   async assertSeeCopyButton(shouldSee: boolean) {
-    await WalletAddressPage.copyButton.waitForDisplayed({ reverse: !shouldSee });
+    await ShowPublicKeyDrawer.copyButton.waitForDisplayed({ reverse: !shouldSee });
     if (shouldSee) {
-      await expect(await WalletAddressPage.copyButton.getText()).to.equal(await t('general.button.copy'));
+      await expect(await ShowPublicKeyDrawer.copyButton.getText()).to.equal(await t('general.button.copy'));
     }
   }
 
   async assertSeePublicKeyPage(mode: 'extended' | 'popup') {
-    await WalletAddressPage.drawerHeaderTitle.waitForDisplayed();
-    await expect(await WalletAddressPage.drawerHeaderTitle.getText()).to.equal(
+    await ShowPublicKeyDrawer.drawerHeaderTitle.waitForDisplayed();
+    await expect(await ShowPublicKeyDrawer.drawerHeaderTitle.getText()).to.equal(
       await t('browserView.settings.wallet.general.showPubKey')
     );
-    await WalletAddressPage.drawerHeaderBackButton.waitForDisplayed();
+    await ShowPublicKeyDrawer.drawerHeaderBackButton.waitForDisplayed();
     if (mode === 'extended') {
-      await WalletAddressPage.drawerNavigationTitle.waitForDisplayed();
-      await expect(await WalletAddressPage.drawerNavigationTitle.getText()).to.equal(
+      await ShowPublicKeyDrawer.drawerNavigationTitle.waitForDisplayed();
+      await expect(await ShowPublicKeyDrawer.drawerNavigationTitle.getText()).to.equal(
         await t('browserView.settings.heading')
       );
-      await WalletAddressPage.drawerHeaderCloseButton.waitForDisplayed();
+      await ShowPublicKeyDrawer.drawerHeaderCloseButton.waitForDisplayed();
     }
     await this.assertSeeQrCode();
-    await WalletAddressPage.walletName.waitForDisplayed();
-    await WalletAddressPage.walletAddress.waitForDisplayed();
+    await ShowPublicKeyDrawer.walletName.waitForDisplayed();
+    await ShowPublicKeyDrawer.walletAddress.waitForDisplayed();
     await this.assertSeeCopyButton(true);
   }
 
   async assertSeeWalletName(wallet: WalletConfig) {
-    await WalletAddressPage.walletName.waitForDisplayed();
-    await expect(await WalletAddressPage.walletName.getText()).to.equal(wallet.name);
+    await ShowPublicKeyDrawer.walletName.waitForDisplayed();
+    await expect(await ShowPublicKeyDrawer.walletName.getText()).to.equal(wallet.name);
   }
 }
 
