6f3cb083a5e26321d057d5ed2b911a46717886c9
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2023-06-29 18:05:41 +0200

    feat(staking): add search and sort for stake pools browser

--- packages/staking/src/features/browse-pools/search/Search.tsx
+++ packages/staking/src/features/browse-pools/search/Search.tsx
@@ -1,13 +1,20 @@
-import { Search as SearchBase } from '@lace/common';
+import { Search as SearchBase, SearchProps as SearchBaseProps } from '@lace/common';
+import { useTranslation } from 'react-i18next';
 import * as styles from './Search.module.scss';
 
-export const Search = () => (
-  <SearchBase
-    className={styles.search}
-    withSearchIcon
-    inputPlaceholder={'Search by type, token name or ID'}
-    onChange={() => console.info('on change')}
-    data-testid="search-input"
-    loading={false}
-  />
-);
+type SearchProps = Pick<SearchBaseProps, 'onChange' | 'loading'>;
+
+export const Search = ({ onChange, loading }: SearchProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <SearchBase
+      className={styles.search}
+      withSearchIcon
+      inputPlaceholder={t('browsePools.stakePoolTableBrowser.searchInputPlaceholder')}
+      onChange={onChange}
+      data-testid="search-input"
+      loading={loading}
+    />
+  );
+};
