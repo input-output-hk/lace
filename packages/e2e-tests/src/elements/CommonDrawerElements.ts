750133c6a104dc44f471dddf7627fc40aed69d4b
wklos-iohk <wojciech.klos@iohk.io>
2023-06-29 11:06:06 +0200

    test(extension): refactor NFT details drawer POM

--- packages/e2e-tests/src/elements/CommonDrawerElements.ts
+++ packages/e2e-tests/src/elements/CommonDrawerElements.ts
@@ -2,7 +2,7 @@
 import { ChainablePromiseElement } from 'webdriverio';
 
 class CommonDrawerElements {
-  private DRAWER_BODY = '#asset-drawer-body';
+  private DRAWER_BODY = '.ant-drawer-body';
   private DRAWER_NAVIGATION_TITLE = '[data-testid="drawer-navigation-title"]';
   private DRAWER_HEADER_BACK_BUTTON = '[data-testid="navigation-button-arrow"]';
   private DRAWER_HEADER_CLOSE_BUTTON = '[data-testid="navigation-button-cross"]';
