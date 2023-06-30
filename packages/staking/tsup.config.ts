40aeb0e5356ad7990226aad99589443083795a5d
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2023-06-30 15:46:38 +0200

    feat(staking): enable using svg as React components (svgr)

--- packages/staking/tsup.config.ts
+++ packages/staking/tsup.config.ts
@@ -1,3 +1,4 @@
+import svgrJsx from '@svgr/plugin-jsx';
 import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
 import svgr from 'esbuild-plugin-svgr';
 import { ScssModulesPlugin } from 'esbuild-scss-modules-plugin';
@@ -16,7 +17,7 @@ const tsupConfig = defineConfig([
       // eslint-disable-next-line new-cap
       ScssModulesPlugin() as never,
       vanillaExtractPlugin({ esbuildOptions: { loader: { '.css': 'empty' } } }),
-      svgr(),
+      svgr({ icon: true, plugins: [svgrJsx] }),
     ],
     external: Object.keys(peerDependencies),
     format: ['esm', 'cjs'],
