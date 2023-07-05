c2e4816d982b5adaffce491613cd959afa728933
Daniel Main <daniel.main.cernhoff@icloud.com>
2023-07-05 10:02:06 +0200

    chore(extension): updated release notes

--- apps/browser-extension-wallet/src/release-notes/1_3_0.tsx
+++ apps/browser-extension-wallet/src/release-notes/1_3_0.tsx
@@ -6,13 +6,12 @@ const ReleaseNote_1_3_0 = (): React.ReactElement => (
     <p>Introducing Lace 1.3.0 This version supports the following features:</p>
     <ul style={{ listStyleType: 'disc', margin: 0 }}>
       <li>ADA handle support</li>
-      <li>See and spend tokens from other addresses</li>
-      <li>Sign CIP30 transactions with Ledger Hardware Wallet</li>
-      <li>Responsive layout improvements - now supporting window width of 668px or more</li>
+      <li>See and spend tokens from all your addresses when recovering a multi-address wallet</li>
+      <li>Responsive layout improvements</li>
     </ul>
     <p>
       Check out the details in the{' '}
-      <a href="https://www.lace.io/blog/join-us-on-a-tour-of-lace-1-3" target="_blank">
+      <a href="https://www.lace.io/blog/lace-1-3-release" target="_blank">
         blog
       </a>
     </p>
