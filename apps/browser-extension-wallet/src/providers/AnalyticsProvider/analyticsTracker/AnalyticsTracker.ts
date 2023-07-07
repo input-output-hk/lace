79646e952cdca30510bc8e17112f5f44f63b60f3
Dominik Guzei <dominik.guzei@gmail.com>
2023-07-07 14:42:04 +0200

    feat(extension): fix analytics disabled mode

--- apps/browser-extension-wallet/src/providers/AnalyticsProvider/analyticsTracker/AnalyticsTracker.ts
+++ apps/browser-extension-wallet/src/providers/AnalyticsProvider/analyticsTracker/AnalyticsTracker.ts
@@ -8,19 +8,18 @@ import { UserIdService } from '@lib/scripts/types';
 export class AnalyticsTracker {
   protected matomoClient?: MatomoClient;
   protected postHogClient?: PostHogClient;
-  protected userIdService: UserIdService;
+  protected userIdService?: UserIdService;
 
   constructor(
     chain: Wallet.Cardano.ChainId,
     analyticsDisabled: boolean,
     enhancedAnalyticsOptInStatus?: EnhancedAnalyticsOptInStatus
   ) {
+    if (analyticsDisabled) return;
     this.userIdService = getUserIdService();
+    this.matomoClient = new MatomoClient(chain, this.userIdService);
 
-    if (!analyticsDisabled) {
-      this.matomoClient = new MatomoClient(chain, this.userIdService);
-    }
-    if (!analyticsDisabled && POSTHOG_ENABLED) {
+    if (POSTHOG_ENABLED) {
       this.postHogClient = new PostHogClient(chain, this.userIdService, enhancedAnalyticsOptInStatus);
     }
   }
@@ -30,9 +29,9 @@ export class AnalyticsTracker {
 
     // eslint-disable-next-line unicorn/prefer-ternary
     if (status === EnhancedAnalyticsOptInStatus.OptedIn) {
-      await this.userIdService.makePersistent();
+      await this.userIdService?.makePersistent();
     } else {
-      await this.userIdService.makeTemporary();
+      await this.userIdService?.makeTemporary();
     }
   }
 
@@ -46,12 +45,12 @@ export class AnalyticsTracker {
   // TODO: rename to sendEventToMatomo  (https://input-output.atlassian.net/browse/LW-7197)
   async sendEvent(props: SendEventProps): Promise<void> {
     await this.matomoClient?.sendEvent(props);
-    await this.userIdService.extendLifespan();
+    await this.userIdService?.extendLifespan();
   }
 
   async sendEventToPostHog(action: PostHogAction, properties: Record<string, string | boolean> = {}): Promise<void> {
     await this.postHogClient?.sendEvent(action, properties);
-    await this.userIdService.extendLifespan();
+    await this.userIdService?.extendLifespan();
   }
 
   setChain(chain: Wallet.Cardano.ChainId): void {
