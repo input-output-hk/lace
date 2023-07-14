1419f6afb4b362635783d34c7181932ead1e827c
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2023-07-14 08:13:20 +0100

    fixup! feat: adds changes to allow user to update a contact from handle

--- packages/core/src/ui/utils/handle.ts
+++ packages/core/src/ui/utils/handle.ts
@@ -3,7 +3,4 @@ const ADA_HANDLE_THRESHOLD = 2;
 
 export const HANDLE_DEBOUNCE_TIME = 1000;
 export const isHandle = (value: string): boolean =>
-  process.env.USE_ADA_HANDLE === 'true' &&
-  value &&
-  value.charAt(0) === ADA_HANDLE_PREFIX &&
-  value.length > ADA_HANDLE_THRESHOLD;
+  value && value.charAt(0) === ADA_HANDLE_PREFIX && value.length > ADA_HANDLE_THRESHOLD;
