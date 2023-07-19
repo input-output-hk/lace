3b9dbea7482b53d67f289e9b2914ac04fca3c1cd
Tomek Marciniak <tmarciniakm@gmail.com>
2023-07-19 13:58:35 +0200

    fix(staking): adjust styles to use tokens

--- packages/staking/src/features/drawer/StakePoolPreferences.css.ts
+++ packages/staking/src/features/drawer/StakePoolPreferences.css.ts
@@ -7,14 +7,12 @@ export const PoolCard = style([
   { flex: 1 },
 ]);
 
-export const PoolHr = style({
-  backgroundColor: 'var(--light-mode-light-grey-plus, var(--dark-mode-mid-grey, #333333))',
-  height: '1px',
-  width: '100%',
-});
+export const PoolHr = style([
+  sx({ height: '$1' }),
+  {
+    backgroundColor: 'var(--light-mode-light-grey-plus, var(--dark-mode-mid-grey, #333333))',
+    width: '100%',
+  },
+]);
 
-export const PoolIndicator = style({
-  borderRadius: '0.25rem',
-  height: '2.5rem',
-  width: '0.25rem',
-});
+export const PoolIndicator = sx({ borderRadius: '$tiny', height: '$40', width: '$4' });
