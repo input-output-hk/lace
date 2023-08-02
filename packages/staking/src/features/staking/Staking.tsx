b5facb3d3f8b4c87d3bd969058319ca2654dd180
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2023-08-02 17:19:35 +0200

    feat(staking): add multidelegation beta banner/modal

--- packages/staking/src/features/staking/Staking.tsx
+++ packages/staking/src/features/staking/Staking.tsx
@@ -1,9 +1,16 @@
+import { MultidelegationBetaModal } from '../modals';
+import { useOutsideHandles } from '../outside-handles-provider';
 import { Setup } from './setup';
 import { StakingView } from './StakingView';
 import { StakingProps } from './types';
 
-export const Staking = ({ language, theme }: StakingProps) => (
-  <Setup theme={theme} language={language}>
-    <StakingView />
-  </Setup>
-);
+export const Staking = ({ language, theme }: StakingProps) => {
+  const { multidelegationFirstVisit, triggerMultidelegationFirstVisit } = useOutsideHandles();
+
+  return (
+    <Setup theme={theme} language={language}>
+      <StakingView />
+      <MultidelegationBetaModal visible={multidelegationFirstVisit} onConfirm={triggerMultidelegationFirstVisit} />
+    </Setup>
+  );
+};
--- packages/staking/src/features/theme/colors.ts
+++ packages/staking/src/features/theme/colors.ts
@@ -1,16 +1,22 @@
-import { darkColorScheme, lightColorScheme } from '@lace/ui';
+import { darkColorScheme, laceGradient, lightColorScheme } from '@lace/ui';
 
 export const colorsContract = {
   $delegationCardInfoLabelColor: '',
   $delegationCardInfoValueColor: '',
+  $laceGradient: '',
+  $primaryWhite: '',
 };
 
 export const lightThemeColors: typeof colorsContract = {
   $delegationCardInfoLabelColor: lightColorScheme.$primary_dark_grey,
   $delegationCardInfoValueColor: lightColorScheme.$primary_black,
+  $laceGradient: laceGradient,
+  $primaryWhite: lightColorScheme.$primary_white,
 };
 
 export const darkThemeColors: typeof colorsContract = {
   $delegationCardInfoLabelColor: darkColorScheme.$primary_light_grey,
   $delegationCardInfoValueColor: darkColorScheme.$primary_white,
+  $laceGradient: laceGradient,
+  $primaryWhite: lightColorScheme.$primary_white,
 };
