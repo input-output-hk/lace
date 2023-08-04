7371ac29c530dd81d0ce4d024c456c5872fc1c00
JP Lorek <juan.lorek@globant.com>
2023-08-04 13:55:32 -0300

    fix(extension): move userIdServiceProperties to config file

--- apps/browser-extension-wallet/src/providers/AnalyticsProvider/getUserIdService.ts
+++ apps/browser-extension-wallet/src/providers/AnalyticsProvider/getUserIdService.ts
@@ -1,6 +1,6 @@
 import { consumeRemoteApi } from '@cardano-sdk/web-extension';
+import { userIdServiceProperties } from '@lib/scripts/background/config';
 import { USER_ID_SERVICE_BASE_CHANNEL, UserIdService } from '@lib/scripts/types';
-import { userIdServiceProperties } from '@lib/scripts/background/services';
 import { runtime } from 'webextension-polyfill';
 
 export const getUserIdService = (): UserIdService =>
