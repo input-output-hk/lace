151daead46c21c08d762e9e3c2e3929c63564d9d
Daniel Main <daniel.main.cernhoff@icloud.com>
2023-08-08 16:55:32 +0200

    chore(extension): LW-7769 bump to 1.4.0 (#371)

---
+++ apps/browser-extension-wallet/src/release-notes/1_4_0.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_4_0 = (): React.ReactElement => (
+  <>
+    <p>Introducing Lace 1.4.0 This version supports the following features:</p>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li />
+    </ul>
+    <p>
+      Check out the details in the{' '}
+      <a href="https://www.lace.io/blog/lace-1-4-release" target="_blank">
+        blog
+      </a>
+    </p>
+  </>
+);
+
+export default ReleaseNote_1_4_0;
