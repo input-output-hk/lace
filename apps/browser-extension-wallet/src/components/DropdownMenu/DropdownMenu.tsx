3020ba47640b08fa7bf16c11df7239f3e6bde699
shawnbusuttil <shawn-buzu@hotmail.co.uk>
2023-08-09 12:58:13 +0100

    refactor(extension): change menu to items in user dropdown

--- apps/browser-extension-wallet/src/components/DropdownMenu/DropdownMenu.tsx
+++ apps/browser-extension-wallet/src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import cn from 'classnames';
 import { Dropdown } from 'antd';
 import { Button } from '@lace/common';
-import { DropdownMenuOverlay } from '../MainMenu';
 
 import ChevronNormal from '../../assets/icons/chevron-down.component.svg';
 import ChevronSmall from '../../assets/icons/chevron-down-small.component.svg';
 import styles from './DropdownMenu.module.scss';
 import { useWalletStore } from '@src/stores';
-import { UserAvatar } from '../MainMenu/DropdownMenuOverlay/components';
+import {
+  AddressBookLink,
+  LockWallet,
+  NetworkChoise,
+  NetworkInfo,
+  SettingsLink,
+  ThemeSwitcher,
+  UserAvatar,
+  UserInfo
+} from '../MainMenu/DropdownMenuOverlay/components';
+import { Sections } from '@components/MainMenu/DropdownMenuOverlay/types';
+import { ItemType, MenuItemType } from 'antd/lib/menu/hooks/useItems';
+import menuStyles from '../MainMenu/DropdownMenuOverlay/DropdownMenuOverlay.module.scss';
 
 export interface DropdownMenuProps {
   isPopup?: boolean;
+  lockWalletButton?: ReactNode;
+  topSection?: MenuItemType;
 }
 
-export const DropdownMenu = ({ isPopup }: DropdownMenuProps): React.ReactElement => {
+export const DropdownMenu = ({
+  isPopup,
+  lockWalletButton = <LockWallet />,
+  topSection = { key: 'user-info', label: <UserInfo /> }
+}: DropdownMenuProps): React.ReactElement => {
   const { walletInfo } = useWalletStore();
   const [open, setOpen] = useState(false);
+  const [currentSection, setCurrentSection] = useState<Sections>(Sections.Main);
   const Chevron = isPopup ? ChevronSmall : ChevronNormal;
+
+  const items: ItemType[] =
+    currentSection === Sections.Main
+      ? [
+          topSection,
+          { key: 'address-book', label: <AddressBookLink isPopup={isPopup} /> },
+          { key: 'settings', label: <SettingsLink /> },
+          { type: 'divider' },
+          { key: 'theme-switcher', label: <ThemeSwitcher isPopup={isPopup} /> },
+          { key: 'network', label: <NetworkChoise onClick={() => setCurrentSection(Sections.NetworkInfo)} /> },
+          { type: 'divider' },
+          lockWalletButton && { key: 'lock-wallet', label: lockWalletButton }
+        ]
+      : [{ key: 'network-info', label: <NetworkInfo onBack={() => setCurrentSection(Sections.Main)} /> }];
 
   return (
     <Dropdown
       destroyPopupOnHide
-      onVisibleChange={setOpen}
-      overlay={<DropdownMenuOverlay isPopup={isPopup} />}
+      onOpenChange={setOpen}
+      menu={{ items, className: menuStyles.menuOverlay }}
       placement="bottomRight"
       trigger={['click']}
     >
