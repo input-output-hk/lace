d8e41ae81a3e894dca936fbe8ea26b501f2cd2f6
JP Lorek <juan.lorek@globant.com>
2023-08-14 12:36:56 -0300

    test(extension): add test for analytics getTokensProperty function

--- apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/TransactionSuccessView.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/TransactionSuccessView.tsx
@@ -7,65 +7,14 @@ import {
   useBuiltTxState,
   useAnalyticsSendFlowTriggerPoint,
   useOutputs,
-  OutputList
+  TokenAnalyticsProperties
 } from '@views/browser/features/send-transaction';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './TransactionSuccessView.module.scss';
 import { useWalletStore } from '@src/stores';
 import { useObservable } from '@lace/common';
-import BigNumber from 'bignumber.js';
-import { Wallet } from '@lace/cardano';
-import { isNFT } from '@src/utils/is-nft';
-import flatMapDeep from 'lodash/flatMapDeep';
-
-interface TokenAnalyticsProperties {
-  id: string;
-  name?: string;
-  ticker?: string;
-  amount: string;
-}
-
-const getCardanoCoinAnalyticsProperties = (cardanoCoin: Wallet.CoinId, amount: string) => ({
-  id: cardanoCoin.id,
-  name: cardanoCoin.name,
-  ticker: cardanoCoin.symbol,
-  amount
-});
-
-const getAssetAnalyticsProperties = (assetsMap: Wallet.Assets, id: string, amount: string) => {
-  const info = assetsMap.get(Wallet.Cardano.AssetId(id));
-  const name = isNFT(info) ? info?.nftMetadata?.name : info?.tokenMetadata?.name;
-  const ticker = info?.tokenMetadata?.ticker;
-  return { id: info?.fingerprint, amount, name, ticker };
-};
-
-const getTokensProperty = (outputs: OutputList, assetsMap: Wallet.Assets, cardanoCoin: Wallet.CoinId) => {
-  const tokensAnalyticsPropertyMap = new Map<string, TokenAnalyticsProperties>();
-  // gets an array of assets sent in each tx output
-  const sentAssets = Object.values(outputs).map(({ assets }) => assets);
-  // flat the array
-  const flattedSentAssets = flatMapDeep(sentAssets);
-
-  for (const { id: key, value } of flattedSentAssets) {
-    const tokensAnalyticsProperty = tokensAnalyticsPropertyMap.get(key);
-
-    if (tokensAnalyticsProperty) {
-      // if the token exists in the property map, add the amount to the current amount in the map
-      const amount = new BigNumber(tokensAnalyticsProperty.amount).plus(value).toString();
-      tokensAnalyticsPropertyMap.set(key, { ...tokensAnalyticsProperty, amount });
-    } else {
-      // if the token don't exists in the property map, get the properties
-      const properties =
-        key === cardanoCoin.id
-          ? getCardanoCoinAnalyticsProperties(cardanoCoin, value)
-          : getAssetAnalyticsProperties(assetsMap, key, value);
-
-      tokensAnalyticsPropertyMap.set(key, properties);
-    }
-  }
-  return [...tokensAnalyticsPropertyMap.values()];
-};
+import { getTokensProperty } from '../helpers';
 
 export const TransactionSuccessView = ({ footerSlot }: { footerSlot?: React.ReactElement }): React.ReactElement => {
   const { t } = useTranslation();
