d4dbe5e4f6a376ae9833aae5b5307da1bb5a0264
JP Lorek <juan.lorek@globant.com>
2023-08-16 13:42:18 -0300

    chore(extension): add getHWPersonProperties function

--- apps/browser-extension-wallet/src/views/browser-view/features/wallet-setup/helpers.ts
+++ apps/browser-extension-wallet/src/views/browser-view/features/wallet-setup/helpers.ts
@@ -1,17 +1,18 @@
 /* eslint-disable camelcase */
 import { Wallet } from '@lace/cardano';
 import { LedgerKeyAgent } from '@cardano-sdk/hardware-ledger';
+import { PostHogProperties } from '@providers/AnalyticsProvider/analyticsTracker';
 
 export const isTrezorHWSupported = (): boolean => process.env.USE_TREZOR_HW === 'true';
 export const isHardwareWalletAvailable = (wallet: Wallet.HardwareWallets): boolean =>
   wallet !== Wallet.KeyManagement.KeyAgentType.Trezor || isTrezorHWSupported();
-type HardwareWalletPersonaProperties = {
+type HardwareWalletPersonProperties = {
   model: string;
-  firmwareVersion: string;
+  firmwareVersion?: string;
   cardanoAppVersion?: string;
 };
 
-export const getTrezorSpecifications = async (): Promise<HardwareWalletPersonaProperties> => {
+export const getTrezorSpecifications = async (): Promise<HardwareWalletPersonProperties> => {
   const { model, major_version, minor_version, patch_version } =
     await Wallet.KeyManagement.TrezorKeyAgent.checkDeviceConnection(Wallet.KeyManagement.CommunicationType.Web);
   return {
@@ -22,12 +23,27 @@ export const getTrezorSpecifications = async (): Promise<HardwareWalletPersonaPr
 
 export const getLedgerSpecifications = async (
   deviceConnection: LedgerKeyAgent['deviceConnection']
-): Promise<HardwareWalletPersonaProperties> => {
+): Promise<HardwareWalletPersonProperties> => {
   const cardanoApp = await deviceConnection.getVersion();
-  const firmware = await LedgerKeyAgent.getAppVersion(Wallet.KeyManagement.CommunicationType.Web, deviceConnection);
   return {
     model: deviceConnection.transport.deviceModel.id,
-    firmwareVersion: `${firmware.version.major}.${firmware.version.minor}.${firmware.version.patch}`,
     cardanoAppVersion: `${cardanoApp.version.major}.${cardanoApp.version.minor}.${cardanoApp.version.patch}`
+  };
+};
+
+export const getHWPersonProperties = async (
+  connectedDevice: Wallet.HardwareWallets,
+  deviceConnection: Wallet.DeviceConnection
+): Promise<PostHogProperties> => {
+  const HWSpecifications =
+    connectedDevice === Wallet.KeyManagement.KeyAgentType.Trezor
+      ? await getTrezorSpecifications()
+      : await getLedgerSpecifications(deviceConnection as LedgerKeyAgent['deviceConnection']);
+  return {
+    $set_once: {
+      initial_hardware_wallet_model: HWSpecifications.model,
+      initial_firmware_version: HWSpecifications?.firmwareVersion,
+      initial_cardano_app_version: HWSpecifications?.cardanoAppVersion
+    }
   };
 };
