908af55076737399a2e33656727865737c09caef
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-08-17 13:33:18 +0200

    feat(staking): LW-7547 read the total staked value from the sdk data (#397)
    * feat(staking): read the total staked value from the sdk data
    
    * refactor(staking): remove console.logs
    
    * feat(staking,cardano): expose Percent util from cardnao and fix staking unit tests

--- apps/browser-extension-wallet/src/views/browser-view/features/staking/components/MultiDelegationStaking.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/staking/components/MultiDelegationStaking.tsx
@@ -1,14 +1,7 @@
 import { OutsideHandlesProvider, Staking } from '@lace/staking';
 import React from 'react';
 import { useBackgroundServiceAPIContext, useCurrencyStore, useExternalLinkOpener, useTheme } from '@providers';
-import {
-  useBalances,
-  useDelegationDetails,
-  useFetchCoinPrice,
-  useLocalStorage,
-  useStakingRewards,
-  useWalletManager
-} from '@hooks';
+import { useBalances, useFetchCoinPrice, useLocalStorage, useStakingRewards, useWalletManager } from '@hooks';
 import { stakePoolDetailsSelector, useDelegationStore } from '@src/features/delegation/stores';
 import { usePassword, useSubmitingState } from '@views/browser/features/send-transaction';
 import { useWalletStore } from '@stores';
@@ -19,7 +12,6 @@ const MULTIDELEGATION_FIRST_VISIT_LS_KEY = 'multidelegationFirstVisit';
 export const MultiDelegationStaking = (): JSX.Element => {
   const { theme } = useTheme();
   const { setWalletPassword } = useBackgroundServiceAPIContext();
-  const delegationDetails = useDelegationDetails();
   const selectedStakePoolDetails = useDelegationStore(stakePoolDetailsSelector);
   const {
     delegationTxBuilder,
@@ -68,7 +60,6 @@ export const MultiDelegationStaking = (): JSX.Element => {
       {...{
         backgroundServiceAPIContextSetWalletPassword: setWalletPassword,
         balancesBalance: balance,
-        delegationDetails,
         delegationStoreSelectedStakePoolDetails: selectedStakePoolDetails,
         delegationStoreSetDelegationTxBuilder: setDelegationTxBuilder,
         delegationStoreDelegationTxBuilder: delegationTxBuilder,
