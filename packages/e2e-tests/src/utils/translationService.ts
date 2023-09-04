f17b968c5a2b872fb94603d9cd519929f03c2f09
Janusz Janus <janusz.janus@iohk.io>
2023-09-04 15:57:16 +0200

    test(extension): add staking to translation sources (#476)
    * test(extension): add staking to translation sources

--- packages/e2e-tests/src/utils/translationService.ts
+++ packages/e2e-tests/src/utils/translationService.ts
@@ -1,22 +1,31 @@
 import { flatten } from 'flat';
 import { readFromFile } from './fileUtils';
+import { en } from '../../../staking/src/features/i18n/translations/en';
 
-const loadTranslations = async function (isCoreTranslation: boolean) {
+const loadTranslations = async function (translationType = 'base') {
   const language = process.env.LACE_LOCALE ?? 'en';
 
   const extensionTranslationPath = `../../../../apps/browser-extension-wallet/src/lib/translations/${language}.json`;
   const coreTranslationPath = `../../../../packages/core/dist/translations/${language}.json`;
 
-  const translationsFilePath = isCoreTranslation ? coreTranslationPath : extensionTranslationPath;
-  const stringStream = readFromFile(__dirname, translationsFilePath);
-  return await flatten(JSON.parse(stringStream.toString()));
+  let stringStream;
+  switch (translationType) {
+    case 'base':
+      stringStream = await flatten(JSON.parse(readFromFile(__dirname, extensionTranslationPath).toString()));
+      break;
+    case 'core':
+      stringStream = await flatten(JSON.parse(readFromFile(__dirname, coreTranslationPath).toString()));
+      break;
+    case 'staking':
+      stringStream = en;
+      break;
+  }
+
+  return stringStream;
 };
-let extensionTranslations: { [index: string]: any };
-let coreTranslations: { [index: string]: any };
+let translations: { [index: string]: any };
 
-export const t = async (key: string, isCoreTranslation = false): Promise<string> => {
-  extensionTranslations = extensionTranslations ?? (await loadTranslations(false));
-  coreTranslations = coreTranslations ?? (await loadTranslations(true));
-
-  return isCoreTranslation ? coreTranslations[key] : extensionTranslations[key];
+export const t = async (key: string, translationType = 'base'): Promise<string> => {
+  translations = await loadTranslations(translationType);
+  return translations[key];
 };
