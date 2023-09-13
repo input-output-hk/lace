f66aa2cab84f8215ee5dd6b15d393d3c39f52827
John Oshalusi <john.oshalusi@iohk.io>
2023-09-13 16:37:45 +0100

    feat(ui): lw-6627 create <Message /> component

---
+++ packages/ui/src/design-system/message/message.css.ts
@@ -0,0 +1,35 @@
+import { style } from '@vanilla-extract/css';
+
+import { sx } from '../../design-tokens';
+
+export const defaultContainer = sx({
+  maxWidth: '$294',
+});
+
+export const sideDrawerContainer = sx({
+  maxWidth: '$336',
+});
+
+export const icon = sx({
+  width: '$84',
+  height: '$84',
+  marginBottom: '$4',
+});
+
+export const title = style([
+  sx({
+    color: '$message_title_color',
+  }),
+  {
+    textAlign: 'center',
+  },
+]);
+
+export const description = style([
+  sx({
+    color: '$message_description_color',
+  }),
+  {
+    textAlign: 'center',
+  },
+]);
