f196fc78b81415567880d369405d997cb4e4103e
John Oshalusi <oshalusijohn@gmail.com>
2023-09-13 13:38:16 +0100

    feat: create <Tooltip /> component (#511)

---
+++ packages/ui/src/design-system/tooltip/tooltip-provider.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+
+import * as Tooltip from '@radix-ui/react-tooltip';
+
+type Props = PropsWithChildren<{
+  delayDuration?: number;
+  skipDelayDuration?: number;
+}>;
+
+export const Provider = ({
+  delayDuration = 0,
+  skipDelayDuration = 0,
+  children,
+}: Readonly<Props>): JSX.Element => {
+  return (
+    <>
+      <Tooltip.Provider
+        delayDuration={delayDuration}
+        skipDelayDuration={skipDelayDuration}
+      >
+        {children}
+      </Tooltip.Provider>
+    </>
+  );
+};
