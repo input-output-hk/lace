bc1b146d1933e307f2ad8d4ff02696001a5b91c4
Tom Mayel <tom.mayel@iohk.io>
2023-09-15 06:57:44 +0100

    feat(extension): add recipient_source LW-8355

--- apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/TransactionSuccessView.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/TransactionSuccessView.tsx
@@ -11,13 +11,14 @@ import {
   TokenAnalyticsProperties,
   useMetadata
 } from '@views/browser/features/send-transaction';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styles from './TransactionSuccessView.module.scss';
 import { useWalletStore } from '@src/stores';
 import { useObservable } from '@lace/common';
 import { getTokensProperty } from '../helpers';
 import { Wallet } from '@lace/cardano';
+import { useGetFilteredAddressBook } from '@src/features/address-book/hooks';
 
 export const TransactionSuccessView = ({ footerSlot }: { footerSlot?: React.ReactElement }): React.ReactElement => {
   const { t } = useTranslation();
@@ -32,6 +33,7 @@ export const TransactionSuccessView = ({ footerSlot }: { footerSlot?: React.Reac
   const assets = useObservable(inMemoryWallet.assetInfo$);
   const [customAnalyticsProperties, setCustomAnalyticsProperties] = useState<TokenAnalyticsProperties[]>();
   const [metadata] = useMetadata();
+  const { getMatchedAddresses } = useGetFilteredAddressBook();
 
   useEffect(() => {
     // run this only when assets value was emitted and customAnalyticsProperties is undefined
@@ -40,13 +42,22 @@ export const TransactionSuccessView = ({ footerSlot }: { footerSlot?: React.Reac
     }
   }, [assets, cardanoCoin, customAnalyticsProperties, uiOutputs]);
 
-  useEffect(() => {
+  const sendEventOnDone = useCallback(async () => {
     // send analytics event after customAnalyticsProperties was set
     if (customAnalyticsProperties) {
-      const recipientTypes = Object.values(uiOutputs).map((row) =>
+      const recipients = Object.values(uiOutputs);
+      const recipientSources = await Promise.all(
+        recipients.map(async (row) => {
+          const addressIdentifier = row.handle || row.address;
+          const found = await getMatchedAddresses({ value: addressIdentifier });
+          return found?.length ? 'address book' : 'not on address book';
+        })
+      );
+      const recipientTypes = recipients.map((row) =>
         row.handle ? TxRecipientType.AdaHandle : TxRecipientType.RegularAddress
       );
       const recipientTypesUnique = uniq(recipientTypes);
+      const recipientSourceUnique = uniq(recipientSources);
       analytics.sendEventToPostHog(PostHogAction.SendAllDoneView, {
         // eslint-disable-next-line camelcase
         trigger_point: triggerPoint,
@@ -61,11 +72,17 @@ export const TransactionSuccessView = ({ footerSlot }: { footerSlot?: React.Reac
         // eslint-disable-next-line camelcase
         metadata_defined: !!metadata,
         // eslint-disable-next-line camelcase
-        recipient_type: recipientTypesUnique
+        recipient_type: recipientTypesUnique,
+        // eslint-disable-next-line camelcase
+        recipient_source: recipientSourceUnique
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customAnalyticsProperties]);
+
+  useEffect(() => {
+    sendEventOnDone();
+  }, [sendEventOnDone]);
 
   return (
     <>
