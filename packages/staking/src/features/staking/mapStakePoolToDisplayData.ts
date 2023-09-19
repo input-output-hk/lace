b902d4e3ba82352480648e9704da81d0b72c5514
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-09-20 01:06:22 +0200

    refactor(staking): add a mapper transforming a stake pool into a universal display data

---
+++ packages/staking/src/features/staking/mapStakePoolToDisplayData.ts
@@ -0,0 +1,45 @@
+import { Wallet } from '@lace/cardano';
+import { lovelacesToAdaString } from '@lace/cardano/src/wallet/util';
+import { formatPercentages, getNumberWithUnit, getRandomIcon } from '@lace/common';
+
+// TODO: try to remove data existense checks. in most cases that data is present according to StakePool type
+// eslint-disable-next-line complexity
+export const mapStakePoolToDisplayData = ({
+  cardanoCoin,
+  stakePool,
+}: {
+  cardanoCoin: Wallet.CoinId;
+  stakePool: Wallet.Cardano.StakePool;
+}) => {
+  const margin = formatPercentages(stakePool.margin.numerator / stakePool.margin.denominator);
+  const fee = Wallet.util.lovelacesToAdaString(stakePool.cost.toString());
+
+  return {
+    apy: stakePool.metrics?.apy && formatPercentages(stakePool.metrics.apy),
+    contact: {
+      primary: stakePool.metadata?.homepage,
+      ...stakePool.metadata?.ext?.pool.contact,
+    },
+    cost: `${margin || '-'}% + ${fee}${cardanoCoin.symbol}`,
+    delegators: stakePool.metrics?.delegators || '-',
+    description: stakePool.metadata?.description || '-',
+    fee,
+    hexId: stakePool.hexId.toString(),
+    id: stakePool.id.toString(),
+    logo:
+      stakePool.metadata?.ext?.pool.media_assets?.icon_png_64x64 ||
+      getRandomIcon({ id: stakePool.id.toString(), size: 30 }),
+    margin,
+    name: stakePool.metadata?.name || '-',
+    owners: stakePool.owners ? stakePool.owners.map((owner: Wallet.Cardano.RewardAccount) => owner.toString()) : [],
+    pledge: stakePool.pledge ? `${lovelacesToAdaString(stakePool.pledge.toString())}${cardanoCoin.symbol}` : '-',
+    retired: stakePool.status === Wallet.Cardano.StakePoolStatus.Retired,
+    saturation: stakePool.metrics?.saturation && formatPercentages(stakePool.metrics.saturation),
+    size: `${stakePool.metrics?.size.live ?? '-'} %`,
+    stake: stakePool.metrics?.stake.active
+      ? getNumberWithUnit(Wallet.util.lovelacesToAdaString(stakePool.metrics?.stake.active.toString()))
+      : { number: '-' },
+    status: stakePool.status,
+    ticker: stakePool.metadata?.ticker || '-',
+  };
+};
