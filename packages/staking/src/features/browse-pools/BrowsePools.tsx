20e2cb3c37f5aa9896d1d2e587d764f1e3715450
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-09-23 00:28:24 +0200

    refactor(staking): remove redundant commented code from browse pools

--- packages/staking/src/features/browse-pools/BrowsePools.tsx
+++ packages/staking/src/features/browse-pools/BrowsePools.tsx
@@ -4,47 +4,6 @@ import { StakePoolsTable } from './stake-pools-table';
 const LACE_APP_ID = 'lace-app';
 
 export const BrowsePools = () => (
-  // const { setIsDrawerVisible } = useStakePoolDetails();
-
-  // const {
-  //   walletStoreStakePoolSearchResults: stakePoolSearchResults,
-  //   walletStoreStakePoolSearchResultsStatus: stakePoolSearchResultsStatus,
-  //   walletStoreFetchStakePools: fetchStakePools,
-  //   delegationStoreSetSelectedStakePool: setSelectedStakePool,
-  //   walletStoreNetworkInfo: networkInfo,
-  //   walletStoreGetKeyAgentType: getKeyAgentType,
-  // } = useOutsideHandles();
-
-  // const isSearching = stakePoolSearchResultsStatus === StateStatus.LOADING;
-
-  // const isInMemory = useMemo(() => getKeyAgentType() === Wallet.KeyManagement.KeyAgentType.InMemory, [getKeyAgentType]);
-
-  // useEffect(() => {
-  //   const fetchSelectedStakePool = async () => {
-  //     if (isInMemory || !networkInfo) return;
-  //     const stakePoolId = localStorage.getItem('TEMP_POOLID');
-  //     if (!stakePoolId) return;
-  //     const searchString = String(stakePoolId);
-  //     await fetchStakePools({ searchString });
-  //     const foundStakePool = stakePoolSearchResults.pageResults.find(
-  //       (pool: Wallet.Cardano.StakePool) => pool?.id?.toString() === stakePoolId
-  //     );
-  //     if (!foundStakePool) return;
-  //     setSelectedStakePool(foundStakePool);
-  //     setIsDrawerVisible(true);
-  //     fetchStakePools({ searchString: '' });
-  //   };
-  //   fetchSelectedStakePool();
-  // }, [
-  //   setIsDrawerVisible,
-  //   setSelectedStakePool,
-  //   stakePoolSearchResults,
-  //   networkInfo,
-  //   isSearching,
-  //   isInMemory,
-  //   fetchStakePools,
-  // ]);
-
   <>
     <PortfolioBar />
     <StakePoolsTable scrollableTargetId={LACE_APP_ID} />
