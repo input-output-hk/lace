64f8f59395eaddce2740202faddfa011c99ecbc5
Lucas Araujo <lucas.araujo@iohk.io>
2023-09-21 22:02:29 -0300

    feat: check for DRep transaction

--- apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/ConfirmTransaction.tsx
+++ apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/ConfirmTransaction.tsx
@@ -1,22 +1,20 @@
-import React, { useMemo } from 'react';
-import { Button, useObservable } from '@lace/common';
+import React, { useState } from 'react';
+import { Button } from '@lace/common';
 import { useTranslation } from 'react-i18next';
-import { DappTransaction } from '@lace/core';
 import { Layout } from '../Layout';
 import { useViewsFlowContext } from '@providers/ViewFlowProvider';
 import { sectionTitle, DAPP_VIEWS } from '../../config';
 import styles from './ConfirmTransaction.module.scss';
 import { Wallet } from '@lace/cardano';
-import { useAddressBookContext, withAddressBookContext } from '@src/features/address-book/context';
 import { useWalletStore } from '@stores';
-import { AddressListType } from '@views/browser/features/activity';
+import { useDisallowSignTx, useSignWithHardwareWallet, useSignTxData } from './hooks';
 import { consumeRemoteApi, RemoteApiPropertyType } from '@cardano-sdk/web-extension';
 import { DappDataService } from '@lib/scripts/types';
 import { DAPP_CHANNELS } from '@src/utils/constants';
 import { runtime } from 'webextension-polyfill';
 import { Skeleton } from 'antd';
-import { TokenInfo } from '@src/utils/get-assets-information';
-import { useCreateAssetList, useDisallowSignTx, useSignTxData, useSignWithHardwareWallet, useTxSummary } from './hooks';
+import { DRepRegistrationContent } from './DRepRegistrationContainer';
+import { SendContent } from './SendContainer';
 
 const dappDataApi = consumeRemoteApi<Pick<DappDataService, 'getSignTxData'>>(
   {
@@ -28,68 +26,42 @@ const dappDataApi = consumeRemoteApi<Pick<DappDataService, 'getSignTxData'>>(
   { logger: console, runtime }
 );
 
-export const ConfirmTransaction = withAddressBookContext((): React.ReactElement => {
+export const ConfirmTransaction = (): React.ReactElement => {
   const {
     utils: { setNextView }
   } = useViewsFlowContext();
   const { t } = useTranslation();
-  const {
-    walletInfo,
-    inMemoryWallet,
-    getKeyAgentType,
-    blockchainProvider: { assetProvider }
-  } = useWalletStore();
-  const { list: addressList } = useAddressBookContext() as { list: AddressListType[] };
+  const { getKeyAgentType } = useWalletStore();
   const { signTxData, errorMessage } = useSignTxData(dappDataApi.getSignTxData);
-  const tx = signTxData?.tx;
-  const outputs = tx?.body?.outputs;
-  const assets = useObservable<TokenInfo | null>(inMemoryWallet.assetInfo$);
-  const availableBalance = useObservable(inMemoryWallet.balance.utxo.available$);
+
   const keyAgentType = getKeyAgentType();
-  const isUsingHardwareWallet = useMemo(
-    () => keyAgentType !== Wallet.KeyManagement.KeyAgentType.InMemory,
-    [keyAgentType]
-  );
+  const isUsingHardwareWallet = keyAgentType !== Wallet.KeyManagement.KeyAgentType.InMemory;
   const disallowSignTx = useDisallowSignTx();
   const { isConfirmingTx, signWithHardwareWallet } = useSignWithHardwareWallet();
-  const createAssetList = useCreateAssetList({ outputs, assets, assetProvider });
-  const { txSummary, hasInsufficientFunds } = useTxSummary({
-    addressList,
-    createAssetList,
-    availableBalance,
-    tx,
-    walletInfo
-  });
-
-  const translations = {
-    transaction: t('core.dappTransaction.transaction'),
-    amount: t('core.dappTransaction.amount'),
-    recipient: t('core.dappTransaction.recipient'),
-    fee: t('core.dappTransaction.fee'),
-    insufficientFunds: t('core.dappTransaction.insufficientFunds'),
-    adaFollowingNumericValue: t('general.adaFollowingNumericValue')
-  };
-  window.addEventListener('beforeunload', cancelTransaction);
+  const [isConfirmBtnDisabled, setConfirmBtnDisabled] = useState(true);
+  const isDRepRegistration = false; // signTxData?.tx.body.certificates.some(
+  //  ({ __typename }) => __typename === Wallet.Cardano.CertificateType.RegisterDelegateRepresentative
+  // );
 
   return (
     <Layout pageClassname={styles.spaceBetween} title={t(sectionTitle[DAPP_VIEWS.CONFIRM_TX])}>
-      {tx && txSummary ? (
-        <DappTransaction
-          transaction={txSummary}
-          dappInfo={signTxData?.dappInfo}
-          errorMessage={errorMessage}
-          translations={translations}
-          hasInsufficientFunds={hasInsufficientFunds}
-        />
-      ) : (
-        <Skeleton loading />
-      )}
+      {!signTxData && <Skeleton loading />}
+      {signTxData &&
+        (isDRepRegistration ? (
+          <DRepRegistrationContent signTxData={signTxData} />
+        ) : (
+          <SendContent
+            signTxData={signTxData}
+            errorMessage={errorMessage}
+            disableConfirmationBtn={setConfirmBtnDisabled}
+          />
+        ))}
       <div className={styles.actions}>
         <Button
           onClick={async () => {
             isUsingHardwareWallet ? signWithHardwareWallet() : setNextView();
           }}
-          disabled={!!errorMessage || hasInsufficientFunds}
+          disabled={isConfirmBtnDisabled}
           loading={isUsingHardwareWallet && isConfirmingTx}
           data-testid="dapp-transaction-confirm"
           className={styles.actionBtn}
@@ -109,4 +81,4 @@ export const ConfirmTransaction = withAddressBookContext((): React.ReactElement 
       </div>
     </Layout>
   );
-});
+};
