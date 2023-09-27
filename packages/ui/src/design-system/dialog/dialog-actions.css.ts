d794ecf9885a2b3f4dcb64cbc1c03a2d65e8c9b9
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2023-09-27 16:05:49 +0200

    feat(ui): remove padding-top from Dialog.Actions

--- packages/ui/src/design-system/dialog/dialog-actions.css.ts
+++ packages/ui/src/design-system/dialog/dialog-actions.css.ts
@@ -4,8 +4,6 @@ export const dialogActions = sx({
   display: 'flex',
   justifyContent: 'center',
   gap: '$16',
-  // TODO waiting for design team decision
-  paddingTop: '$8',
   flexDirection: {
     popupScreen: 'column-reverse',
     smallScreen: 'row',
