e7567f4aa4e96cfac9160a0369346405a812a29d
xdzurman <kamil.dzurman@gmail.com>
2023-09-28 19:05:28 +0200

    feat(staking): contraint slider to a range of 1 to 100

--- packages/staking/src/features/drawer/DelegationRatioSlider.tsx
+++ packages/staking/src/features/drawer/DelegationRatioSlider.tsx
@@ -11,18 +11,28 @@ export type DelegationRatioSlider = Omit<Slider.SliderProps, 'value' | 'onValueC
   onValueChange?: (value: number) => void;
 };
 
+const CONSTRAINTS = {
+  MAX: 100,
+  MIN: 1,
+};
+
 export const DelegationRatioSlider = React.forwardRef(
   (props: DelegationRatioSlider, forwardedRef: React.ForwardedRef<HTMLInputElement>) => {
-    const value = props.value || props.defaultValue || 0;
-    const handlePlusClick = () => props.onValueChange && props.onValueChange(value + 1);
-    const handleMinusClick = () => props.onValueChange && props.onValueChange(value - 1);
+    const fallbackValue = CONSTRAINTS.MIN;
+    const value = props.value || props.defaultValue || fallbackValue;
+    const handlePlusClick = () => props.onValueChange && value < CONSTRAINTS.MAX && props.onValueChange(value + 1);
+    const handleMinusClick = () => props.onValueChange && value > CONSTRAINTS.MIN && props.onValueChange(value - 1);
     return (
       <div className={styles.SliderContainer}>
         <IconButton.Primary icon={<SliderMinusIcon />} onClick={handleMinusClick} />
         <Slider.Root
+          min={CONSTRAINTS.MIN}
+          max={CONSTRAINTS.MAX}
           className={styles.SliderRoot}
           {...props}
-          onValueChange={(newValue) => (props.onValueChange ? props.onValueChange(newValue[0] || 0) : undefined)}
+          onValueChange={(newValue) =>
+            props.onValueChange ? props.onValueChange(newValue[0] || fallbackValue) : undefined
+          }
           value={[value]}
           defaultValue={props.defaultValue ? [props.defaultValue] : undefined}
           ref={forwardedRef}
