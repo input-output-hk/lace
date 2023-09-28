7adee1e85e932b0c1e11ef47af8833ee215ca5db
xdzurman <kamil.dzurman@gmail.com>
2023-09-28 10:23:10 +0200

    feat(staking): improve naming/comments

--- packages/staking/src/features/store/delegationPortfolioStore/stateMachine/normalizePercentages.ts
+++ packages/staking/src/features/store/delegationPortfolioStore/stateMachine/normalizePercentages.ts
@@ -2,8 +2,9 @@ import { PERCENTAGE_SCALE_MAX } from '../constants';
 
 // Rounds values of integer properties of an input array of objects, while keeping the sum of the numbers equal to 100 (PERCENTAGE_SCALE_MAX)
 // Requires input array to sum to 100
-// Examples: roundPercentages([33.3, 33.3, 33.3]) => [34, 33, 33]
-export const roundPercentages = <K extends string, T extends { [key in K]: number }>(items: T[], key: K) => {
+// Examples: normalizePercentages([{val: 33.33},{val: 33.33},{val: 33.33}], 'val')
+//                            =>  [{val: 34},   {val: 33},   {val: 33}   ]
+export const normalizePercentages = <K extends string, T extends { [key in K]: number }>(items: T[], key: K) => {
   const currentSum = items.reduce((acc, item) => acc + item[key], 0);
   const epsilon = 0.1;
   if (Math.abs(currentSum - PERCENTAGE_SCALE_MAX) > epsilon)
