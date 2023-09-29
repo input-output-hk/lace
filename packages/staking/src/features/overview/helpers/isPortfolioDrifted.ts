ad6924d3875850a89ea91a4f5bd8196f955ef7da
refi93 <rafael.korbas@gmail.com>
2023-09-29 15:20:33 +0200

    fix(staking): temporarily disable portfolio drift check

--- packages/staking/src/features/overview/helpers/isPortfolioDrifted.ts
+++ packages/staking/src/features/overview/helpers/isPortfolioDrifted.ts
@@ -6,6 +6,8 @@ import type { CurrentPortfolioStakePool } from '../../store';
 import { PERCENTAGE_SCALE_MAX } from '../../store';
 
 const PORTFOLIO_DRIFT_PERCENTAGE_THRESHOLD = 15;
+// tmp hotfix before release: disabling portfolio drift, until portfolio persistence is implemented
+const TMP_PERSISTENCE_HOTFIX_DISABLE_PORTFOLIO_DRIFT = true;
 
 const getPortfolioTotalPercentageDrift = (portfolio: CurrentPortfolioStakePool[]): number => {
   const totalValue = Wallet.BigIntMath.sum(portfolio.map(({ value }) => value));
@@ -22,6 +24,8 @@ const getPortfolioTotalPercentageDrift = (portfolio: CurrentPortfolioStakePool[]
 
 // TODO: move this file to store. It gets imported also outside the overview feature so the store seems better place.
 export const isPortfolioDrifted = (currentPortfolio: CurrentPortfolioStakePool[]) => {
+  if (TMP_PERSISTENCE_HOTFIX_DISABLE_PORTFOLIO_DRIFT) return false;
+
   const drift = getPortfolioTotalPercentageDrift(currentPortfolio);
   return drift >= PORTFOLIO_DRIFT_PERCENTAGE_THRESHOLD;
 };
