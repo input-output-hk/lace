99af3a115b68184b773ed7b1cbed480cfe38afd5
refi93 <rafael.korbas@gmail.com>
2023-10-02 17:44:06 +0200

    feat(staking): make pool details card sticky on management drawer

---
+++ packages/staking/src/features/drawer/preferences/StepPreferencesContent.css.ts
@@ -0,0 +1,14 @@
+import { style } from '@lace/ui';
+import { theme } from '../../theme';
+
+export const delegationCard = style({
+  '@media': {
+    '(min-height: 668px)': {
+      backgroundColor: theme.colors.$drawerBackgroundColor,
+      minWidth: theme.spacing.$584,
+      position: 'sticky',
+      top: 0,
+      zIndex: 1,
+    },
+  },
+});
