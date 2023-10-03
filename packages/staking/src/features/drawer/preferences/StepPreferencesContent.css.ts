2bbb421fefc7afa7bb0cc35710eade1cfee5cc08
refi93 <rafael.korbas@gmail.com>
2023-10-03 10:41:48 +0200

    feat(staking): add blurry shadow to sticky delegation card

--- packages/staking/src/features/drawer/preferences/StepPreferencesContent.css.ts
+++ packages/staking/src/features/drawer/preferences/StepPreferencesContent.css.ts
@@ -5,6 +5,7 @@ export const delegationCardWrapper = style({
   '@media': {
     '(min-height: 668px)': {
       backgroundColor: theme.colors.$drawerBackgroundColor,
+      boxShadow: `10px ${theme.spacing.$24} 10px ${theme.colors.$drawerBackgroundColor}, -10px ${theme.spacing.$24} 10px ${theme.colors.$drawerBackgroundColor}`,
       minWidth: theme.spacing.$584,
       position: 'sticky',
       top: 0,
