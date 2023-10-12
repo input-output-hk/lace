ee1fd71353ff97e9b955fea7b445bef2135c9b78
JP Lorek <juan.lorek@globant.com>
2023-10-12 13:25:56 -0300

    feat(extension): enable fullscreen video and remove thumbnail

--- packages/core/src/ui/components/WalletSetup/WalletSetupMnemonicIntroStep.tsx
+++ packages/core/src/ui/components/WalletSetup/WalletSetupMnemonicIntroStep.tsx
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { WalletSetupStepLayout, WalletTimelineSteps } from './WalletSetupStepLayout';
 import { TranslationsFor } from '@ui/utils/types';
-import { ReactComponent as PlayIcon } from '../../assets/icons/play-icon.component.svg';
-import Thumbnail from '../../assets/images/video-thumbnail.png';
 import styles from './WalletSetupOption.module.scss';
 
 export interface WalletSetupMnemonicIntroStepProps {
@@ -38,18 +36,13 @@ export const WalletSetupMnemonicIntroStep = ({
           onClickVideo();
         }}
       >
-        {shouldDisplayThumbnail && (
-          <>
-            <PlayIcon className={styles.playIcon} />
-            <img src={Thumbnail} alt="" className={styles.image} data-testid="mnemonic-intro-image" />
-          </>
-        )}
+        {shouldDisplayThumbnail && <div className={styles.overlay} />}
         <iframe
           ref={videoRef}
           className={styles.video}
           src="https://www.youtube-nocookie.com/embed/hOFVXo969rk?si=0a-hNDVME6eTboIX"
           title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         />
       </div>
     </WalletSetupStepLayout>
