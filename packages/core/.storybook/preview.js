61226913da94be09cbdf4452d1daa4f7cab0cf79
Renan Ferreira <renan.ferreira@iohk.io>
2023-10-18 11:24:08 -0300

    chore(core): downgrade storybook to v6

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,9 +1,10 @@
-import type { Preview } from '@storybook/react';
+import React from 'react';
 import 'antd/dist/antd.css';
 import 'normalize.css';
 import './theme.scss';
+import { ThemeColorScheme, ThemeProvider } from '@lace/ui';
 
-const preview: Preview = {
+const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
@@ -12,7 +13,14 @@ const preview: Preview = {
         date: /Date$/
       }
     }
-  }
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider colorScheme={ThemeColorScheme.Light}>
+        <Story />
+      </ThemeProvider>
+    )
+  ]
 };
 
 export default preview;
