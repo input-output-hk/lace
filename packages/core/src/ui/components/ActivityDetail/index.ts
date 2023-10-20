edc882f88727987b777d45fad72296c6fb071a13
refi93 <rafael.korbas@gmail.com>
2023-10-20 23:00:43 +0200

    fix(all): fix type import

--- packages/core/src/ui/components/ActivityDetail/index.ts
+++ packages/core/src/ui/components/ActivityDetail/index.ts
@@ -1,6 +1,6 @@
 export * from './TransactionDetails';
 export * from './RewardsDetails';
-export * from './activityType';
+export type * from './activityType';
 export * from './ActivityTypeIcon';
 export * from './TransactionDetailAsset';
 export * from './TransactionInputOutput';
