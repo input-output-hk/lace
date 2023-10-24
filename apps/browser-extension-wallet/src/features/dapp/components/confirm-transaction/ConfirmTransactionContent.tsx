2618204c8b0de8b720ed7b0ad0c13da957356b08
Lucas <lucas.araujo@iohk.io>
2023-10-24 13:10:36 -0300

    [LW-7983] [LW-7984] dRep confirmation screens (#554)
    * refactor: move logic out of component
    
    * fix: file path
    
    * feat(core): setup storybook
    
    * refactor(ui): extract insufficient funds warning component
    
    * feat(core): add lace/ui package
    
    * feat(core): create ConfirmDRepRegistration component
    
    * feat(extension): install @lace/ui
    
    * refactor: move functions to utils
    
    * fix: type
    
    * refactor: types file
    
    * feat: check for DRep transaction
    
    * feat: integrate confirm drep registration component
    
    * refactor(core): use Metadata component
    
    * refactor(core): use flexbox
    
    * refactor(core): remove insufficient funds message
    
    * refactor(core): create ConfirmDRepRetirement
    
    * refactor: remove insufficient funds warning; break down main component
    
    * feat: add certificate data
    
    * feat: add drep retirement container
    
    * fix: condition
    
    * fix: type
    
    * fix: style
    
    * fix: check for anchor
    
    * fix: check for undefined certificates
    
    * fix: merge conflict
    
    * fix: type error
    
    * feat: temp env changes
    
    * feat(extension): use consistent naming of DRep ID
    
    * feat(extension): show anchor metadata if exists
    
    * feat(extension): convert user facing DRepID to bech32
    
    * fix: add cardano symbol to deposit amount
    
    * chore(core): downgrade storybook to v6
    
    * fix: update lock file
    
    * fix: styles and text
    
    * fix: storybook build
    
    * fix: storybook build
    
    * feat: check retired drep key
    
    * fix: check retired drep key
    
    ---------
    
    Co-authored-by: Renan Ferreira <renan.ferreira@iohk.io>
    Co-authored-by: Michael Chappell <7581002+mchappell@users.noreply.github.com>

---
+++ apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/ConfirmTransactionContent.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Skeleton } from 'antd';
+import { ConfirmDRepRegistrationContainer } from './ConfirmDRepRegistrationContainer';
+import { DappTransactionContainer } from './DappTransactionContainer';
+import { TxType } from './utils';
+import { SignTxData } from './types';
+import { ConfirmDRepRetirementContainer } from './ConfirmDRepRetirementContainer';
+
+interface Props {
+  txType?: TxType;
+  signTxData?: SignTxData;
+  errorMessage?: string;
+}
+
+export const ConfirmTransactionContent = ({ txType, signTxData, errorMessage }: Props): React.ReactElement => {
+  if (!signTxData) {
+    return <Skeleton loading />;
+  }
+  if (txType === TxType.DRepRegistration) {
+    return <ConfirmDRepRegistrationContainer signTxData={signTxData} errorMessage={errorMessage} />;
+  }
+  if (txType === TxType.DRepRetirement) {
+    return <ConfirmDRepRetirementContainer signTxData={signTxData} errorMessage={errorMessage} />;
+  }
+
+  return <DappTransactionContainer signTxData={signTxData} errorMessage={errorMessage} />;
+};
---
+++ apps/browser-extension-wallet/src/features/dapp/components/confirm-transaction/utils.ts
@@ -0,0 +1,144 @@
+import { Wallet } from '@lace/cardano';
+import { assetsBurnedInspector, assetsMintedInspector, createTxInspector } from '@cardano-sdk/core';
+import { CardanoTxOut } from '@src/types';
+import { RemoteApiPropertyType, exposeApi } from '@cardano-sdk/web-extension';
+import { UserPromptService } from '@lib/scripts/background/services';
+import { DAPP_CHANNELS } from '@src/utils/constants';
+import { runtime } from 'webextension-polyfill';
+import { of } from 'rxjs';
+import { sectionTitle, DAPP_VIEWS } from '../../config';
+
+const DAPP_TOAST_DURATION = 50;
+
+export enum TxType {
+  Send = 'Send',
+  Mint = 'Mint',
+  Burn = 'Burn',
+  DRepRegistration = 'DRepRegistration',
+  DRepRetirement = 'DRepRetirement'
+}
+
+export const getTitleKey = (txType: TxType): string => {
+  if (txType === TxType.DRepRegistration) {
+    return 'core.drepRegistration.title';
+  }
+
+  if (txType === TxType.DRepRetirement) {
+    return 'core.drepRetirement.title';
+  }
+
+  return sectionTitle[DAPP_VIEWS.CONFIRM_TX];
+};
+
+export const disallowSignTx = (close = false): void => {
+  exposeApi<Pick<UserPromptService, 'allowSignTx'>>(
+    {
+      api$: of({
+        async allowSignTx(): Promise<boolean> {
+          return Promise.reject();
+        }
+      }),
+      baseChannel: DAPP_CHANNELS.userPrompt,
+      properties: { allowSignTx: RemoteApiPropertyType.MethodReturningPromise }
+    },
+    { logger: console, runtime }
+  );
+  close && setTimeout(() => window.close(), DAPP_TOAST_DURATION);
+};
+
+export const allowSignTx = (): void => {
+  exposeApi<Pick<UserPromptService, 'allowSignTx'>>(
+    {
+      api$: of({
+        async allowSignTx(): Promise<boolean> {
+          return Promise.resolve(true);
+        }
+      }),
+      baseChannel: DAPP_CHANNELS.userPrompt,
+      properties: { allowSignTx: RemoteApiPropertyType.MethodReturningPromise }
+    },
+    { logger: console, runtime }
+  );
+};
+
+export const getTransactionAssetsId = (outputs: CardanoTxOut[]): Wallet.Cardano.AssetId[] => {
+  const assetIds: Wallet.Cardano.AssetId[] = [];
+  const assetMaps = outputs.map((output) => output.value.assets);
+  for (const asset of assetMaps) {
+    if (asset) {
+      for (const id of asset.keys()) {
+        !assetIds.includes(id) && assetIds.push(id);
+      }
+    }
+  }
+  return assetIds;
+};
+
+const isDRepRegistrationCertificate = (type: Wallet.Cardano.CertificateType) =>
+  type === Wallet.Cardano.CertificateType.RegisterDelegateRepresentative;
+
+const isDRepRetirementCertificate = (type: Wallet.Cardano.CertificateType) =>
+  type === Wallet.Cardano.CertificateType.UnregisterDelegateRepresentative;
+
+export const dRepRegistrationInspector = (
+  tx: Wallet.Cardano.Tx
+): Wallet.Cardano.RegisterDelegateRepresentativeCertificate | undefined =>
+  tx?.body?.certificates?.find(({ __typename }) => isDRepRegistrationCertificate(__typename)) as
+    | Wallet.Cardano.RegisterDelegateRepresentativeCertificate
+    | undefined;
+
+export const dRepRetirementInspector = (
+  tx: Wallet.Cardano.Tx
+): Wallet.Cardano.UnRegisterDelegateRepresentativeCertificate | undefined =>
+  tx?.body?.certificates?.find(({ __typename }) => isDRepRetirementCertificate(__typename)) as
+    | Wallet.Cardano.UnRegisterDelegateRepresentativeCertificate
+    | undefined;
+
+export const getTxType = (tx: Wallet.Cardano.Tx): TxType => {
+  const inspector = createTxInspector({
+    minted: assetsMintedInspector,
+    burned: assetsBurnedInspector,
+    dRepRegistration: dRepRegistrationInspector,
+    dRepRetirement: dRepRetirementInspector
+  });
+
+  const { minted, burned, dRepRegistration, dRepRetirement } = inspector(tx as Wallet.Cardano.HydratedTx);
+  const isMintTransaction = minted.length > 0;
+  const isBurnTransaction = burned.length > 0;
+
+  if (isMintTransaction) {
+    return TxType.Mint;
+  }
+
+  if (isBurnTransaction) {
+    return TxType.Burn;
+  }
+
+  if (dRepRegistration) {
+    return TxType.DRepRegistration;
+  }
+
+  if (dRepRetirement) {
+    return TxType.DRepRetirement;
+  }
+
+  return TxType.Send;
+};
+
+export const drepIDasBech32FromHash = (value: Wallet.Crypto.Hash28ByteBase16): Wallet.Cardano.DRepID =>
+  Wallet.Cardano.DRepID(Wallet.HexBlob.toTypedBech32('drep', Wallet.HexBlob(value)));
+
+export const pubDRepKeyToHash = async (
+  pubDRepKeyHex: Wallet.Crypto.Ed25519PublicKeyHex
+): Promise<Wallet.Crypto.Hash28ByteBase16> => {
+  const pubDRepKey = await Wallet.Crypto.Ed25519PublicKey.fromHex(pubDRepKeyHex);
+  const drepKeyHex = (await pubDRepKey.hash()).hex();
+  return Wallet.Crypto.Hash28ByteBase16.fromEd25519KeyHashHex(drepKeyHex);
+};
+
+export const getOwnRetirementMessageKey = (isOwnRetirement: boolean | undefined): string => {
+  if (isOwnRetirement === undefined) {
+    return '';
+  }
+  return isOwnRetirement ? 'core.drepRetirement.isOwnRetirement' : 'core.drepRetirement.isNotOwnRetirement';
+};
