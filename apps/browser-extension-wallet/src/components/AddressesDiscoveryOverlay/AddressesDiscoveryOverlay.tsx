9b92805fea99d1e344b55cc61d4d162244e62a6b
Szymon Masłowski <szymon.maslowski@iohk.io>
2023-11-03 17:14:15 +0100

    fix(extension): hide addresses discovery modal (#700)

--- apps/browser-extension-wallet/src/components/AddressesDiscoveryOverlay/AddressesDiscoveryOverlay.tsx
+++ apps/browser-extension-wallet/src/components/AddressesDiscoveryOverlay/AddressesDiscoveryOverlay.tsx
@@ -38,11 +38,13 @@ export const AddressesDiscoveryOverlay: FC = ({ children }) => {
   return (
     <>
       {children}
-      <WarningModal
-        header={t('addressesDiscovery.overlay.title')}
-        content={<Loader className={styles.loader} data-testid="hd-discovery-loader" />}
-        visible={initialHdDiscoveryCompleted && hdDiscoveryStatus === AddressesDiscoveryStatus.InProgress}
-      />
+      {initialHdDiscoveryCompleted && hdDiscoveryStatus === AddressesDiscoveryStatus.InProgress && (
+        <WarningModal
+          visible
+          header={t('addressesDiscovery.overlay.title')}
+          content={<Loader className={styles.loader} data-testid="hd-discovery-loader" />}
+        />
+      )}
     </>
   );
 };
