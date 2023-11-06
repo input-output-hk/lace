a5ed2a7f4fbcd4733c9204a93071ec0d91cc6cd7
Lucas <lucas.araujo@iohk.io>
2023-11-06 15:17:47 -0300

    feat(extension): [LW-8998] Add missing tracking points around dApp connector submission in Lace (#701)
    ---------
    
    Co-authored-by: Dominik Guzei <dominik.guzei@gmail.com>

--- apps/browser-extension-wallet/src/features/dapp/components/DappTransactionSuccess.tsx
+++ apps/browser-extension-wallet/src/features/dapp/components/DappTransactionSuccess.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@lace/common';
+import { Button, PostHogAction } from '@lace/common';
 import { Image } from 'antd';
 import Success from '../../../assets/icons/success-staking.svg';
 import styles from './Layout.module.scss';
+import { useAnalyticsContext } from '@providers';
+import { TX_CREATION_TYPE_KEY, TxCreationType } from '@providers/AnalyticsProvider/analyticsTracker';
 
 export const DappTransactionSuccess = (): React.ReactElement => {
+  const analytics = useAnalyticsContext();
   const { t } = useTranslation();
+
+  const onClose = async () => {
+    await analytics?.sendEventToPostHog(PostHogAction.SendAllDoneCloseClick, {
+      [TX_CREATION_TYPE_KEY]: TxCreationType.External
+    });
+    window.close();
+  };
+
+  useEffect(() => {
+    analytics?.sendEventToPostHog(PostHogAction.SendAllDoneView, {
+      [TX_CREATION_TYPE_KEY]: TxCreationType.External
+    });
+  }, [analytics]);
 
   return (
     <div data-testid="dapp-sign-tx-success" className={styles.noWalletContainer}>
@@ -20,11 +36,7 @@ export const DappTransactionSuccess = (): React.ReactElement => {
         </div>
       </div>
       <div className={styles.footer}>
-        <Button
-          data-testid="dapp-sign-tx-success-close-button"
-          className={styles.footerBtn}
-          onClick={() => window.close()}
-        >
+        <Button data-testid="dapp-sign-tx-success-close-button" className={styles.footerBtn} onClick={onClose}>
           {t('general.button.close')}
         </Button>
       </div>
