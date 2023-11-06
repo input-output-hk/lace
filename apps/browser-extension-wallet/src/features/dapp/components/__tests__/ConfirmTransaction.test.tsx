9d763cbe511aae379ceea3a911d3efbd3e74e804
Lucas Araujo <lucas.araujo@iohk.io>
2023-11-06 09:00:17 -0300

    fix: add analytics provider to test

--- apps/browser-extension-wallet/src/features/dapp/components/__tests__/ConfirmTransaction.test.tsx
+++ apps/browser-extension-wallet/src/features/dapp/components/__tests__/ConfirmTransaction.test.tsx
@@ -13,7 +13,7 @@ import { ConfirmTransaction } from '../ConfirmTransaction';
 import '@testing-library/jest-dom';
 import { I18nextProvider } from 'react-i18next';
 import { StoreProvider } from '@src/stores';
-import { AppSettingsProvider, DatabaseProvider, ViewFlowProvider } from '@src/providers';
+import { AnalyticsProvider, AppSettingsProvider, DatabaseProvider, ViewFlowProvider } from '@src/providers';
 import { APP_MODE_BROWSER } from '@src/utils/constants';
 import i18n from '@lib/i18n';
 import { BehaviorSubject } from 'rxjs';
@@ -70,9 +70,11 @@ const getWrapper =
       <AppSettingsProvider>
         <DatabaseProvider>
           <StoreProvider appMode={APP_MODE_BROWSER}>
-            <I18nextProvider i18n={i18n}>
-              <ViewFlowProvider viewStates={sendViewsFlowState}>{children}</ViewFlowProvider>
-            </I18nextProvider>
+            <AnalyticsProvider analyticsDisabled>
+              <I18nextProvider i18n={i18n}>
+                <ViewFlowProvider viewStates={sendViewsFlowState}>{children}</ViewFlowProvider>
+              </I18nextProvider>
+            </AnalyticsProvider>
           </StoreProvider>
         </DatabaseProvider>
       </AppSettingsProvider>
