8f7470a7afa930e714b8b7f16e184610cbc01661
xdzurman <kamil.dzurman@gmail.com>
2023-11-09 10:53:46 +0100

    feat(staking): introduce feature flags for HW wallets multidelegation

--- apps/browser-extension-wallet/src/hooks/useMultiDelegationEnabled.ts
+++ apps/browser-extension-wallet/src/hooks/useMultiDelegationEnabled.ts
@@ -1,1 +1,19 @@
-export const useMultiDelegationEnabled = (): boolean => process.env.USE_MULTI_DELEGATION_STAKING === 'true';
+import { Wallet } from '@lace/cardano';
+import { useWalletStore } from '@src/stores';
+import { useMemo } from 'react';
+
+export const useMultiDelegationEnabled = (): boolean => {
+  const { getKeyAgentType } = useWalletStore();
+
+  return useMemo(() => {
+    const keyAgentType = getKeyAgentType();
+    switch (keyAgentType) {
+      case Wallet.KeyManagement.KeyAgentType.Ledger:
+        return process.env.USE_MULTI_DELEGATION_STAKING_LEDGER === 'true';
+      case Wallet.KeyManagement.KeyAgentType.Trezor:
+        return process.env.USE_MULTI_DELEGATION_STAKING_TREZOR === 'true';
+      default:
+        return true;
+    }
+  }, [getKeyAgentType]);
+};
