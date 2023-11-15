601971a2184eba0a2acc4d07d362d7a5cc2b7476
Renan Ferreira <renan.ferreira@iohk.io>
2023-11-15 15:29:26 -0300

    refactor(ui): make profile picture rounded variant

--- packages/ui/src/design-system/profile-picture/user-profile.component.tsx
+++ packages/ui/src/design-system/profile-picture/user-profile.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import * as RadixUIAvatar from '@radix-ui/react-avatar';
+import classNames from 'classnames';
 
 import * as Text from '../typography';
 
@@ -11,6 +12,7 @@ interface Props {
   fallback: string;
   alt?: string;
   delayMs?: number;
+  radius?: 'circle' | 'rounded';
 }
 
 export const UserProfile = ({
@@ -18,8 +20,14 @@ export const UserProfile = ({
   fallback: letter,
   alt,
   delayMs = 600,
+  radius = 'circle',
 }: Readonly<Props>): JSX.Element => (
-  <RadixUIAvatar.Root className={cx.root}>
+  <RadixUIAvatar.Root
+    className={classNames(cx.root, {
+      [cx.rounded]: radius === 'rounded',
+      [cx.circle]: radius === 'circle',
+    })}
+  >
     <RadixUIAvatar.Image className={cx.image} src={imageSrc} alt={alt} />
     <RadixUIAvatar.Fallback delayMs={delayMs}>
       <Text.Body.Normal weight="$bold" className={cx.fallbackText}>
