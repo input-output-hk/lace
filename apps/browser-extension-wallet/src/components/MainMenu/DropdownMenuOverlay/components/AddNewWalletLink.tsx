a8426cb2e15a5c24fd6ac71e251b1d948954f8bf
Renan Ferreira <renan.ferreira@iohk.io>
2023-11-21 13:39:41 -0300

    feat(extension): add add new wallet option to profile dropdown

--- apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/AddNewWalletLink.tsx
+++ apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/AddNewWalletLink.tsx
@@ -4,28 +4,11 @@ import { Menu } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import styles from '../DropdownMenuOverlay.module.scss';
-// import { useAnalyticsContext } from '@providers';
-// import {
-//   MatomoEventActions,
-//   MatomoEventCategories,
-//   AnalyticsEventNames,
-//   PostHogAction
-// } from '@providers/AnalyticsProvider/analyticsTracker';
 
-const handleOnClicked = () => {
-  // analytics.sendEventToMatomo({
-  //   category: MatomoEventCategories.ADDRESS_BOOK,
-  //   action: MatomoEventActions.CLICK_EVENT,
-  //   name: isPopup
-  //     ? AnalyticsEventNames.AddressBook.VIEW_ADDRESSES_POPUP
-  //     : AnalyticsEventNames.AddressBook.VIEW_ADDRESSES_BROWSER
-  // });
-  // analytics.sendEventToPostHog(PostHogAction.UserWalletProfileAddressBookClick);
-};
+const handleOnClicked = (): void => void 0;
 
 export const AddNewWalletLink = (): React.ReactElement => {
   const { t } = useTranslation();
-  // const analytics = useAnalyticsContext();
 
   return (
     <Link to={walletRoutePaths.newWallet.home} onClick={handleOnClicked}>
