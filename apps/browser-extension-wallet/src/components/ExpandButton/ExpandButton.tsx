6684920971ba4a432cec1b40d54efd3158d9bf54
Renan Ferreira <renan.ferreira@iohk.io>
2023-11-23 10:30:21 -0300

    refactor(extension): update the expand button styling

--- apps/browser-extension-wallet/src/components/ExpandButton/ExpandButton.tsx
+++ apps/browser-extension-wallet/src/components/ExpandButton/ExpandButton.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ExpandIcon from '../../assets/icons/expand.component.svg';
+import classnames from 'classnames';
 
 import styles from './ExpandButton.module.scss';
 
 export const ExpandButton = ({ label, onClick }: { label: string; onClick: () => void }): React.ReactElement => (
-  <a onClick={onClick} href="#" className={styles.button} data-testid="expand-button">
+  <a
+    onClick={onClick}
+    href="#"
+    className={classnames(styles.button, {
+      [styles.multiWallet]: process.env.USE_MULTI_WALLET === 'true'
+    })}
+    data-testid="expand-button"
+  >
     <ExpandIcon className={styles.icon} />
     <span className={styles.text}>{label}</span>
   </a>
