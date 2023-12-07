c451a0804b4897bdada2f1befe2f62fdfe5ed201
Renan Ferreira <renan.ferreira@iohk.io>
2023-12-07 14:35:00 -0300

    refactor(extension): remove all done screen

--- apps/browser-extension-wallet/src/views/browser-view/features/multi-wallet/hardware-wallet/steps/AllDone.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AllDone as View } from '../../components';
-import { useHistory } from 'react-router-dom';
-import { walletRoutePaths } from '@routes';
-
-export const AllDone = (): JSX.Element => {
-  const history = useHistory();
-  return <View onFinish={() => history.push(walletRoutePaths.assets)} isHardwareWallet />;
-};
--- apps/browser-extension-wallet/src/views/browser-view/features/multi-wallet/restore-wallet/steps/AllDone.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { walletRoutePaths } from '@routes';
-import { AllDone as View } from '../../components';
-import { useHistory } from 'react-router-dom';
-
-export const AllDone = (): JSX.Element => {
-  const history = useHistory();
-  return <View onFinish={() => history.push(walletRoutePaths.assets)} />;
-};
