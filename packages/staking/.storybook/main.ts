edfaa6daf05a8df6a1c75b8139ffa8520d96c929
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2024-01-03 14:06:11 +0100

    chore(staking): update and fix storybook config

---
+++ packages/staking/.storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+import { join, dirname } from 'path';
+import { mergeConfig } from 'vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import svgrPlugin from 'vite-plugin-svgr';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('storybook-addon-pseudo-states'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  async viteFinal(cfg) {
+    return mergeConfig(cfg, {
+      plugins: [
+        svgrPlugin({
+          include: '**/*.svg',
+          svgrOptions: { icon: true, exportType: 'default' },
+        }),
+        vanillaExtractPlugin({
+          esbuildOptions: { loader: { '.css': 'empty' } },
+        }),
+      ],
+    });
+  },
+};
+
+export default config;
