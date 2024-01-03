78e80da21606a381f335a5fd8ec446d6a8ae24f7
Renan Ferreira <renan.ferreira@iohk.io>
2024-01-03 14:57:52 -0300

    chore(core): use storybook v6 preview API instead of v7

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -4,23 +4,20 @@ import 'normalize.css';
 import './theme.scss';
 import { ThemeColorScheme, ThemeProvider } from '@lace/ui';
 
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/
-      }
+export const preview = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
     }
-  },
-  decorators: [
-    (Story) => (
-      <ThemeProvider colorScheme={ThemeColorScheme.Light}>
-        <Story />
-      </ThemeProvider>
-    )
-  ]
+  }
 };
 
-export default preview;
+export const decorators = [
+  (Story) => (
+    <ThemeProvider colorScheme={ThemeColorScheme.Light}>
+      <Story />
+    </ThemeProvider>
+  )
+];
