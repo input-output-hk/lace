934c45201dc82fe54c24a100425a2f4e965cef7c
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2024-01-04 14:02:27 +0000

    refactor: update dev preview build to target dev envs

--- apps/browser-extension-wallet/webpack-utils.js
+++ apps/browser-extension-wallet/webpack-utils.js
@@ -8,7 +8,7 @@ const transformManifest = (content, mode) => {
     const manifest = JSON.parse(content.toString());
     manifest.name = manifest.name.replace('$WALLET_MANIFEST_NAME', process.env.WALLET_MANIFEST_NAME);
     if (process.env.BUILD_DEV_PREVIEW === 'true') {
-      manifest.version = `${manifest.version}.${Date.now()}`;
+      manifest.version = `${manifest.version}.${Date.getUTCMonth()}${Date.getUTCDate()}`;
     }
     manifest.content_security_policy.extension_pages = manifest.content_security_policy.extension_pages
       .replace(
