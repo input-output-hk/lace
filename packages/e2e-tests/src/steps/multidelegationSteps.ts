667e340fe739699a2369fbb07304f6b0d960994c
Janusz Janus <janusz.janus@iohk.io>
2024-01-10 17:16:21 +0100

    Add test automation - Staking - Manage staking 'Confirm new portfolio' button works correctly

--- packages/e2e-tests/src/steps/multidelegationSteps.ts
+++ packages/e2e-tests/src/steps/multidelegationSteps.ts
@@ -329,7 +329,7 @@ When(/^I click Manage button$/, async () => {
   await MultidelegationPage.clickManageButton();
 });
 
-Then(/^I see Manage delegation page$/, async () => {
+Then(/^I see Manage delegation drawer$/, async () => {
   await ManageStakingDrawerAssert.assertSeeManageStakingDrawer(true);
 });
 
@@ -387,3 +387,23 @@ Given(
     await ManageStakingDrawerAssert.assertSeeRemovePoolButtonTooltip(tooltipForPool);
   }
 );
+
+Then(/^I (see|don't see) "Confirm new portfolio" button$/, async (visible: 'see' | 'dont see') => {
+  await ManageStakingDrawerAssert.assertSeeConfirmNewPortfolioButton(visible === 'see');
+});
+
+Then(/^"Confirm new portfolio" button is (enabled|disabled)$/, async (isEnabled: 'enabled' | 'disabled') => {
+  await ManageStakingDrawerAssert.assertConfirmNewPortfolioButtonState(isEnabled === 'enabled');
+});
+
+When(/^I click (plus|minus) button for pool "(\d+)"$/, async (ratioButton: 'plus' | 'minus', poolNo: number) => {
+  await ManageStakingDrawer.clickRatioButtonForPool(ratioButton, poolNo);
+});
+
+When(/^I click "Confirm new portfolio" button$/, async () => {
+  await ManageStakingDrawer.clickConfirmNewPortfolioButton();
+});
+
+Then(/^I see Manage delegation drawer Confirmation page$/, async () => {
+  await StakingConfirmationDrawerAssert.assertSeeStakingConfirmationDrawer();
+});
