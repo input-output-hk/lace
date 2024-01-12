a7977396e2d45ad6048c6f708888d5e1b742d1be
Renan Ferreira <renan.ferreira@iohk.io>
2024-01-12 20:57:23 -0300

    fix(ui): apply linear gradient to icons

--- packages/icons/src/LaceGradientComponent.tsx
+++ packages/icons/src/LaceGradientComponent.tsx
@@ -1,4 +1,14 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
-const SvgLaceGradientcomponent = (props: SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" {...props} />;
+const SvgLaceGradientcomponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="1em" height="1em" {...props}>
+    <defs>
+      <linearGradient id="lace-gradient_component_svg__a" x1="0%" x2="100%" y1="0%" y2="0%">
+        <stop offset="-18.3%" stopColor="#FF92E1" />
+        <stop offset="118.89%" stopColor="#FDC300" />
+      </linearGradient>
+    </defs>
+    <rect width="100%" height="100%" fill="url(#lace-gradient_component_svg__a)" />
+  </svg>
+);
 export { SvgLaceGradientcomponent as ReactComponent };
