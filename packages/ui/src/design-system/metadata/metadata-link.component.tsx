b1100520818e0e6ee0806a8d7bc296e424bc1c92
vetalcore <vetalcore@gmail.com>
2024-01-24 11:29:51 +0200

    fix(extension): remove code duplications for metadata and metadata link components

--- packages/ui/src/design-system/metadata/metadata-link.component.tsx
+++ packages/ui/src/design-system/metadata/metadata-link.component.tsx
@@ -1,58 +1,31 @@
 import React from 'react';
 
-import { Flex } from '../flex';
-import { Grid, Cell } from '../grid';
 import { TextLink } from '../text-link';
-import { Tooltip } from '../tooltip';
-import * as Typography from '../typography';
 
-import * as cx from './metadata.css';
+import { MetadataBase } from './metadata.base';
 
-import type { OmitClassName } from '../../types';
+import type { Props as BaseProps } from './metadata.base';
 
-type Props = OmitClassName<'div'> & {
-  label: string;
+type Props = BaseProps & {
   text: string;
   url: string;
-  tooltip?: string;
 };
 
 export const MetadataLink = ({
-  label,
-  text,
   url,
-  tooltip,
+  text,
   ...props
 }: Readonly<Props>): JSX.Element => {
   return (
-    <Grid {...props} columns="$6">
-      <Cell colStart="$1" colEnd="$3">
-        {tooltip == undefined ? (
-          <Typography.Body.Normal className={cx.label}>
-            {label}
-          </Typography.Body.Normal>
-        ) : (
-          <Tooltip label={tooltip}>
-            <div>
-              <Typography.Body.Normal className={cx.label}>
-                {label}
-              </Typography.Body.Normal>
-            </div>
-          </Tooltip>
-        )}
-      </Cell>
-      <Cell colStart="$3" colEnd="$7">
-        <Flex justifyContent="flex-end" h="$fill">
-          <a
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ textDecoration: 'none' }}
-          >
-            <TextLink label={text} />
-          </a>
-        </Flex>
-      </Cell>
-    </Grid>
+    <MetadataBase {...props}>
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ textDecoration: 'none' }}
+      >
+        <TextLink label={text} />
+      </a>
+    </MetadataBase>
   );
 };
--- packages/ui/src/design-system/metadata/metadata.component.tsx
+++ packages/ui/src/design-system/metadata/metadata.component.tsx
@@ -1,48 +1,20 @@
 import React from 'react';
 
-import { Flex } from '../flex';
-import { Grid, Cell } from '../grid';
-import { Tooltip } from '../tooltip';
 import * as Typography from '../typography';
 
+import { MetadataBase } from './metadata.base';
 import * as cx from './metadata.css';
 
-import type { OmitClassName } from '../../types';
+import type { Props as BaseProps } from './metadata.base';
 
-type Props = OmitClassName<'div'> & {
-  label: string;
+type Props = BaseProps & {
   text: string;
-  tooltip?: string;
 };
 
-export const Metadata = ({
-  label,
-  text,
-  tooltip,
-  ...props
-}: Readonly<Props>): JSX.Element => {
+export const Metadata = ({ text, ...props }: Readonly<Props>): JSX.Element => {
   return (
-    <Grid {...props} columns="$6">
-      <Cell colStart="$1" colEnd="$3">
-        {tooltip == undefined ? (
-          <Typography.Body.Normal className={cx.label}>
-            {label}
-          </Typography.Body.Normal>
-        ) : (
-          <Tooltip label={tooltip}>
-            <div>
-              <Typography.Body.Normal className={cx.label}>
-                {label}
-              </Typography.Body.Normal>
-            </div>
-          </Tooltip>
-        )}
-      </Cell>
-      <Cell colStart="$3" colEnd="$7">
-        <Flex justifyContent="flex-end" h="$fill">
-          <Typography.Address className={cx.text}>{text}</Typography.Address>
-        </Flex>
-      </Cell>
-    </Grid>
+    <MetadataBase {...props}>
+      <Typography.Address className={cx.text}>{text}</Typography.Address>
+    </MetadataBase>
   );
 };
