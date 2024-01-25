f48e2d200f5f43039f7be43da3d806e1cf0a1c4c
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2024-01-25 16:19:32 +0200

    test(extension): increase e2e timeout duration

--- packages/e2e-tests/src/assert/topNavigationAssert.ts
+++ packages/e2e-tests/src/assert/topNavigationAssert.ts
@@ -22,7 +22,7 @@ class TopNavigationAssert {
   }
 
   async assertLogoPresent() {
-    await MenuHeader.logo.waitForDisplayed({ timeout: 60_000 });
+    await MenuHeader.logo.waitForDisplayed({ timeout: 160_000 });
   }
 
   async assertSeeMenuButton() {
@@ -80,7 +80,7 @@ class TopNavigationAssert {
     expectedStatus = (await t(expectedStatus)) ?? expectedStatus;
     await MenuHeader.menuUserDetailsButton.waitForDisplayed();
     await browser.waitUntil(async () => (await MenuHeader.menuWalletStatus.getText()) === expectedStatus, {
-      timeout: 80_000,
+      timeout: 180_000,
       interval: 500,
       timeoutMsg: `expected sync status: ${expectedStatus} was not displayed`
     });
