e948003e97fab74fefc3b9200a81af04dd171f27
Tomislav Horaček <tomislav@tt-media.hr>
2024-02-01 11:30:30 +0100

    chore: improve staking helper

--- apps/browser-extension-wallet/src/views/browser-view/features/staking/helpers.ts
+++ apps/browser-extension-wallet/src/views/browser-view/features/staking/helpers.ts
@@ -3,13 +3,13 @@ import { Wallet } from '@lace/cardano';
 import * as HardwareLedger from '../../../../../../../node_modules/@cardano-sdk/hardware-ledger/dist/cjs';
 import * as HardwareTrezor from '../../../../../../../node_modules/@cardano-sdk/hardware-trezor/dist/cjs';
 
-export enum LedgerMultidelegationAppVersion {
+export enum LedgerMultidelegationMinAppVersion {
   MAJOR = 6,
   MINOR = 1,
   PATCH = 2
 }
 
-export enum TrezorMultidelegationFirmwareVersion {
+export enum TrezorMultidelegationFirmwareMinVersion {
   MAJOR = 2,
   MINOR = 6,
   PATCH = 4
@@ -18,22 +18,26 @@ export enum TrezorMultidelegationFirmwareVersion {
 export const isMultidelegationSupportedByDevice = async (
   keyAgentType: Exclude<Wallet.KeyManagement.KeyAgentType, Wallet.KeyManagement.KeyAgentType.InMemory>
 ): Promise<boolean> => {
-  if (keyAgentType === Wallet.KeyManagement.KeyAgentType.Ledger) {
-    const ledgerInfo = await HardwareLedger.LedgerKeyAgent.getAppVersion(Wallet.KeyManagement.CommunicationType.Web);
-    return (
-      ledgerInfo.version.major >= LedgerMultidelegationAppVersion.MAJOR &&
-      ledgerInfo.version.minor >= LedgerMultidelegationAppVersion.MINOR &&
-      ledgerInfo.version.patch >= LedgerMultidelegationAppVersion.PATCH
-    );
-  } else if (keyAgentType === Wallet.KeyManagement.KeyAgentType.Trezor) {
-    const trezorInfo = await HardwareTrezor.TrezorKeyAgent.checkDeviceConnection(
-      Wallet.KeyManagement.CommunicationType.Web
-    );
-    return (
-      trezorInfo.major_version >= TrezorMultidelegationFirmwareVersion.MAJOR &&
-      trezorInfo.minor_version >= TrezorMultidelegationFirmwareVersion.MINOR &&
-      trezorInfo.patch_version >= TrezorMultidelegationFirmwareVersion.PATCH
-    );
+  switch (keyAgentType) {
+    case Wallet.KeyManagement.KeyAgentType.Ledger: {
+      const ledgerInfo = await HardwareLedger.LedgerKeyAgent.getAppVersion(Wallet.KeyManagement.CommunicationType.Web);
+      return (
+        ledgerInfo.version.major >= LedgerMultidelegationMinAppVersion.MAJOR &&
+        ledgerInfo.version.minor >= LedgerMultidelegationMinAppVersion.MINOR &&
+        ledgerInfo.version.patch >= LedgerMultidelegationMinAppVersion.PATCH
+      );
+    }
+    case Wallet.KeyManagement.KeyAgentType.Trezor: {
+      const trezorInfo = await HardwareTrezor.TrezorKeyAgent.checkDeviceConnection(
+        Wallet.KeyManagement.CommunicationType.Web
+      );
+      return (
+        trezorInfo.major_version >= TrezorMultidelegationFirmwareMinVersion.MAJOR &&
+        trezorInfo.minor_version >= TrezorMultidelegationFirmwareMinVersion.MINOR &&
+        trezorInfo.patch_version >= TrezorMultidelegationFirmwareMinVersion.PATCH
+      );
+    }
+    default:
+      return true;
   }
-  return true;
 };
