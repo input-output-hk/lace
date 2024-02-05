f4eb167e23514bce7abd192cd7945e20e5be5951
Lucas Araujo <lucas.araujo@iohk.io>
2024-02-05 15:21:24 -0300

    refactor(ui): export

--- packages/ui/src/design-system/auto-suggest-box/index.ts
+++ packages/ui/src/design-system/auto-suggest-box/index.ts
@@ -1,1 +1,11 @@
-export { AutoSuggestBox } from './auto-suggest-box.component';
+export { AutoSuggestBox as Root } from './auto-suggest-box.component';
+export {
+  PickedSuggestionClassic,
+  PickedSuggestion3Item,
+  Suggestion3Item,
+  SuggestionClassic,
+} from './auto-suggest-box-suggestion.component';
+export type {
+  SuggestionClassicType,
+  Suggestion3ItemType,
+} from './auto-suggest-box-types';
--- packages/ui/src/design-system/index.ts
+++ packages/ui/src/design-system/index.ts
@@ -37,4 +37,4 @@ export { RadioButtonGroup } from './radio-button';
 export { SelectGroup } from './select';
 export { ActionCard } from './action-card';
 export { Loader } from './loader';
-export { AutoSuggestBox } from './auto-suggest-box';
+export * as AutoSuggestBox from './auto-suggest-box';
