f624a9d2d026780576c507271a14870541b5f863
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2024-02-05 16:31:40 +0100

    chore(core): fix storybook build

--- packages/core/.storybook/main.js
+++ packages/core/.storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -19,6 +21,53 @@ module.exports = {
   webpackFinal: (config) => {
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test('.svg'));
     fileLoaderRule.exclude = /\.svg$/;
+
+    const jsRuleIndex = config.module.rules.findIndex((rule) => rule.test?.test('.js'));
+
+    // Please refer to the apps/browser-extension-wallet/webpack.common.js
+    config.module.rules[jsRuleIndex] = {
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules\/(?!(@cardano-sdk)\/).*/,
+      loader: 'swc-loader',
+      options: {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true
+          },
+          target: 'es2019',
+          loose: false
+        }
+      },
+      resolve: {
+        fullySpecified: false
+      }
+    };
+
+    // Please refer to the apps/browser-extension-wallet/webpack.common.js
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+        https: require.resolve('https-browserify'),
+        http: require.resolve('stream-http'),
+        'get-port-please': false,
+        net: false,
+        fs: false,
+        os: false,
+        path: false,
+        events: require.resolve('events/'),
+        buffer: require.resolve('buffer/'),
+        stream: require.resolve('readable-stream'),
+        crypto: require.resolve('crypto-browserify'),
+        constants: require.resolve('constants-browserify'),
+        zlib: require.resolve('browserify-zlib'),
+        dns: false,
+        tls: false,
+        child_process: false
+      },
+      plugins: [new TsconfigPathsPlugin({ configFile: 'src/tsconfig.json' })]
+    };
 
     config.module.rules.push({
       test: /\.svg$/i,
