29cdec25c984b80a457e7a20e14c9a171a981f34
vetalcore <vetalcore@gmail.com>
2024-02-07 13:30:56 +0200

    feat(staking): migrate table component (#845)
    * feat(extension): lw-9270 new stake pool table
    
    * feat(extension): lw-9270 add feeature toggle for ros column
    
    * feat(extension): lw-9270 apply compact notation onto stake pool transformer
    
    * feat(extension): lw-9270 new stake pool table #2
    
    * feat(extension): unselect stake pool from portfolio
    
    * feat(extension): adjust single pool delegation flow
    
    * feat(extension): add tickets numbers into the todos comment
    
    * fix(extension): lw-9270 add compact notation for cost and pledge
    
    * fix(extension): lw-9270 resolve pr comments
    
    * fix(extension): lw-9270 resolve pr comments
    
    * fix(extension): lw-9270 change max pool size tooltip position
    
    * fix(extension): lw-9270 use name field to sort by default, use pool tx mapper from staking package
    
    * fix(extension): lw-9270 fix live stake mapping
    
    * fix(extension): lw-9270 adjust stake pools total count based on selected items
    
    * fix(extension): lw-9270 fix tests
    
    * fix(extension): lw-9270 use checkbox component from ui package
    
    * fix(extension): colours duplication in ui package
    
    * fix(extension): fix liveStake nullish values
    
    * fix(extension): resolved sded comments
    
    * fix(extension): resolved sonarcloud issues
    
    * fix(extension): remove focus from checkboxes
    
    * feat(staking): migrate table component
    
    * fix(extension): fix tests
    
    * fix(extension): fix tests
    
    * fix(extension): calculate initial items count
    
    * fix(extension): align checkbox wrapper
    
    * fix(extension): workaround for selected columns
    
    * fix(extension): remove table body leftovers
    
    * fix(extension): move table component into the ui package
    
    * feat(ui): add table story
    
    * fix(extension): resolve pr comments
    
    * fix(extension): fix initial items loading
    
    * fix(extension): allow columns to overflow
    
    * fix(extension): fix table key prop warning
    
    * fix(extension): resolve pr comments
    
    * fix(extension): resolve pr comments
    
    * fix(extension): use sx for styling
    
    * fix(extension): unify mappers
    
    * fix(extension): fix ChildSprinkleProps mismatch for flex wrapped with forwardRef
    
    * fix(extension): fix table story
    
    * fix(extension): update jest to support bigint serialization
    
    * fix(extension): resolve sonarcloude issues
    
    * fix(extension): resolve sdet comments
    
    * fix(extension): resolve sdet comments
    
    * fix(extension): change row height to avoid overlapping

--- packages/staking/src/features/BrowsePools/StakePoolsTable/StakePoolsTable.tsx
+++ packages/staking/src/features/BrowsePools/StakePoolsTable/StakePoolsTable.tsx
@@ -1,15 +1,13 @@
-import { Wallet } from '@lace/cardano';
-import { PostHogAction, Search, getRandomIcon } from '@lace/common';
+import { PostHogAction, Search } from '@lace/common';
 import { Box } from '@lace/ui';
 import debounce from 'lodash/debounce';
-import uniqBy from 'lodash/uniqBy';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StateStatus, useOutsideHandles } from '../../outside-handles-provider';
 import { mapStakePoolToDisplayData, useDelegationPortfolioStore } from '../../store';
 import * as styles from './StakePoolsTable.css';
 import { StakePoolsTableEmpty } from './StakePoolsTableEmpty/StakePoolsTableEmpty';
-import { StakePoolTableBrowser } from './StakePoolTableBrowser/StakePoolTableBrowser';
+import { StakePoolTableBrowser, StakePoolTableBrowserProps } from './StakePoolTableBrowser/StakePoolTableBrowser';
 import { SortDirection, SortField, StakePoolSortOptions } from './types';
 
 type StakePoolsTableProps = {
@@ -22,29 +20,20 @@ const DEFAULT_SORT_OPTIONS: StakePoolSortOptions = {
 };
 
 const searchDebounce = 300;
-const defaultFetchLimit = 100;
 
 export const StakePoolsTable = ({ scrollableTargetId }: StakePoolsTableProps) => {
+  const componentRef = useRef<HTMLDivElement | null>(null);
   const { t } = useTranslation();
   const [searchValue, setSearchValue] = useState<string>('');
-  const [isSearching, setIsSearching] = useState<boolean>(true);
-  const [isLoadingList, setIsLoadingList] = useState<boolean>(true);
   const [sort, setSort] = useState<StakePoolSortOptions>(DEFAULT_SORT_OPTIONS);
-  const [stakePools, setStakePools] = useState<Wallet.StakePoolSearchResults['pageResults']>([]);
-  const [skip, setSkip] = useState<number>(0);
   const selectedPortfolioStakePools = useDelegationPortfolioStore((store) =>
     store.selectedPortfolio.map(({ stakePool }) => stakePool)
   );
   const {
     currentChain,
-    walletStoreStakePoolSearchResults: {
-      pageResults,
-      totalResultCount,
-      skip: searchSkip = 0,
-      searchQuery,
-      searchFilters,
-    },
+    walletStoreStakePoolSearchResults: { pageResults, totalResultCount },
     walletStoreStakePoolSearchResultsStatus,
+    walletStoreResetStakePools: resetStakePools,
     walletStoreFetchStakePools: fetchStakePools,
     analytics,
   } = useOutsideHandles();
@@ -65,31 +54,22 @@ export const StakePoolsTable = ({ scrollableTargetId }: StakePoolsTableProps) =>
   const debouncedSearch = useMemo(() => debounce(fetchStakePools, searchDebounce), [fetchStakePools]);
 
   useEffect(() => {
-    // Fetch pools on mount, network switching, searchValue change and sort change
-    setIsLoadingList(true);
-    debouncedSearch({ searchString: searchValue, sort });
-  }, [currentChain, searchValue, sort, debouncedSearch]);
+    if (componentRef?.current) {
+      // reset pools on network switching, searchValue change and sort change
+      resetStakePools?.();
+    }
+  }, [currentChain, searchValue, sort, debouncedSearch, resetStakePools]);
 
-  const loadMoreData = () => fetchStakePools({ searchString: searchValue, skip: skip + defaultFetchLimit, sort });
-
-  useEffect(() => {
-    // Check query parameters to see if it's making a new search
-    const queryMatches = searchQuery === searchValue;
-    const filterMatch = searchFilters?.field === sort?.field && searchFilters?.order === sort?.order;
-    setIsSearching(!queryMatches || !filterMatch);
-  }, [searchQuery, searchFilters, searchValue, sort]);
-
-  useEffect(() => {
-    // Update stake pool list and new offset position
-    setStakePools((prevPools: Wallet.StakePoolSearchResults['pageResults']) =>
-      searchSkip === 0 ? pageResults : [...prevPools, ...pageResults]
-    );
-    setSkip(searchSkip);
-    setIsLoadingList(false);
-  }, [pageResults, searchSkip]);
+  const loadMoreData = useCallback(
+    ({ startIndex, endIndex }: Parameters<StakePoolTableBrowserProps['loadMoreData']>[0]) => {
+      if (startIndex !== endIndex) {
+        debouncedSearch({ limit: endIndex, searchString: searchValue, skip: startIndex, sort });
+      }
+    },
+    [debouncedSearch, searchValue, sort]
+  );
 
   const onSearch = (searchString: string) => {
-    setIsSearching(true);
     const startedTyping = searchValue === '' && searchString !== '';
     if (startedTyping) {
       analytics.sendEventToPostHog(PostHogAction.StakingBrowsePoolsSearchClick);
@@ -97,46 +77,19 @@ export const StakePoolsTable = ({ scrollableTargetId }: StakePoolsTableProps) =>
     setSearchValue(searchString);
   };
 
-  // imitates apps/browser-extension-wallet/src/stores/slices/stake-pool-search-slice.ts
-  const naiveSelectedPoolsSearch = (searchString: string, pools: Wallet.Cardano.StakePool[]) => {
-    const lowerCaseSearchString = searchString.toLowerCase();
-    return pools.filter(
-      (pool) =>
-        pool.metadata?.name.toLowerCase().includes(lowerCaseSearchString) ||
-        pool.metadata?.ticker.toLowerCase().includes(lowerCaseSearchString) ||
-        pool.id.toLowerCase() === lowerCaseSearchString
-    );
-  };
-
-  const combinedUnique = useMemo(() => {
-    const combinedStakePools = [
-      ...(searchValue
-        ? naiveSelectedPoolsSearch(searchValue, selectedPortfolioStakePools)
-        : selectedPortfolioStakePools),
-      ...stakePools,
-    ];
-    return uniqBy(combinedStakePools, (p) => p.id);
-  }, [stakePools, selectedPortfolioStakePools, searchValue]);
-
   const list = useMemo(
-    () =>
-      combinedUnique.map((pool) => {
-        const stakePool = mapStakePoolToDisplayData({ stakePool: pool });
-        const logo = getRandomIcon({ id: pool.id.toString(), size: 30 });
+    () => pageResults.map((pool) => (pool ? mapStakePoolToDisplayData({ stakePool: pool }) : undefined)),
 
-        return {
-          ...stakePool,
-          hexId: pool.hexId,
-          liveStake: `${stakePool.liveStake.number}${stakePool.liveStake.unit}`,
-          logo: stakePool.logo || logo,
-          stakePool: pool,
-        };
-      }) || [],
-    [combinedUnique]
+    [pageResults]
+  );
+
+  const selectedList = useMemo(
+    () => selectedPortfolioStakePools.map((pool) => mapStakePoolToDisplayData({ stakePool: pool })),
+    [selectedPortfolioStakePools]
   );
 
   return (
-    <Box className={styles.stakePoolsTable} data-testid="stake-pool-table">
+    <Box ref={componentRef} className={styles.stakePoolsTable} data-testid="stake-pool-table">
       <Search
         withSearchIcon
         inputPlaceholder={t('browsePools.stakePoolTableBrowser.searchInputPlaceholder')}
@@ -144,17 +97,12 @@ export const StakePoolsTable = ({ scrollableTargetId }: StakePoolsTableProps) =>
         data-testid="search-input"
         loading={fetchingPools}
       />
-      <Box mt="$10">
+      <Box mt="$10" mb="$112">
         <StakePoolTableBrowser
-          items={list}
+          emptyPlaceholder={!fetchingPools && totalResultCount === 0 && <StakePoolsTableEmpty />}
+          selectedPools={selectedList}
+          pools={list}
           loadMoreData={loadMoreData}
-          locale={{ emptyText: true }}
-          // TODO: there are too many loading states and it's confusing, we should refactor this and reduce them
-          // do not show loader if we are already searching/filtering
-          total={isSearching ? 0 : totalResultCount}
-          emptyPlaceholder={!fetchingPools && !isSearching && <StakePoolsTableEmpty />}
-          // Show skeleton if it's loading the list while a search is not being performed
-          showSkeleton={isLoadingList && !isSearching}
           scrollableTargetId={scrollableTargetId}
           translations={tableHeaderTranslations}
           activeSort={sort}
---
+++ packages/ui/src/design-system/table/table-header.component.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { IconButton } from '@lace/ui';
+import { Tooltip } from 'antd';
+import cn from 'classnames';
+
+import * as cx from './table.css';
+
+export interface Headers<Columns> {
+  label: string;
+  value: Columns;
+  tooltipText?: string;
+}
+
+export interface HeaderProps<Columns> {
+  isActiveSortItem: (value: string) => boolean;
+  isSortingAvailable: (value: string) => boolean;
+  onSortChange: (field: Columns) => void;
+  order: 'asc' | 'desc';
+  withSelection?: boolean;
+  dataTestId?: string;
+  headers: Headers<Columns>[];
+}
+
+export const Header = <T extends string>({
+  headers,
+  isActiveSortItem,
+  isSortingAvailable,
+  onSortChange,
+  order,
+  withSelection = false,
+  dataTestId = 'table',
+}: Readonly<HeaderProps<T>>): React.ReactElement => (
+  <div
+    data-testid={`${dataTestId}-list-header`}
+    className={cn(cx.row, cx.header, {
+      [cx.selectable]: withSelection,
+    })}
+  >
+    {/* +1 column in case all the rows have selection available (checkbox as an additional first column) */}
+    {withSelection && <div />}
+    {headers.map(({ label, value, tooltipText }) => (
+      <div
+        className={cn(cx.cell, cx.headerItem, {
+          [cx.withAction]: isSortingAvailable(value),
+          [cx.active]: isActiveSortItem(value),
+        })}
+        key={value}
+        onClick={(): void => {
+          onSortChange(value);
+        }}
+        data-testid={`${dataTestId}-list-header-${value}`}
+      >
+        <span className={cx.cellInner}>
+          <span className={cx.headerItemInner}>
+            <Tooltip destroyTooltipOnHide title={tooltipText}>
+              {label}
+            </Tooltip>
+          </span>
+          {isSortingAvailable(value) && isActiveSortItem(value) && (
+            <IconButton.Caret
+              direction={order}
+              data-testid={`${dataTestId}-sort-order-${order}`}
+            />
+          )}
+        </span>
+      </div>
+    ))}
+  </div>
+);
