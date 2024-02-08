f2da17ff6b6e5fb11bcf2c980024f5b2acb83a89
Tom Mayel <tom.mayel@iohk.io>
2024-02-08 18:15:12 +0000

    feat(staking): sort and filter fix border

--- packages/staking/src/features/BrowsePools/SortAndFilter/SortAndFilter.css.ts
+++ packages/staking/src/features/BrowsePools/SortAndFilter/SortAndFilter.css.ts
@@ -2,20 +2,16 @@ import { style, sx } from '@lace/ui';
 import { theme } from 'features/theme';
 
 export const textBoxLeft = style({
-  borderBottomLeftRadius: 16,
   borderBottomRightRadius: 0,
   borderRightColor: theme.colors.$browsePoolsFilterInputRightBorderColor,
   borderRightStyle: 'solid',
-  borderRightWidth: 1.5,
-  borderTopLeftRadius: 16,
+  borderRightWidth: 2,
   borderTopRightRadius: 0,
 });
 
 export const textBoxRight = style({
   borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 16,
   borderTopLeftRadius: 0,
-  borderTopRightRadius: 16,
 });
 
 export const radioGroup = sx({
