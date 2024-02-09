1bce631b6d33ef7edfdb804d4c1db2ce862a1dac
Lucas <lucas.araujo@iohk.io>
2024-02-09 08:24:33 -0300

    [LW-9565] Disable account confirmation (#855)
    * feat: move edit account component to core package
    
    * feat: add account disable confirmation component

---
+++ packages/core/src/ui/components/Account/DisableAccountConfirmation/DisableAccountConfirmation.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Dialog } from '@lace/ui';
+
+interface Props {
+  open: boolean;
+  translations: {
+    title: string;
+    description: string;
+    cancel: string;
+    confirm: string;
+  };
+  onCancel: () => void;
+  onConfirm: () => void;
+  zIndex?: number;
+}
+
+export const DisableAccountConfirmation = ({
+  open,
+  onConfirm,
+  onCancel,
+  zIndex,
+  translations: { title, description, cancel, confirm }
+}: Props): JSX.Element => (
+  <Dialog.Root open={open} setOpen={() => void 0} zIndex={zIndex}>
+    <Dialog.Title>{title}</Dialog.Title>
+    <Dialog.Description>{description}</Dialog.Description>
+    <Dialog.Actions>
+      <Dialog.Action cancel label={cancel} onClick={onCancel} />
+      <Dialog.Action label={confirm} onClick={onConfirm} />
+    </Dialog.Actions>
+  </Dialog.Root>
+);
