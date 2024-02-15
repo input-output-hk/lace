23884bb07383b370864d4187147e27b2d93015c6
wklos-iohk <wojciech.klos@iohk.io>
2024-02-15 16:20:49 +0100

    test(extension): adjust memory

--- packages/e2e-tests/wdio.conf.base.ts
+++ packages/e2e-tests/wdio.conf.base.ts
@@ -14,7 +14,7 @@ export const config: WebdriverIO.Config = {
   logLevel: 'error',
   outputDir: 'logs',
   bail: 0,
-  execArgv: ['--max-old-space-size=8192'],
+  execArgv: ['--max-old-space-size=16384'],
   baseUrl: '',
   capabilities: [],
   injectGlobals: true,
