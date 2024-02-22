cc2daa4a6e4390937fafc2e92f116b136d344eb5
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-02-22 09:43:39 +0000

    fix: remove test-id

--- packages/ui/src/design-system/summary-expander/summary-expander.component.tsx
+++ packages/ui/src/design-system/summary-expander/summary-expander.component.tsx
@@ -47,12 +47,7 @@ export const SummaryExpander = ({
         </Typography.Body.Large>
 
         <Collapsible.Trigger asChild>
-          <Trigger
-            open={open}
-            disabled={disabled}
-            {...props}
-            data-testid="summary-expander-trigger"
-          />
+          <Trigger open={open} disabled={disabled} {...props} />
         </Collapsible.Trigger>
       </Flex>
 
