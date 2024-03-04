be63aace6b50adb310207c6b8f8eeea4d500c53c
januszjanus <janusz.janus@iohk.io>
2024-03-04 12:47:31 +0100

    test(extension): tweaks 3

--- packages/e2e-tests/wdio.conf.base.ts
+++ packages/e2e-tests/wdio.conf.base.ts
@@ -57,6 +57,6 @@ export const config: WebdriverIO.Config = {
     tags: extensionUtils.isMainnet() ? '@Mainnet' : '@Testnet',
     tagsInTitle: true,
     timeout: 200_000,
-    retry: 1
+    retry: 2
   } as WebdriverIO.CucumberOpts
 };
