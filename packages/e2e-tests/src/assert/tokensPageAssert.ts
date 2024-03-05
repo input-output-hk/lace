8fafde0f3585084755d189d7e6a225352a9e3078
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-03-05 09:00:37 +0000

    fix: debug e2e tests

--- packages/e2e-tests/src/assert/tokensPageAssert.ts
+++ packages/e2e-tests/src/assert/tokensPageAssert.ts
@@ -143,19 +143,25 @@ class TokensPageAssert {
       Number.parseFloat(subtractedAmount) +
       Number.parseFloat(fee);
     const expectedValueRounded = Number.parseFloat(expectedValue.toFixed(2));
-    const tokenBalance = await testContext.load(`${Asset.getByName(tokenName)?.ticker}tokenBalance`);
-    Logger.log(
-      `calculating balance with amount ${subtractedAmount} and fee ${fee} and starting balance ${tokenBalance}`
-    );
     Logger.log(`waiting for token: ${tokenName} with value: ${expectedValueRounded}`);
     await browser.waitUntil(
       async () => {
-        const valueOnPage = await TokensPage.getTokenBalanceAsFloatByName(tokenName);
-        Logger.log(`currentValue on page: ${valueOnPage}`);
+        const tokenValueAsFloat = await TokensPage.getTokenBalanceAsFloatByName(tokenName);
+        Logger.log(
+          `1. ${tokenValueAsFloat === expectedValueRounded + 0.01}, ${tokenValueAsFloat} - ${
+            expectedValueRounded + 0.01
+          }`
+        );
+        Logger.log(
+          `2. ${tokenValueAsFloat === expectedValueRounded - +0.01}, ${tokenValueAsFloat} - ${
+            expectedValueRounded - 0.01
+          }`
+        );
+        Logger.log(`3. ${tokenValueAsFloat === expectedValueRounded}, ${tokenValueAsFloat} - ${expectedValueRounded}`);
         return (
-          (await TokensPage.getTokenBalanceAsFloatByName(tokenName)) === expectedValueRounded + 0.01 ||
-          (await TokensPage.getTokenBalanceAsFloatByName(tokenName)) === expectedValueRounded - 0.01 ||
-          (await TokensPage.getTokenBalanceAsFloatByName(tokenName)) === expectedValueRounded
+          tokenValueAsFloat === expectedValueRounded + 0.01 ||
+          tokenValueAsFloat === expectedValueRounded - 0.01 ||
+          tokenValueAsFloat === expectedValueRounded
         );
       },
       {
