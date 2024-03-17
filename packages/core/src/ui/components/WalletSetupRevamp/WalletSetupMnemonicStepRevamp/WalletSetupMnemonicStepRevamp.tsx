88793eff5ac56df9bc712146e5dfcc00408198be
emiride <emir.hodzich@gmail.com>
2024-03-17 23:50:57 +0100

    test(extension): fixed onboarding tests

--- packages/core/src/ui/components/WalletSetupRevamp/WalletSetupMnemonicStepRevamp/WalletSetupMnemonicStepRevamp.tsx
+++ packages/core/src/ui/components/WalletSetupRevamp/WalletSetupMnemonicStepRevamp/WalletSetupMnemonicStepRevamp.tsx
@@ -141,7 +141,7 @@ export const WalletSetupMnemonicStepRevamp = ({
               {translations.copyToClipboard}
             </Button>
           ) : (
-            <Button type="link" onClick={() => pasteRecoveryPhrase()}>
+            <Button type="link" onClick={() => pasteRecoveryPhrase()} data-testid="paste-from-clipboard">
               {translations.pasteFromClipboard}
             </Button>
           )
