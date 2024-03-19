8feb1a1cb147f022d9591fdda26ca780153b496c
Lucas <lucas.araujo@iohk.io>
2024-03-19 10:36:17 -0300

    [LW-9871] Posthog events for multi wallet (#946)
    * feat: multi wallet events
    
    * feat: add wallet and account amount as user property
    
    * fix: mock analytics
    
    * fix: add missing dependency
    
    * refactor: update property name
    
    * fix: use correct passphrase event for recovery wallet flow
    
    ---------
    
    Co-authored-by: Renan Ferreira <renan.ferreira@iohk.io>

--- apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/WalletAccounts.tsx
+++ apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/WalletAccounts.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-handler-names */
 import React, { useCallback, useMemo } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
-import { NavigationButton, toast } from '@lace/common';
+import { NavigationButton, PostHogAction, toast } from '@lace/common';
 import styles from './WalletAccounts.module.scss';
 import { ProfileDropdown } from '@lace/ui';
 import { AccountData } from '@lace/ui/dist/design-system/profile-dropdown/accounts/profile-dropdown-accounts-list.component';
@@ -20,6 +20,8 @@ import { WalletType } from '@cardano-sdk/web-extension';
 import { Link } from 'react-router-dom';
 import { useBackgroundServiceAPIContext } from '@providers/BackgroundServiceAPI';
 import { BrowserViewSections } from '@lib/scripts/types';
+import { useAnalyticsContext } from '@providers';
+import { getWalletAccountsQtyString } from '@src/utils/get-wallet-count-string';
 
 const defaultAccountName = (accountNumber: number) => `Account #${accountNumber}`;
 
@@ -38,6 +40,7 @@ type EnableAccountHWSigningDialogData = {
 
 export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: () => void }): React.ReactElement => {
   const { t } = useTranslation();
+  const analytics = useAnalyticsContext();
   const accountsListLabel = useMemo(
     () => ({
       unlock: t('browserView.settings.wallet.accounts.unlockLabel'),
@@ -107,6 +110,7 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
 
   const activateAccount = useCallback(
     async (accountIndex: number) => {
+      analytics.sendEventToPostHog(PostHogAction.MultiWalletSwitchAccount);
       await activateWallet({
         walletId: wallet.walletId,
         accountIndex
@@ -114,7 +118,7 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
       const accountName = accountsData.find((acc) => acc.accountNumber === accountIndex)?.label;
       closeDropdownAndShowAccountActivated(accountName);
     },
-    [wallet.walletId, activateWallet, accountsData, closeDropdownAndShowAccountActivated]
+    [wallet.walletId, activateWallet, accountsData, closeDropdownAndShowAccountActivated, analytics]
   );
 
   const editAccount = useCallback(
@@ -146,6 +150,10 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
           accountIndex,
           metadata: { name }
         });
+        analytics.sendEventToPostHog(PostHogAction.MultiWalletEnableAccount, {
+          // eslint-disable-next-line camelcase
+          $set: { wallet_accounts_quantity: await getWalletAccountsQtyString(walletRepository) }
+        });
         clearTimeout(timeout);
         enableAccountHWSigningDialog.hide();
         closeDropdownAndShowAccountActivated(name);
@@ -153,7 +161,15 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
         showHWErrorState();
       }
     },
-    [addAccount, wallet, enableAccountHWSigningDialog, closeDropdownAndShowAccountActivated, showHWErrorState]
+    [
+      addAccount,
+      wallet,
+      enableAccountHWSigningDialog,
+      closeDropdownAndShowAccountActivated,
+      showHWErrorState,
+      analytics,
+      walletRepository
+    ]
   );
 
   const unlockAccount = useCallback(
@@ -187,13 +203,17 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
           passphrase,
           metadata: { name: defaultAccountName(accountIndex) }
         });
+        analytics.sendEventToPostHog(PostHogAction.MultiWalletEnableAccount, {
+          // eslint-disable-next-line camelcase
+          $set: { wallet_accounts_quantity: await getWalletAccountsQtyString(walletRepository) }
+        });
         enableAccountPasswordDialog.hide();
         closeDropdownAndShowAccountActivated(name);
       } catch {
         enableAccountPasswordDialog.setData({ ...enableAccountPasswordDialog.data, wasPasswordIncorrect: true });
       }
     },
-    [wallet, addAccount, enableAccountPasswordDialog, closeDropdownAndShowAccountActivated]
+    [wallet, addAccount, enableAccountPasswordDialog, closeDropdownAndShowAccountActivated, analytics, walletRepository]
   );
 
   const lockAccount = useCallback(async () => {
@@ -201,9 +221,12 @@ export const WalletAccounts = ({ isPopup, onBack }: { isPopup: boolean; onBack: 
       walletId: wallet.walletId,
       accountIndex: disableAccountConfirmation.data.accountNumber
     });
-
+    analytics.sendEventToPostHog(PostHogAction.MultiWalletDisableAccount, {
+      // eslint-disable-next-line camelcase
+      $set: { wallet_accounts_quantity: await getWalletAccountsQtyString(walletRepository) }
+    });
     disableAccountConfirmation.hide();
-  }, [walletRepository, disableAccountConfirmation, wallet.walletId]);
+  }, [walletRepository, disableAccountConfirmation, wallet.walletId, analytics]);
 
   const renameAccount = useCallback(
     async (newAccountName: string) => {
