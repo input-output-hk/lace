d666bc5382bc17f62915cf39c68f9e24b0180311
vetalcore <vetalcore@gmail.com>
2024-03-25 16:31:38 +0200

    fix(extension): load more stake pool grid items if all stake pools are unselected

--- packages/staking/src/features/BrowsePools/StakePoolsGrid/Grid.tsx
+++ packages/staking/src/features/BrowsePools/StakePoolsGrid/Grid.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-console */
 /* eslint-disable react/no-multi-comp */
 import { Flex, useVisibleItemsCount } from '@lace/ui';
-import { useEffect, useLayoutEffect, useRef } from 'react';
+import debounce from 'lodash/debounce';
+import { RefObject, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { ListRange, VirtuosoGrid, VirtuosoGridProps } from 'react-virtuoso';
+import useResizeObserver from 'use-resize-observer';
 import * as cx from './StakePoolsGrid.css';
 
 export type GridProps<T> = VirtuosoGridProps<T, null> & {
@@ -11,7 +14,10 @@ export type GridProps<T> = VirtuosoGridProps<T, null> & {
   rowHeight: number;
   numberOfItemsPerRow?: number;
   scrollableTargetId?: string;
+  parentRef: RefObject<HTMLDivElement>;
 };
+
+const DEFAULT_DEBOUNCE = 200;
 
 export const Grid = <T extends Record<string, unknown> | undefined>({
   itemContent,
@@ -20,9 +26,11 @@ export const Grid = <T extends Record<string, unknown> | undefined>({
   rowHeight,
   numberOfItemsPerRow,
   scrollableTargetId = '',
+  parentRef,
 }: GridProps<T>) => {
   const tableReference = useRef<HTMLDivElement | null>(null);
   const scrollableTargetReference = useRef<VirtuosoGridProps<T, undefined>['customScrollParent']>();
+  const [overscan, setOverscan] = useState({ main: 0, reverse: 0 });
 
   useLayoutEffect(() => {
     if (scrollableTargetId) {
@@ -41,10 +49,30 @@ export const Grid = <T extends Record<string, unknown> | undefined>({
     }
   }, [initialRowsCount, loadMoreData, numberOfItemsPerRow, rowHeight]);
 
+  const updateGridOverscan = useCallback(() => {
+    if (!tableReference?.current || !parentRef?.current) return;
+    // VirtuosoGrid won't render more items in case it's top position (visible items count) changes, force it manually via overscan value
+    const tableVsParentHeightDiff =
+      parentRef.current.getBoundingClientRect().height - tableReference.current.getBoundingClientRect().height;
+    setOverscan({ main: tableVsParentHeightDiff, reverse: 0 });
+  }, [parentRef]);
+
+  useLayoutEffect(() => {
+    updateGridOverscan();
+  }, [updateGridOverscan]);
+
+  const onResize = useMemo(
+    () => debounce(updateGridOverscan, DEFAULT_DEBOUNCE, { leading: true }),
+    [updateGridOverscan]
+  );
+
+  useResizeObserver<HTMLElement>({ onResize, ref: parentRef });
+
   return (
     <Flex h="$fill" ref={tableReference} data-testid="stake-pool-list-scroll-wrapper">
       {items.length > 0 && scrollableTargetReference.current && (
         <VirtuosoGrid<T>
+          overscan={overscan}
           listClassName={cx.grid}
           data={items}
           customScrollParent={scrollableTargetReference.current}
