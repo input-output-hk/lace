510b98aa6e2296e93b8b34103b93e82fe262332b
shawnbusuttil <shawn-buzu@hotmail.co.uk>
2024-03-26 14:46:26 +0000

    chore: fix lint issues

--- apps/browser-extension-wallet/src/views/browser-view/features/wallet-setup/components/WalletSetupWizard.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/wallet-setup/components/WalletSetupWizard.tsx
@@ -211,8 +211,7 @@ export const WalletSetupWizard = ({
     },
     [createWallet, mnemonic, analytics, setupType, goToMyWallet, moveForward]
   );
-  
-  
+
   const handleSubmit = async (result: { password: string; walletName: string }) => {
     sendAnalytics(postHogOnboardingActions[setupType]?.ENTER_WALLET);
     await handleCompleteCreation(result.walletName, result.password);
