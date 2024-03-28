d4991621eb3c7cf3dd7591c3ec2a7bbe27ec141d
Piotr Czeglik <piotr.czeglik@iohk.io>
2024-03-28 15:53:24 +0100

    chore: test release env

--- apps/browser-extension-wallet/webpack.app.prod.js
+++ apps/browser-extension-wallet/webpack.app.prod.js
@@ -2,25 +2,12 @@ const { merge } = require('webpack-merge');
 
 const prodConfig = require('./webpack.common.prod');
 const appConfig = require('./webpack.common.app');
-
-let processEnvs = process.env;
-console.log('--PROCESS ENVS--');
-console.log(processEnvs);
-
 require('dotenv-defaults').config({
   path: './.env',
   encoding: 'utf8',
-  defaults: process.env.BUILD_DEV_PREVIEW === 'true' ? './.env.developerpreview' : './.env.defaults',
-  overwrite: true
+  defaults: process.env.BUILD_DEV_PREVIEW === 'true' ? './.env.developerpreview' : './.env.defaults'
 });
 
-if (processEnvs.CI === 'true') {
-  process.env = {
-    ...process.env,
-    ...processEnvs
-  };
-  console.log('--PROCESS ENVS 2--');
-  console.log(process.env);
-}
+console.log(process.env);
 
 module.exports = () => merge(prodConfig(), appConfig());
