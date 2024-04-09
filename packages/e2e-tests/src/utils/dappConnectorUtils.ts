21e9f79a5218fda652ff51eabc4bf6234162411c
bslabiak <112852128+bslabiak@users.noreply.github.com>
2024-04-09 13:45:10 +0200

    test(extension): LW-10167 add from/to section dapp tx and new ui elem… (#1011)
    * test(extension): lw-10167 add from/to section dapp tx and new ui elements for test automation
    
    * test(extension): remove additional div
    
    * test(extension): remove additional div from second expander
    
    * test(extension): fix after comments
    
    * test(extension): extract methods to utils file
    
    * test(extension): fix parsing method
    
    * test(extension): fix test after translation changes

---
+++ packages/e2e-tests/src/utils/dappConnectorUtils.ts
@@ -0,0 +1,27 @@
+import ConfirmTransactionPage from '../elements/dappConnector/confirmTransactionPage';
+import { getTestWallet } from '../support/walletConfiguration';
+
+const subtractFeeFromCucumberListElement = async (entry: string) => {
+  const fee = Number((await ConfirmTransactionPage.transactionFeeValueAda.getText()).split(' ')[0]);
+  const [amount, currency] = entry.split(' ');
+  const amountIncludingFee = (Number(amount) - fee).toFixed(2);
+  return `${amountIncludingFee} ${currency}`;
+};
+
+const shortenAddressFromCucumberListElement = (wallet: string) => {
+  const [addressLabel, walletValue] = wallet.split(' ');
+  const fullAddress = String(getTestWallet(walletValue).address);
+  return `${addressLabel} ${fullAddress.slice(0, 8)}...${fullAddress.slice(-8)}`;
+};
+
+export const parseDappCucumberAssetList = async (assetsList: string[]): Promise<string[]> =>
+  await Promise.all(
+    assetsList.map(async (asset) => {
+      if (asset.includes('- FEE')) {
+        return await subtractFeeFromCucumberListElement(asset);
+      } else if (asset.includes('Address')) {
+        return shortenAddressFromCucumberListElement(asset);
+      }
+      return asset;
+    })
+  );
