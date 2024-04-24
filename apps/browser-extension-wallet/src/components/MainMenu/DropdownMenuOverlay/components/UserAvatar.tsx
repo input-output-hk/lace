30324be89a9a02a39d5fae7ec1d0cf54cb83bc69
John Oshalusi <john.oshalusi@iohk.io>
2024-04-24 12:40:32 +0100

    feat: set nft as user profile avatar (#1048)

--- apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/UserAvatar.tsx
+++ apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/components/UserAvatar.tsx
@@ -3,25 +3,19 @@ import React from 'react';
 import cn from 'classnames';
 import styles from '../DropdownMenuOverlay.module.scss';
 import { Image } from 'antd';
-import { useGetHandles } from '@hooks';
-import { getAssetImageUrl } from '@src/utils/get-asset-image-url';
 
 interface UserAvatarProps {
   walletName: string;
   isPopup?: boolean;
+  avatar?: string;
 }
 
-export const UserAvatar = ({ walletName, isPopup }: UserAvatarProps): React.ReactElement => {
-  const [handle] = useGetHandles();
-  const handleImage = handle?.profilePic;
-
-  return (
-    <div className={cn(styles.userAvatar, { [styles.isPopup]: isPopup })} data-testid="user-avatar">
-      {handleImage ? (
-        <Image src={getAssetImageUrl(handleImage)} className={styles.userAvatarImage} preview={false} />
-      ) : (
-        <span>{walletName[0]?.toUpperCase()}</span>
-      )}
-    </div>
-  );
-};
+export const UserAvatar = ({ walletName, isPopup, avatar }: UserAvatarProps): React.ReactElement => (
+  <div className={cn(styles.userAvatar, { [styles.isPopup]: isPopup })} data-testid="user-avatar">
+    {avatar ? (
+      <Image src={avatar} className={styles.userAvatarImage} preview={false} />
+    ) : (
+      <span>{walletName[0]?.toUpperCase()}</span>
+    )}
+  </div>
+);
