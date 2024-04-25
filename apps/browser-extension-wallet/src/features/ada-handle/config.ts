60febca0b0694b545e7b82ed3be55394b8d9634e
Vanessa Rodriguez Cristobal <vanessa.cristobal@iohk.io>
2024-04-25 09:16:10 +0100

    feat: replace Koralabs handleProvider with SDK handleHttpProvider

--- apps/browser-extension-wallet/src/features/ada-handle/config.ts
+++ apps/browser-extension-wallet/src/features/ada-handle/config.ts
@@ -4,7 +4,7 @@ import { Wallet } from '@lace/cardano';
 export const ADA_HANDLE_POLICY_ID = Wallet.ADA_HANDLE_POLICY_ID;
 export const isAdaHandleEnabled = process.env.USE_ADA_HANDLE === 'true';
 export const HANDLE_SERVER_URLS: Record<Exclude<Cardano.NetworkMagics, Cardano.NetworkMagics.Sanchonet>, string> = {
-  [Cardano.NetworkMagics.Mainnet]: 'https://api.handle.me',
-  [Cardano.NetworkMagics.Preprod]: 'https://preprod.api.handle.me',
-  [Cardano.NetworkMagics.Preview]: 'https://preview.api.handle.me'
+  [Cardano.NetworkMagics.Mainnet]: process.env.ADA_HANDLE_URL_MAINNET,
+  [Cardano.NetworkMagics.Preprod]: process.env.ADA_HANDLE_URL_PREPROD,
+  [Cardano.NetworkMagics.Preview]: process.env.ADA_HANDLE_URL_PREVIEW
 };
--- apps/browser-extension-wallet/src/hooks/useHandleResolver.ts
+++ apps/browser-extension-wallet/src/hooks/useHandleResolver.ts
@@ -1,18 +1,20 @@
 import { useMemo } from 'react';
 import { useWalletStore } from '@src/stores';
-import { KoraLabsHandleProvider } from '@cardano-sdk/cardano-services-client';
-import { ADA_HANDLE_POLICY_ID, HANDLE_SERVER_URLS } from '@src/features/ada-handle/config';
+import { handleHttpProvider } from '@cardano-sdk/cardano-services-client';
+import { HandleProvider } from '@cardano-sdk/core';
+import { HANDLE_SERVER_URLS } from '@src/features/ada-handle/config';
+import { logger } from '@lib/wallet-api-ui';
 
-export const useHandleResolver = (): KoraLabsHandleProvider => {
+export const useHandleResolver = (): HandleProvider => {
   const {
     currentChain: { networkMagic }
   } = useWalletStore();
 
   return useMemo(() => {
     const serverUrl = HANDLE_SERVER_URLS[networkMagic as keyof typeof HANDLE_SERVER_URLS];
-    return new KoraLabsHandleProvider({
-      serverUrl,
-      policyId: ADA_HANDLE_POLICY_ID
+    return handleHttpProvider({
+      baseUrl: serverUrl,
+      logger
     });
   }, [networkMagic]);
 };
