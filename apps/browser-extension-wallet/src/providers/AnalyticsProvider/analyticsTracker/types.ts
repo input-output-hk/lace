cc27d084a25c390cd4806957647aed312ad94729
John Oshalusi <john.oshalusi@iohk.io>
2024-04-30 20:42:31 +0100

    feat: improve copy and paste mnemonic (#1094)
    * feat: improve copy and paste mnemonic
    
    * refactor(extension,core): improve TranslationsFor type, refactor MnemonicStep, fix clipoard bug
    
    * feat(extension,core): add copy and paste shortcuts for the mnemonic screens, reduce toasts duration
    
    * refactor(core): improve TransationsFor type
    
    * refactor(core): enhance useKeyboardShortcut hook and use for copy/paste shortcuts
    
    * refactor(extension,core): renamed hook, removed unnecesary type cast
    
    ---------
    
    Co-authored-by: Szymon Masłowski <szymon.maslowski@iohk.io>

--- apps/browser-extension-wallet/src/providers/AnalyticsProvider/analyticsTracker/types.ts
+++ apps/browser-extension-wallet/src/providers/AnalyticsProvider/analyticsTracker/types.ts
@@ -58,7 +58,9 @@ export type PostHogActionsKeys =
   | 'RECOVERY_PHRASE_INTRO_VIDEO_GOTIT_CLICK'
   | 'RECOVERY_PHRASE_COPY_TO_CLIPBOARD_CLICK'
   | 'RECOVERY_PHRASE_PASTE_FROM_CLIPBOARD_CLICK'
-  | 'RECOVERY_PASSPHRASE_VERIFICATION_NEXT_CLICK';
+  | 'RECOVERY_PASSPHRASE_VERIFICATION_NEXT_CLICK'
+  | 'RECOVERY_PHRASE_COPY_READ_MORE_CLICK'
+  | 'RECOVERY_PHRASE_PASTE_READ_MORE_CLICK';
 export type PostHogOnboardingActionsValueType = Partial<Record<PostHogActionsKeys, PostHogAction>>;
 export type PostHogOnboardingActionsType = Partial<Record<OnboardingFlows, PostHogOnboardingActionsValueType>>;
 export type PostHogPersonProperties = {
