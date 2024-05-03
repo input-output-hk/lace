1994bb168a01edd2c26099cb43658c4c1c966ab9
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2024-05-03 09:17:24 +0200

    fix(ui): item indicator vertical alignment in Select

--- packages/ui/src/design-system/select/select-item/select-item-indicator.component.tsx
+++ packages/ui/src/design-system/select/select-item/select-item-indicator.component.tsx
@@ -3,12 +3,14 @@ import React, { forwardRef } from 'react';
 import { ReactComponent as CheckIcon } from '@lace/icons/dist/CheckComponent';
 import * as Select from '@radix-ui/react-select';
 
+import * as cx from './select-item-indicator.css';
+
 /**
  * https://www.radix-ui.com/primitives/docs/components/select#itemindicator
  */
 export const ItemIndicator = forwardRef<HTMLSpanElement>(
   ({}, forwardReference) => (
-    <Select.ItemIndicator ref={forwardReference}>
+    <Select.ItemIndicator ref={forwardReference} className={cx.indicator}>
       <CheckIcon />
     </Select.ItemIndicator>
   ),
---
+++ packages/ui/src/design-system/select/select-item/select-item-indicator.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+
+export const indicator = style({
+  display: 'inline-flex',
+  flexShrink: 0,
+  alignItems: 'center',
+  justifyContent: 'center',
+});
