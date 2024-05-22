a103a02a7d515cb93146af894c1d84d8ae8f8d05
Przemysław Włodek <przem.wlodek.github@gmail.com>
2024-05-22 11:42:07 +0200

    feat(ui,core,staking): [LW-9594] refactor Select component + design updates (#1109)

---
+++ packages/ui/src/design-system/select/types.ts
@@ -0,0 +1,2 @@
+export type SelectVariant = 'grey' | 'outline' | 'plain';
+export type SelectAlign = 'bottom' | 'selected';
--- packages/ui/src/design-tokens/borders.data.ts
+++ packages/ui/src/design-tokens/borders.data.ts
@@ -1,3 +1,4 @@
 export const borders = {
   $button_default: '2px solid',
+  $focused_outline: `3px solid`,
 } as const;
