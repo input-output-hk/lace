338c648f8db62c2e2a277151dba067643fe1e946
Szymon Masłowski <szymon.maslowski@iohk.io>
2024-05-22 17:37:32 +0200

    feat: improve state type safety

--- apps/browser-extension-wallet/src/views/browser-view/features/shared-wallet/create-flow/SharedWalletCreationStore.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/shared-wallet/create-flow/SharedWalletCreationStore.tsx
@@ -4,12 +4,34 @@ import React, { Dispatch, ReactElement, ReactNode, createContext, useContext, us
 import { useHistory } from 'react-router-dom';
 import { SharedWalletCreationStep } from './types';
 
-type State = {
+type BaseState = {
+  step: SharedWalletCreationStep;
+};
+
+export type SupportingData = {
   activeWalletName: string;
   coSignersKeys: string[];
-  step: SharedWalletCreationStep;
   walletName: string;
 };
+
+// This type util forces to describe complete state - all props from the BaseState and SupportingData has to be specified, otherwise we have a TS error
+type MakeState<S extends BaseState & SupportingData> = BaseState & SupportingData & S;
+
+type StateSetup = MakeState<{
+  activeWalletName: string;
+  coSignersKeys: undefined;
+  step: SharedWalletCreationStep.Setup;
+  walletName: string;
+}>;
+
+type StateCoSigners = MakeState<{
+  activeWalletName: string;
+  coSignersKeys: string[];
+  step: SharedWalletCreationStep.CoSigners;
+  walletName: string;
+}>;
+
+type State = StateSetup | StateCoSigners;
 
 type Action = { type: 'next' } | { type: 'back' } | { type: 'walletNameChanged'; walletName: string };
 
@@ -29,7 +51,7 @@ export const useSharedWalletCreationStore = (): ContextValue => {
 
 const makeInitialState = (activeWalletName: string): State => ({
   activeWalletName,
-  coSignersKeys: ['', ''],
+  coSignersKeys: undefined,
   step: SharedWalletCreationStep.Setup,
   walletName: ''
 });
@@ -46,37 +68,44 @@ export const SharedWalletCreationStore = ({ children }: SharedWalletCreationStor
 
   const initialState = makeInitialState(activeWalletName);
   const [state, dispatch] = useReducer((prevState: State, action: Action): State => {
-    if (prevState.step === SharedWalletCreationStep.Setup) {
-      if (action.type === 'walletNameChanged') {
-        return {
-          ...prevState,
-          walletName: action.walletName
-        };
-      }
-      if (action.type === 'back') {
-        history.push(walletRoutePaths.sharedWallet.root);
+    switch (prevState.step) {
+      case SharedWalletCreationStep.Setup: {
+        if (action.type === 'walletNameChanged') {
+          return {
+            ...prevState,
+            walletName: action.walletName
+          };
+        }
+        if (action.type === 'back') {
+          // TODO: Side effects should be handled outside the reducer
+          history.push(walletRoutePaths.sharedWallet.root);
+          return prevState;
+        }
+        if (action.type === 'next') {
+          return {
+            ...prevState,
+            step: SharedWalletCreationStep.CoSigners
+          };
+        }
         return prevState;
       }
-      if (action.type === 'next') {
-        return {
-          ...prevState,
-          step: SharedWalletCreationStep.CoSigners
-        };
-      }
-    }
 
-    if (prevState.step === SharedWalletCreationStep.CoSigners) {
-      if (action.type === 'back') {
-        return {
-          ...prevState,
-          step: SharedWalletCreationStep.Setup
-        };
-      }
-      if (action.type === 'next') {
-        return {
-          ...prevState,
-          step: SharedWalletCreationStep.Quorum
-        };
+      case SharedWalletCreationStep.CoSigners: {
+        if (action.type === 'back') {
+          // TS error that the `coSignersKeys: undefined` is missing. Adding `coSignersKeys: []` also throws an error as undefined is expected instead of an []
+          return {
+            ...prevState,
+            step: SharedWalletCreationStep.Setup
+          };
+        }
+        if (action.type === 'next') {
+          return {
+            ...prevState,
+            // TS error that State does not expect Quorum as a step
+            step: SharedWalletCreationStep.Quorum
+          };
+        }
+        return prevState;
       }
     }
 
