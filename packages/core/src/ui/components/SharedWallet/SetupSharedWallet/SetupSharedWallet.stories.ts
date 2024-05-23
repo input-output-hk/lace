335ff0615bc3e8f12bbd7803acefa5043aae5edc
shawnbusuttil <shawn-buzu@hotmail.co.uk>
2024-05-23 12:14:04 +0100

    fix: stabilize storybook

--- packages/core/src/ui/components/SharedWallet/SetupSharedWallet/SetupSharedWallet.stories.ts
+++ packages/core/src/ui/components/SharedWallet/SetupSharedWallet/SetupSharedWallet.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { SetupSharedWallet } from './SetupSharedWallet';
-// import { ComponentProps } from 'react';
+import { ComponentProps } from 'react';
 
 const meta: Meta<typeof SetupSharedWallet> = {
   title: 'Shared Wallets/SetupSharedWallet',
@@ -14,33 +14,26 @@ const meta: Meta<typeof SetupSharedWallet> = {
 export default meta;
 type Story = StoryObj<typeof SetupSharedWallet>;
 
-// const noop = (): void => void 0;
+const noop = (): void => void 0;
 
-// const data: ComponentProps<typeof SetupSharedWallet> = {
-//   translations: {
-//     backButton: 'Back',
-//     nextButton: 'Next',
-//     subtitle: 'Choose a name to identify your new shared wallet and the select the wallet you want it linked to.',
-//     textBoxLabel: 'Shared wallet name',
-//     title: "Let's create your shared wallet"
-//   },
-//   data: {
-//     name: 'My Shared Wallet',
-//     isNextEnabled: true
-//   },
-//   events: {
-//     onBack: noop,
-//     onNext: noop,
-//     onNameChange: noop
-//   }
-// };
+const data: ComponentProps<typeof SetupSharedWallet> = {
+  activeWalletName: 'Wallet 1',
+  activeWalletAddress:
+    'addr_test1qz9kum802qxqf72ztg77a83j9lx2xle37v0wy2qprauqdw7d2yfye8mcz8jh6k86d5t7zx2f4z5n4twk0acn956zulusujyj9k',
+  onBack: noop,
+  onNext: noop,
+  onNameChange: noop
+};
 
 export const Overview: Story = {
-  args: {}
+  args: {
+    ...data
+  }
 };
 
 export const Disabled: Story = {
   args: {
+    ...data,
     data: {
       name: '',
       isNextEnabled: false
