629cbb7d58e1d832f11aa55f3a74fe57d2ff4aa4
Dominik Guzei <dominik.guzei@gmail.com>
2024-05-27 14:04:32 +0200

    feat(core): add nami migration updating wallet loading screen [LW-10495] (#7)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -44,3 +44,4 @@ export * from '@ui/components/WalletSetupRevamp';
 export * from '@ui/components/ListEmptyState';
 export * from '@ui/components/MidnightEventBanner';
 export * from '@ui/components/MidnightPreLaunchSettingsBanner';
+export * from '@ui/components/NamiMigrationUpdatingYourWallet';
---
+++ packages/core/src/ui/components/NamiMigrationUpdatingYourWallet/NamiMigrationUpdatingYourWallet.tsx
@@ -0,0 +1,16 @@
+import React, { VFC } from 'react';
+import { Card, Flex, Loader, Text } from '@lace/ui';
+import styles from './NamiMigrationUpdatingYourWallet.module.scss';
+import { useTranslation } from 'react-i18next';
+
+export const NamiMigrationUpdatingYourWallet: VFC = () => {
+  const { t } = useTranslation();
+  return (
+    <Card.Elevated className={styles.card}>
+      <Flex mt="$60" alignItems="center" gap="$148" flexDirection="column">
+        <Text.Heading>{t('core.nami.migration.updating.heading')}</Text.Heading>
+        <Loader />
+      </Flex>
+    </Card.Elevated>
+  );
+};
