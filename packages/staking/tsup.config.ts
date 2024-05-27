d8afe125ad8e19aa2a6ac26bbb6b04fb787c77fe
Renan Valentin <renan.ferreira@iohk.io>
2024-05-27 08:49:15 -0300

    chore: use short identifier for vanilla-extract (#1144)

--- packages/staking/tsup.config.ts
+++ packages/staking/tsup.config.ts
@@ -14,13 +14,13 @@ const tsupConfig = defineConfig([
     },
     entry: ['./src/index.ts'],
     esbuildPlugins: [
-      vanillaExtractPlugin({ esbuildOptions: { loader: { '.css': 'empty' } } }),
+      vanillaExtractPlugin({ esbuildOptions: { loader: { '.css': 'empty' } }, identifiers: 'short' }),
       // eslint-disable-next-line new-cap
       ScssModulesPlugin() as never,
       svgr({ icon: true, plugins: [svgrJsx] }),
     ],
     external: Object.keys(peerDependencies),
-    format: ['esm', 'cjs'],
+    format: ['cjs'],
     loader: {
       '.png': 'dataurl',
     },
