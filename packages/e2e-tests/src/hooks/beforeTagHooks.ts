dc55e6cee899101b657f011b28c562e12eff407f
wklos-iohk <wojciech.klos@iohk.io>
2024-06-04 10:22:41 +0200

    test(extension): add new wallet for multi account tests

--- packages/e2e-tests/src/hooks/beforeTagHooks.ts
+++ packages/e2e-tests/src/hooks/beforeTagHooks.ts
@@ -48,7 +48,7 @@ Before(
 
 Before(
   {
-    tags: '@AddressBook-extended or @Transactions-Extended or @Tokens-extended or @Staking-Extended or @LockWallet-extended or @Top-Navigation-Extended or @NFTs-Extended or @NFT-Folders-Extended or @SendTx-Bundles-Extended or @SendTx-Simple-Extended or @MainNavigation-Extended or @Send-Transaction-Metadata-Extended or @Settings-Extended or @DAppConnector or @DAppConnector-Extended or @Analytics-Settings-Extended or @Banxa-Extended or @Accounts-Extended'
+    tags: '@AddressBook-extended or @Transactions-Extended or @Tokens-extended or @Staking-Extended or @LockWallet-extended or @Top-Navigation-Extended or @NFTs-Extended or @NFT-Folders-Extended or @SendTx-Bundles-Extended or @SendTx-Simple-Extended or @MainNavigation-Extended or @Send-Transaction-Metadata-Extended or @Settings-Extended or @DAppConnector or @DAppConnector-Extended or @Analytics-Settings-Extended or @Banxa-Extended'
   },
   async () => {
     await extendedViewWalletInitialization();
@@ -59,7 +59,7 @@ Before(
 
 Before(
   {
-    tags: '@Tokens-popup or @Transactions-Popup or @Staking-Popup or @LockWallet-popup or @Top-Navigation-Popup or @AddressBook-popup or @Common-Popup or @SendTx-Simple-Popup or @MainNavigation-Popup or @Settings-Popup or @NFTs-Popup or @NFT-Folders-Popup or @Send-Transaction-Metadata-Popup or @ForgotPassword or @DAppConnector-Popup or @Analytics-Settings-Popup or @Banxa-Popup or @Accounts-Popup'
+    tags: '@Tokens-popup or @Transactions-Popup or @Staking-Popup or @LockWallet-popup or @Top-Navigation-Popup or @AddressBook-popup or @Common-Popup or @SendTx-Simple-Popup or @MainNavigation-Popup or @Settings-Popup or @NFTs-Popup or @NFT-Folders-Popup or @Send-Transaction-Metadata-Popup or @ForgotPassword or @DAppConnector-Popup or @Analytics-Settings-Popup or @Banxa-Popup'
   },
   async () => {
     await popupViewWalletInitialization();
@@ -242,3 +242,25 @@ Before({ tags: '@Multidelegation-DelegatedFunds-MultiplePools-Extended' }, async
   await localStorageInitializer.disableShowingMultidelegationDAppsIssueModal();
   await localStorageInitializer.initializeShowMultiAddressDiscoveryModal(false);
 });
+
+Before(
+  {
+    tags: '@Accounts-Extended'
+  },
+  async () => {
+    await extendedViewRepositoryWalletInitialization([TestWalletName.MultiAccActive1]);
+    await localStorageInitializer.disableShowingMultidelegationBetaBanner();
+    await localStorageInitializer.disableShowingMultidelegationPersistenceBanner();
+  }
+);
+
+Before(
+  {
+    tags: '@Accounts-Popup'
+  },
+  async () => {
+    await popupViewRepositoryWalletInitialization([TestWalletName.MultiAccActive1]);
+    await localStorageInitializer.disableShowingMultidelegationBetaBanner();
+    await localStorageInitializer.disableShowingMultidelegationPersistenceBanner();
+  }
+);
