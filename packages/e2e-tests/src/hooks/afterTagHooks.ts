690edb6af38235939dd04360a8ec1b4f512923ac
wklos-iohk <wojciech.klos@iohk.io>
2024-06-04 13:43:03 +0200

    test(extension): fix LW-10328

--- packages/e2e-tests/src/hooks/afterTagHooks.ts
+++ packages/e2e-tests/src/hooks/afterTagHooks.ts
@@ -1,7 +1,14 @@
 import { After } from '@wdio/cucumber-framework';
 import { switchToWindowWithLace } from '../utils/window';
+import extendedView from '../page/extendedView';
+import { browser } from '@wdio/globals';
 
 After(
   { tags: '@DAppConnector or @DAppConnector-Extended or @DAppConnector-Popup' },
   async () => await switchToWindowWithLace()
 );
+
+After({ tags: '@LW-7125' }, async () => {
+  await browser.reloadSession();
+  await extendedView.visit();
+});
