cb3575a33e38a782c204b4e57e1f5f63989c0e8d
januszjanus <janusz.janus@iohk.io>
2024-06-11 16:37:37 +0200

    test(extension): install deps fix for real chrome

--- packages/playwright-tests/playwright.config.ts
+++ packages/playwright-tests/playwright.config.ts
@@ -34,8 +34,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' }
     }
 
     // {
