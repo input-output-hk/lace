b0238f501cdcf25d3766a8526eea13de95b362d2
januszjanus <janusz.janus@iohk.io>
2024-06-14 13:59:30 +0200

    test(extension): back to 1 instance per worker

--- packages/e2e-tests/wdio.conf.base.ts
+++ packages/e2e-tests/wdio.conf.base.ts
@@ -7,7 +7,7 @@ export const config: WebdriverIO.Config = {
   specs: ['./src/features/**/*.feature'],
   automationProtocol: 'webdriver',
   exclude: [],
-  maxInstances: 2,
+  maxInstances: 1,
   maxInstancesPerCapability: 1,
   path: '/',
   logLevel: 'error',
