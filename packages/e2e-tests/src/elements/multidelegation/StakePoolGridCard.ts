acf72ea77a27fb15f5e6ab8624f0b277b1d16ebb
Wojtek Kłos <114915819+wklos-iohk@users.noreply.github.com>
2024-06-21 11:14:26 +0200

    test(extension): add tests for LW-10284 (#1233)

--- packages/e2e-tests/src/elements/multidelegation/StakePoolGridCard.ts
+++ packages/e2e-tests/src/elements/multidelegation/StakePoolGridCard.ts
@@ -4,13 +4,16 @@ import { ChainablePromiseElement } from 'webdriverio';
 export class StakePoolGridCard {
   private SELECTED_POOLS_LIST = '[data-testid="selected-pools-list"]';
   private AVAILABLE_POOLS_LIST = '[data-testid="stake-pool-list-scroll-wrapper"]';
-  private CARD = '[data-testid="stake-pool-card"]';
   private CARD_TITLE = '[data-testid="stake-pool-card-title"]';
+  private SATURATION_VALUE = '[data-testid="saturation-value"]';
+  private METRIC_VALUE = '[data-testid="stake-pool-metric-value"]';
 
-  protected card;
+  protected card: ChainablePromiseElement<WebdriverIO.Element | undefined>;
 
   constructor(index = 0, isOnSelectedPoolsList = false) {
-    this.card = $(isOnSelectedPoolsList ? this.SELECTED_POOLS_LIST : this.AVAILABLE_POOLS_LIST).$$(this.CARD)[index];
+    this.card = $(isOnSelectedPoolsList ? this.SELECTED_POOLS_LIST : this.AVAILABLE_POOLS_LIST).$(
+      `[data-index="${index}"]`
+    );
   }
 
   get container(): ChainablePromiseElement<WebdriverIO.Element | undefined> {
@@ -19,6 +22,14 @@ export class StakePoolGridCard {
 
   get title(): ChainablePromiseElement<WebdriverIO.Element> {
     return this.card.$(this.CARD_TITLE);
+  }
+
+  get saturation(): ChainablePromiseElement<WebdriverIO.Element> {
+    return this.card.$(this.SATURATION_VALUE);
+  }
+
+  get metricValue(): ChainablePromiseElement<WebdriverIO.Element> {
+    return this.card.$(this.METRIC_VALUE);
   }
 
   async click(): Promise<void> {
