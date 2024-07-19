b133fa22a6bf2c1c609159b46edff5147d29f2df
Daniel Main <daniel@funktional.dev>
2024-07-19 12:11:56 +0200

    chore(extension): now scripts can generate artifacts for firefox

--- apps/browser-extension-wallet/webpack-utils.js
+++ apps/browser-extension-wallet/webpack-utils.js
@@ -4,30 +4,67 @@ const transformManifest = (content, mode) => {
     encoding: 'utf8',
     defaults: process.env.BUILD_DEV_PREVIEW === 'true' ? './.env.developerpreview' : './.env.defaults'
   });
+
   try {
     const manifest = JSON.parse(content.toString());
+
     manifest.name = manifest.name.replace('$WALLET_MANIFEST_NAME', process.env.WALLET_MANIFEST_NAME);
+
     if (process.env.BUILD_DEV_PREVIEW === 'true') {
       const date = new Date();
       manifest.version = `${manifest.version}.${date.getMonth()}${date.getDate()}`;
     }
-    manifest.content_security_policy = manifest.content_security_policy
-      .replace(
-        '$CARDANO_SERVICES_URLS',
-        `${process.env.CARDANO_SERVICES_URL_MAINNET} ${process.env.CARDANO_SERVICES_URL_PREPROD} ${process.env.CARDANO_SERVICES_URL_PREVIEW} ${process.env.CARDANO_SERVICES_URL_SANCHONET}`
-      )
-      .replace(
-        '$ADA_HANDLE_URLS',
-        `${process.env.ADA_HANDLE_URL_MAINNET} ${process.env.ADA_HANDLE_URL_PREPROD} ${process.env.ADA_HANDLE_URL_PREVIEW} ${process.env.ADA_HANDLE_URL_SANCHONET}`
-      )
-      .replace('$LOCALHOST_DEFAULT_SRC', mode === 'development' ? 'http://localhost:3000' : '')
-      .replace('$LOCALHOST_SCRIPT_SRC', mode === 'development' ? 'http://localhost:3000' : '')
-      .replace(
-        '$LOCALHOST_CONNECT_SRC',
-        mode === 'development'
-          ? 'http://localhost:* http://127.0.0.1:* ws://localhost:3000 ws://0.0.0.0:3000/ws wss://localhost:3000  ws://localhost:3001 ws://0.0.0.0:3001/ws wss://localhost:3001'
-          : 'http://localhost:* http://127.0.0.1:*'
-      );
+
+    const cardanoServicesUrls = [
+      process.env.CARDANO_SERVICES_URL_MAINNET,
+      process.env.CARDANO_SERVICES_URL_PREPROD,
+      process.env.CARDANO_SERVICES_URL_PREVIEW,
+      process.env.CARDANO_SERVICES_URL_SANCHONET
+    ].join(' ');
+
+    const adaHandleUrls = [
+      process.env.ADA_HANDLE_URL_MAINNET,
+      process.env.ADA_HANDLE_URL_PREPROD,
+      process.env.ADA_HANDLE_URL_PREVIEW,
+      process.env.ADA_HANDLE_URL_SANCHONET
+    ].join(' ');
+
+    const connectSrcUrls = [
+      cardanoServicesUrls,
+      adaHandleUrls,
+      process.env.COINGECKO_URL,
+      process.env.MUESLISWAP_URL,
+      process.env.LOCALHOST_CONNECT_SRC,
+      process.env.LOOPBACK_IP_CONNECT_SRC,
+      process.env.POSTHOG_URL,
+      process.env.TYPEKIT_URL,
+      'data:'
+    ].join(' ');
+
+    const extension_pages = `default-src 'self' ; frame-src ${process.env.TREZOR_URL_SRC} ${process.env.YOUTUBE_URL_SRC}; script-src 'self' 'wasm-unsafe-eval' ; font-src 'self' ${process.env.TYPEKIT_URL}; object-src 'self'; connect-src ${cardanoServicesUrls} ${adaHandleUrls} ${process.env.COINGECKO_URL} ${process.env.MUESLISWAP_URL} ${process.env.LOCALHOST_CONNECT_SRC} ${process.env.LOOPBACK_IP_CONNECT_SRC} ${process.env.POSTHOG_URL} ${process.env.TYPEKIT_URL} data:; style-src * 'unsafe-inline'; img-src * data:;`;
+
+    manifest.content_security_policy = {
+      extension_pages
+    };
+
+    if (process.env.BROWSER === 'firefox') {
+      manifest.background = {
+        scripts: ['./js/background.js']
+      };
+      if (process.env.WALLET_MANIFEST_FIREFOX_ID) {
+        manifest.browser_specific_settings = {
+          gecko: {
+            id: process.env.WALLET_MANIFEST_FIREFOX_ID + '@lace.io',
+            strict_min_version: '91.0'
+          }
+        };
+      }
+    } else if (process.env.BROWSER === 'chrome') {
+      manifest.background = {
+        service_worker: './js/background.js'
+      };
+      manifest.key = process.env.LACE_EXTENSION_KEY;
+    }
 
     return JSON.stringify(manifest);
   } catch (error) {
