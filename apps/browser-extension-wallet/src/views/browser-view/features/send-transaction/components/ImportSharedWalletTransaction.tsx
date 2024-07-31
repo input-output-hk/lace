374e53f0602fcd2b58687aebe04f4b524354b060
Szymon Masłowski <szymon.maslowski@iohk.io>
2024-07-31 15:00:31 +0200

    fix: filter out own tx output from the tx summary of co sign flow

--- apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/ImportSharedWalletTransaction.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/send-transaction/components/ImportSharedWalletTransaction.tsx
@@ -4,11 +4,13 @@ import { useDrawer } from '@views/browser/stores';
 import { useBuiltTxState, useSections } from '../store';
 import { Serialization } from '@cardano-sdk/core';
 import { Wallet } from '@lace/cardano';
+import { useWalletState } from '@hooks/useWalletState';
 
 export const ImportSharedWalletTransaction = (): JSX.Element => {
   const [config] = useDrawer();
   const { setBuiltTxData } = useBuiltTxState();
   const { setSection } = useSections();
+  const walletState = useWalletState();
 
   // TODO: LW-10946 get the transaction data from the onContinue and set it using useBuiltTxState
   // eslint-disable-next-line react/jsx-handler-names
@@ -20,10 +22,13 @@ export const ImportSharedWalletTransaction = (): JSX.Element => {
         const importedSharedWalletTx = Serialization.Transaction.fromCbor(Wallet.TxCBOR(txData.transaction.cborHex));
         const { body, id } = importedSharedWalletTx.toCore();
 
+        const ownAddresses = new Set([...(walletState?.addresses || [])].map((address) => address.address));
+        const filteredOutputs = body.outputs.map((o) => o).filter((o) => ownAddresses.has(o.address));
+
         setBuiltTxData({
           uiTx: {
             fee: body.fee,
-            outputs: new Set(body.outputs),
+            outputs: new Set(filteredOutputs.length === 0 ? body.outputs : filteredOutputs),
             hash: id,
             handleResolutions: [],
             validityInterval: body.validityInterval
