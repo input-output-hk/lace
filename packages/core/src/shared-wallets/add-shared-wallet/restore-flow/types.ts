13dd835535983337860117f438a49af19488e143
Shawn <shawn.busuttil@iohk.io>
2024-08-05 10:34:24 +0100

    refactor: use schema validator on import shared wallet (#1333)
    * refactor: use schema validator on import shared wallet
    
    * fix: missing nativescript and metadata validation
    
    * refactor: move validator out and extend docs
    
    * refactor: extract types

--- packages/core/src/shared-wallets/add-shared-wallet/restore-flow/types.ts
+++ packages/core/src/shared-wallets/add-shared-wallet/restore-flow/types.ts
@@ -1,14 +1,5 @@
 import { CoSigner, QuorumOptionValue } from '../creation-flow';
 
-export enum FileErrorMessage {
-  INVALID_KEY = 'Invalid key',
-  UNRECOGNIZED = 'File is unrecognized',
-}
-
-export interface FileValidationError extends Error {
-  message: FileErrorMessage;
-}
-
 export enum SharedWalletRestorationStep {
   Done = 'Done',
   Import = 'Import',
