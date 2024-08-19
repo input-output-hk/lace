6135199abe674e5c8cd470f763f5665110dc428c
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2024-08-19 18:34:54 +0100

    refactor: simplify Ada Handle provider configuration
    There's no need to manage a separate set of configuration for this provider, given we're now using the same backend.

--- apps/browser-extension-wallet/webpack-utils.js
+++ apps/browser-extension-wallet/webpack-utils.js
@@ -16,10 +16,6 @@ const transformManifest = (content, mode) => {
         '$CARDANO_SERVICES_URLS',
         `${process.env.CARDANO_SERVICES_URL_MAINNET} ${process.env.CARDANO_SERVICES_URL_PREPROD} ${process.env.CARDANO_SERVICES_URL_PREVIEW} ${process.env.CARDANO_SERVICES_URL_SANCHONET}`
       )
-      .replace(
-        '$ADA_HANDLE_URLS',
-        `${process.env.ADA_HANDLE_URL_MAINNET} ${process.env.ADA_HANDLE_URL_PREPROD} ${process.env.ADA_HANDLE_URL_PREVIEW} ${process.env.ADA_HANDLE_URL_SANCHONET}`
-      )
       .replace('$LOCALHOST_DEFAULT_SRC', mode === 'development' ? 'http://localhost:3000' : '')
       .replace('$LOCALHOST_SCRIPT_SRC', mode === 'development' ? 'http://localhost:3000' : '')
       .replace(
