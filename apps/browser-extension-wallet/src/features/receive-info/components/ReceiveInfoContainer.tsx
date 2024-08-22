a4be9daf00e80214f39eb288d28d756792a11b79
Angel Castillo <Angel.Castillo@iohk.io>
2024-08-23 03:43:53 +0800

    chore: bump sdk version (#1380)
    * chore: bump sdk version
    
    * fix(extension): add most updated addresses to wallets cache addresses
    
    * fix(extension): receive info address now always picks the lowest derivaiton index
    
    * fix: do not update new wallet metadata with a previously active wallet's address
    
    ---------
    
    Co-authored-by: Martynas Kazlauskas <martynas.kazlauskas@iohk.io>

--- apps/browser-extension-wallet/src/features/receive-info/components/ReceiveInfoContainer.tsx
+++ apps/browser-extension-wallet/src/features/receive-info/components/ReceiveInfoContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useRedirection } from '../../../hooks';
 import { walletRoutePaths } from '../../../routes';
 import { useWalletStore } from '../../../stores';
@@ -10,10 +10,15 @@ export const ReceiveInfoContainer = (): React.ReactElement => {
   const { walletInfo } = useWalletStore();
   const handles = useGetHandles();
 
+  const userAddresses = useMemo(() => {
+    const addresses = [...walletInfo.addresses];
+    return addresses.sort((a, b) => a.index - b.index);
+  }, [walletInfo.addresses]);
+
   return (
     <ReceiveInfo
       name={walletInfo?.name}
-      address={walletInfo?.addresses[0].address}
+      address={userAddresses[0].address}
       handles={handles}
       goBack={redirectToOverview}
     />
--- apps/browser-extension-wallet/src/lib/scripts/background/cache-wallets-address.ts
+++ apps/browser-extension-wallet/src/lib/scripts/background/cache-wallets-address.ts
@@ -1,31 +1,40 @@
-import { WalletManager, WalletRepository } from '@cardano-sdk/web-extension';
+import { UpdateWalletMetadataProps, WalletManager, WalletRepository } from '@cardano-sdk/web-extension';
+import { blockingWithLatestFrom } from '@cardano-sdk/util-rxjs';
 import { Wallet } from '@lace/cardano';
-import { filter, switchMap, withLatestFrom, zip } from 'rxjs';
+import { EMPTY, filter, map, Observable, switchMap } from 'rxjs';
+import { isNotNil } from '@cardano-sdk/util';
+
+export const walletMetadataWithAddresses = (
+  walletManager: WalletManager<Wallet.WalletMetadata, Wallet.AccountMetadata>,
+  walletRepository: WalletRepository<Wallet.WalletMetadata, Wallet.AccountMetadata>
+): Observable<UpdateWalletMetadataProps<Wallet.WalletMetadata>> =>
+  walletManager.activeWalletId$.pipe(
+    switchMap((activeWallet) =>
+      activeWallet
+        ? walletManager.activeWallet$.pipe(
+            switchMap((observableWallet) => observableWallet.addresses$),
+            map((addresses) => addresses.map(({ address }) => address)),
+            blockingWithLatestFrom(
+              walletRepository.wallets$.pipe(
+                map((wallets) => wallets.find(({ walletId }) => walletId === activeWallet.walletId)),
+                filter(isNotNil)
+              )
+            ),
+            map(([walletAddresses, walletEntity]) => ({
+              walletId: activeWallet.walletId,
+              metadata: {
+                ...walletEntity.metadata,
+                walletAddresses
+              }
+            }))
+          )
+        : EMPTY
+    )
+  );
 
 export const cacheActivatedWalletAddressSubscription = (
   walletManager: WalletManager<Wallet.WalletMetadata, Wallet.AccountMetadata>,
   walletRepository: WalletRepository<Wallet.WalletMetadata, Wallet.AccountMetadata>
 ): void => {
-  zip([
-    walletManager.activeWalletId$.pipe(filter((activeWalletId) => Boolean(activeWalletId))),
-    walletManager.activeWallet$.pipe(
-      filter((wallet) => Boolean(wallet)),
-      switchMap((wallet) => wallet.addresses$)
-    )
-  ])
-    .pipe(withLatestFrom(walletRepository.wallets$))
-    .subscribe(([[activeWallet, walletAddresses], wallets]) => {
-      const wallet = wallets.find(({ walletId }) => walletId === activeWallet.walletId);
-      const uniqueAddresses = [
-        ...new Set([...(wallet.metadata.walletAddresses || []), ...walletAddresses.map(({ address }) => address)])
-      ];
-
-      walletRepository.updateWalletMetadata({
-        walletId: activeWallet.walletId,
-        metadata: {
-          ...wallet.metadata,
-          walletAddresses: uniqueAddresses
-        }
-      });
-    });
+  walletMetadataWithAddresses(walletManager, walletRepository).subscribe(walletRepository.updateWalletMetadata);
 };
