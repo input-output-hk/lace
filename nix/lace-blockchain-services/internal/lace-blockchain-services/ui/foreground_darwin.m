f28aad7e635a5ebd525328629283ac4f4d1c8a02
Michal Rus <m@michalrus.com>
2024-08-28 11:17:32 +0200

    chore: move lace-blockchain-services to a separate repository (#1387)
    LW-11295
    
    The new repository is <https://github.com/input-output-hk/desktop-full-node-backend>.

--- nix/lace-blockchain-services/internal/lace-blockchain-services/ui/foreground_darwin.m
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-void lbs__ui__bring_app_to_foreground() {
-  [[NSApplication sharedApplication] setActivationPolicy:NSApplicationActivationPolicyAccessory];
-  [NSApp activateIgnoringOtherApps:YES];
-}
