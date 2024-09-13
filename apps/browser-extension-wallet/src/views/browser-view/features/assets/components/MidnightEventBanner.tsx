cc887eeacfa5f9f389a8fd80348d8e1d23958c41
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2024-09-13 16:25:53 +0100

    fix(extension): [LW-11512] prevent midnight banner display (#1433)

--- apps/browser-extension-wallet/src/views/browser-view/features/assets/components/MidnightEventBanner.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/assets/components/MidnightEventBanner.tsx
@@ -43,7 +43,7 @@ export const MidnightEventBanner = (): JSX.Element => {
   }, []);
 
   const shouldHide = () => {
-    if (process.env.USE_MESSAGE_SIGNING !== 'true') {
+    if (process.env.USE_GLACIER_DROP !== 'true') {
       return true;
     }
 
