9334cd072983bcef9e26cc3349d9daba0b03efbd
Mircea Hasegan <mircea.hasegan@iohk.io>
2024-10-10 13:14:58 +0200

    fixup! chore: resolve ci issues

--- apps/browser-extension-wallet/src/lib/scripts/background/injectUtil.ts
+++ apps/browser-extension-wallet/src/lib/scripts/background/injectUtil.ts
@@ -7,7 +7,6 @@ import {
   RemoteApiPropertyType
 } from '@cardano-sdk/web-extension';
 
-const MODE_STORAGE_KEY = 'lace-features';
 const logger = console;
 
 export const WALLET_MODE_CHANNEL = 'wallet-mode';
@@ -33,27 +32,5 @@ const withModeApi = async <T>(invoke: (api: ModeApi) => Promise<T>, runtime: Min
   }
 };
 
-const loadAndStoreFeatureMode = async (runtime: MinimalRuntime) =>
-  withModeApi(async (modeApi) => {
-    const featureMode = await modeApi.getMode();
-    localStorage.setItem(MODE_STORAGE_KEY, JSON.stringify(featureMode));
-    return featureMode;
-  }, runtime);
-
-/**
- * Get feature flags:
- * - if doesn't exist in local storage, get feature flags from service worker
- *   by using the specified runtime for messaging
- * - if exists in local storage
- *   - return stored feature flags
- *   - as a side effect, get feature flags from service worker by using the
- *     specified runtime for messaging and store them in local storage
- */
 export const getMode = async (runtime: MinimalRuntime): Promise<'lace' | 'nami'> =>
-  //   const storedMode = localStorage.getItem(MODE_STORAGE_KEY);
-  //   if (storedMode) {
-  //     // update feature flags in local storage without blocking the return
-  //     void loadAndStoreFeatureMode(runtime);
-  //     return JSON.parse(storedMode) as WalletMode;
-  //   }
-  loadAndStoreFeatureMode(runtime);
+  withModeApi(async (modeApi) => await modeApi.getMode(), runtime);
