460502c9749afecb9483ca0308c867c67f3d81f0
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2024-10-17 10:36:59 +0100

    fix: close all lace windows before executing migration

--- apps/browser-extension-wallet/src/features/nami-migration/migration-tool/cross-extension-messaging/lace/create-lace-migration-open-listener.ts
+++ apps/browser-extension-wallet/src/features/nami-migration/migration-tool/cross-extension-messaging/lace/create-lace-migration-open-listener.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import { closeAllLaceWindows } from '@lib/scripts/background/util';
 import { MessageSender, NamiMessages } from '../shared/types';
 
 export const createLaceMigrationOpenListener =
@@ -6,6 +7,8 @@ export const createLaceMigrationOpenListener =
   async (message: NamiMessages, sender: MessageSender): Promise<void> => {
     console.log('[NAMI MIGRATION] createLaceMigrationOpenListener', message, sender);
     if (message === NamiMessages.open && sender.id === namiExtensionId) {
+      // First close all open lace tabs
+      await closeAllLaceWindows();
       createTab({ url: `chrome-extension://${laceExtensionId}/app.html` });
     }
   };
