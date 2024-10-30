83e4392c11aca274dc016db0a90e7bf62169687f
vetalcore <vetalcore@gmail.com>
2024-10-30 15:27:14 +0200

    fix: [lw-11783] do not show add shared wallet btn for hw

--- apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/DropdownMenuOverlay.tsx
+++ apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/DropdownMenuOverlay.tsx
@@ -43,7 +43,7 @@ export const DropdownMenuOverlay: VFC<Props> = ({
   const posthog = usePostHogClientContext();
   const sharedWalletsEnabled = posthog?.isFeatureFlagEnabled('shared-wallets');
   const [currentSection, setCurrentSection] = useState<Sections>(Sections.Main);
-  const { environmentName, setManageAccountsWallet, walletType, isSharedWallet } = useWalletStore();
+  const { environmentName, setManageAccountsWallet, walletType, isSharedWallet, isHardwareWallet } = useWalletStore();
 
   const openWalletAccounts = (wallet: AnyBip32Wallet<Wallet.WalletMetadata, Wallet.AccountMetadata>) => {
     setManageAccountsWallet(wallet);
@@ -88,7 +88,7 @@ export const DropdownMenuOverlay: VFC<Props> = ({
             {process.env.USE_MULTI_WALLET === 'true' && (
               <AddNewWalletLink isPopup={isPopup} sendAnalyticsEvent={sendAnalyticsEvent} />
             )}
-            {sharedWalletsEnabled && !isSharedWallet && <AddSharedWalletLink isPopup={isPopup} />}
+            {sharedWalletsEnabled && !isSharedWallet && !isHardwareWallet && <AddSharedWalletLink isPopup={isPopup} />}
             <AddressBookLink />
             <SettingsLink />
             <Separator />
