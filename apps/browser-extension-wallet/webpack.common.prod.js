cdea5e8eb5c2fbb81eebe25d2aa43940d1427618
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2024-11-15 14:19:19 +0000

    Revert "build: delete source maps from prod build after Sentry upload (#1527)"
    This reverts commit b8c7440832cf54d94cc202d27602a12d3fddc50f.

--- apps/browser-extension-wallet/webpack.common.prod.js
+++ apps/browser-extension-wallet/webpack.common.prod.js
@@ -22,9 +22,6 @@ module.exports = () => ({
             authToken: process.env.SENTRY_AUTH_TOKEN,
             org: process.env.SENTRY_ORG,
             project: process.env.SENTRY_PROJECT,
-            sourcemaps: {
-              filesToDeleteAfterUpload: ['**/*.js.map']
-            },
             telemetry: false,
             url: 'https://sentry.io/'
           })
