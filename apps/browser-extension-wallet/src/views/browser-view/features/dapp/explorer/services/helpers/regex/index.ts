732556b0db13ef237dd683d48f9b644dff08e69d
Szymon Masłowski <szymon.maslowski@iohk.io>
2025-01-03 16:41:39 +0100

    chore: remove dead files raised during static analysis

--- apps/browser-extension-wallet/src/views/browser-view/features/dapp/explorer/services/helpers/regex/index.ts
+++
@@ -1,6 +0,0 @@
-export const EMAIL_REGEX =
-  /[\d!#$%&'*+/=?^_`a-z{|}~-]+(?:\.[\d!#$%&'*+/=?^_`a-z{|}~-]+)*@(?:[\da-z](?:[\da-z-]*[\da-z])?\.)+[\da-z](?:[\da-z-]*[\da-z])?/gm;
-
-export const URL_REGEX = /[\w#%+.:=@~-]{1,256}\.[\d()A-Za-z]{1,6}\b([\w#%&()+./:=?@~-]*)/gm;
-
-export const LETTERS_AND_NUMBERS_REGEX = /^[\d A-Za-z]+$/gm;
