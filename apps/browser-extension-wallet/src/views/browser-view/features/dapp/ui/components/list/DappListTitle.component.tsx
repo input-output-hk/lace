68d001afaf804903cff2bd901965dee9bcdb4ed7
John Oshalusi <john.oshalusi@iohk.io>
2025-01-15 13:17:08 +0100

    feat: fill in the tooltip text

--- apps/browser-extension-wallet/src/views/browser-view/features/dapp/ui/components/list/DappListTitle.component.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/dapp/ui/components/list/DappListTitle.component.tsx
@@ -3,12 +3,33 @@ import { Flex, Text, InfoComponent, Tooltip } from '@input-output-hk/lace-ui-too
 import capitalize from 'lodash/capitalize';
 
 import styles from './DappListTitle.module.scss';
+import { Trans } from 'react-i18next';
 
 export const DappListTitle = ({ title, count }: { title: string; count?: number }): ReactElement => (
   <Flex alignItems="flex-end">
     <Flex justifyContent="center" alignItems="center" gap="$4">
       <Text.Heading>{capitalize(title)}</Text.Heading>
-      <Tooltip label="This is a placeholder text." align="start" side="top">
+      <Tooltip
+        label={
+          <Trans
+            i18nKey="dappdiscovery.general_info"
+            components={{
+              p: <p />,
+              link1: <a href="https://dappradar.com/" target="_blank" rel="noreferrer" />,
+              link2: (
+                <a
+                  href="https://dappradar.com/blog/how-to-list-your-dapps-on-dappradar-for-free"
+                  target="_blank"
+                  rel="noreferrer"
+                />
+              )
+            }}
+            style={{ width: '300px' }}
+          />
+        }
+        align="start"
+        side="top"
+      >
         <InfoComponent className={styles.icon} />
       </Tooltip>
     </Flex>
