3194959d9d4b8afed2db016db4ec45cbe06f3711
vetalcore <vetalcore@gmail.com>
2025-01-21 00:34:12 +0200

    fix: resolve sonarcloud issues

--- packages/common/src/ui/hooks/useFetchImage.ts
+++ packages/common/src/ui/hooks/useFetchImage.ts
@@ -49,7 +49,7 @@ const isImageLoading = (imageUrl: string): Promise<void> =>
   new Promise((resolve, reject) => {
     const img = new Image();
     img.addEventListener('load', () => resolve());
-    img.addEventListener('error', (err) => reject(err));
+    img.addEventListener('error', (err) => reject(new Error(err.error)));
     img.src = imageUrl;
   });
 
