e112c2689fbf8eb8583df22624e0a2cf9ce91c0d
Dominik Guzei <dominik.guzei@gmail.com>
2025-01-20 20:41:42 +0100

    fix(common): wait for scroll target in dom [LW-12079]
    Use a MutationObserver to wait for the scroll target of the
    VirtualiseGrid component to be found in the DOM. Combine it
    with useState to trigger re-renders when its found.

--- packages/common/src/ui/components/VirtualisedGrid/VirtualisedGrid.tsx
+++ packages/common/src/ui/components/VirtualisedGrid/VirtualisedGrid.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-magic-numbers */
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ListRange, VirtuosoGrid, VirtuosoGridProps } from 'react-virtuoso';
 import cn from 'classnames';
 import styles from './VirtualisedGrid.module.scss';
 
 export const fixedVirtualisedGridColumns = [1, 2, 3, 4, 5, 6, 7, 8] as const;
-export type VirtualisedGridColumns = typeof fixedVirtualisedGridColumns[number];
+export type VirtualisedGridColumns = (typeof fixedVirtualisedGridColumns)[number];
 
 export const getTypedColumn = (column: number): VirtualisedGridColumns | undefined =>
   fixedVirtualisedGridColumns.find((c) => c === column);
@@ -21,6 +21,43 @@ export type GridProps<T> = VirtuosoGridProps<T, null> & {
   columns?: VirtualisedGridColumns;
 };
 
+/**
+ * Hook that waits for an element matching the query selector to appear in the DOM,
+ * assigns it to the provided ref and triggers a re-render.
+ *
+ * @param selector - The CSS selector to match the element.
+ */
+const useQuerySelectorRef = <T extends HTMLElement>(
+  selector: string
+): [React.MutableRefObject<T | undefined>, boolean] => {
+  const ref = useRef<T | undefined>();
+  const [found, setFound] = useState(false); // State to trigger re-renders
+
+  useEffect(() => {
+    const observer = new MutationObserver(() => {
+      const element = document.querySelector<T>(selector);
+      if (element) {
+        ref.current = element;
+        setFound(true); // Update state to trigger a re-render
+        observer.disconnect(); // Stop observing once the element is found
+      }
+    });
+
+    // Start observing the entire document for changes
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+
+    // Cleanup function to disconnect the observer
+    return () => {
+      observer.disconnect();
+    };
+  }, [selector]);
+
+  return [ref, found];
+};
+
 export const VirtualisedGrid = <T extends Record<string, unknown> | undefined>({
   itemContent,
   items,
@@ -31,13 +68,7 @@ export const VirtualisedGrid = <T extends Record<string, unknown> | undefined>({
   columns,
   ...props
 }: GridProps<T>): React.ReactElement => {
-  const scrollableTargetReference = useRef<VirtuosoGridProps<T, undefined>['customScrollParent']>();
-
-  useLayoutEffect(() => {
-    if (scrollableTargetId) {
-      scrollableTargetReference.current = document.querySelector(`#${scrollableTargetId}`) as unknown as HTMLDivElement;
-    }
-  }, [scrollableTargetId]);
+  const [scrollableTargetReference] = useQuerySelectorRef(`#${scrollableTargetId}`);
 
   return (
     <div ref={tableReference} data-testid={testId}>
