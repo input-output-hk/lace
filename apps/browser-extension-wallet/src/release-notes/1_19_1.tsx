3fc11f44c5dc3543dd8979a6f18b7c20e897be7d
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-01-28 15:10:41 +0000

    chore: bump manifest version v1.19.1 (#1675)

---
+++ apps/browser-extension-wallet/src/release-notes/1_19_1.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_19_1 = (): React.ReactElement => (
+  <>
+    <p>In this release:</p>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Network requests now pause when the app is idle, reducing unnecessary network demand</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_19_1;
