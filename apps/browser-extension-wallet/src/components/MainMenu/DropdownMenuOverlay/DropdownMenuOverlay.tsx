c30a33cb0f620b1f3b598438824e25dc3d169a36
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-01-29 18:54:24 +0000

    fix(extension): [LW-12155] switch to nami mode (#1682)
    * feat: remove switch to nami mode from settings pages
    
    * feat: add switch to nami mode in dropdown menu
    
    * feat: augment nami mode switch styling
    
    * fix: [LW-12155]

--- apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/DropdownMenuOverlay.tsx
+++ apps/browser-extension-wallet/src/components/MainMenu/DropdownMenuOverlay/DropdownMenuOverlay.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useCallback, useState, VFC, useMemo } from 'react';
+import React, { ReactNode, useCallback, useState, VFC, useMemo, useEffect } from 'react';
 import { Menu, MenuProps } from 'antd';
 import {
   AddNewWalletLink,
@@ -12,6 +12,7 @@ import {
   ThemeSwitcher,
   UserInfo
 } from './components';
+import { Switch } from '@lace/common';
 import styles from './DropdownMenuOverlay.module.scss';
 import { NetworkInfo } from './components/NetworkInfo';
 import { Sections } from './types';
@@ -24,6 +25,11 @@ import type { AnyBip32Wallet } from '@cardano-sdk/web-extension';
 import { WalletType } from '@cardano-sdk/web-extension';
 import { Wallet } from '@lace/cardano';
 import { usePostHogClientContext } from '@providers/PostHogClientProvider';
+import { BackgroundStorage } from '@lib/scripts/types';
+import { getBackgroundStorage, setBackgroundStorage } from '@lib/scripts/background/storage';
+import { useBackgroundServiceAPIContext } from '@providers';
+import { WarningModal } from '@src/views/browser-view/components';
+import { useTranslation } from 'react-i18next';
 
 interface Props extends MenuProps {
   isPopup?: boolean;
@@ -40,10 +46,22 @@ export const DropdownMenuOverlay: VFC<Props> = ({
   sendAnalyticsEvent,
   ...props
 }): React.ReactElement => {
+  const { t } = useTranslation();
   const posthog = usePostHogClientContext();
+  const backgroundServices = useBackgroundServiceAPIContext();
+
   const sharedWalletsEnabled = posthog?.isFeatureFlagEnabled('shared-wallets');
   const [currentSection, setCurrentSection] = useState<Sections>(Sections.Main);
   const { environmentName, setManageAccountsWallet, walletType, isSharedWallet, isHardwareWallet } = useWalletStore();
+  const [namiMigration, setNamiMigration] = useState<BackgroundStorage['namiMigration']>();
+  const [modalOpen, setModalOpen] = useState(false);
+  const useSwitchToNamiMode = posthog?.isFeatureFlagEnabled('use-switch-to-nami-mode');
+
+  useEffect(() => {
+    getBackgroundStorage()
+      .then((storage) => setNamiMigration(storage.namiMigration))
+      .catch(console.error);
+  }, []);
 
   const openWalletAccounts = (wallet: AnyBip32Wallet<Wallet.WalletMetadata, Wallet.AccountMetadata>) => {
     setManageAccountsWallet(wallet);
@@ -76,8 +94,47 @@ export const DropdownMenuOverlay: VFC<Props> = ({
 
   const showAddSharedWalletLink = sharedWalletsEnabled && !isSharedWallet && !isHardwareWallet;
 
+  const handleNamiModeChange = async (activated: boolean) => {
+    const mode = activated ? 'nami' : 'lace';
+    const migration: BackgroundStorage['namiMigration'] = {
+      ...namiMigration,
+      mode
+    };
+
+    setNamiMigration(migration);
+    backgroundServices.handleChangeMode({ mode });
+    await setBackgroundStorage({
+      namiMigration: migration
+    });
+    setModalOpen(false);
+    if (activated) {
+      await posthog.sendEvent(PostHogAction.SettingsSwitchToNamiClick);
+      try {
+        await backgroundServices.handleOpenPopup();
+      } catch (error) {
+        // improve logging
+        console.warn(error);
+      }
+    } else {
+      window.location.reload();
+    }
+  };
+
   return (
     <Menu {...props} className={styles.menuOverlay} data-testid="header-menu">
+      <WarningModal
+        header={
+          <div className={styles.switchToNamiModalTitle}>{t('browserView.settings.legacyMode.confirmation.title')}</div>
+        }
+        content={t('browserView.settings.legacyMode.confirmation.description')}
+        visible={modalOpen}
+        onCancel={() => setModalOpen(false)}
+        onConfirm={() => handleNamiModeChange(true)}
+        cancelLabel={t('browserView.settings.legacyMode.confirmation.cancel')}
+        confirmLabel={t('browserView.settings.legacyMode.confirmation.confirm')}
+        confirmCustomClassName={styles.settingsConfirmButton}
+        isPopupView={isPopup}
+      />
       {currentSection === Sections.Main && (
         <div
           className={classNames(
@@ -96,6 +153,17 @@ export const DropdownMenuOverlay: VFC<Props> = ({
             <Separator />
             {shouldShowSignMessage && getSignMessageLink()}
             <ThemeSwitcher isPopup={isPopup} />
+            {useSwitchToNamiMode && !isSharedWallet && (
+              <div className={styles.menuItemTheme}>
+                {t('browserView.settings.legacyMode.section')}
+                <Switch
+                  testId="settings-nami-mode-switch"
+                  checked={namiMigration?.mode === 'nami'}
+                  onChange={(checked) => (checked ? setModalOpen(true) : handleNamiModeChange(false))}
+                  className={styles.namiModeSwitch}
+                />
+              </div>
+            )}
             <NetworkChoise onClick={handleNetworkChoise} />
             {lockWalletButton && (
               <>
