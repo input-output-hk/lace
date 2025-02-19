cd468fb094706b02149caeee798819a85cebe654
wklos-iohk <wojciech.klos@iohk.io>
2025-02-19 11:24:48 +0100

    test(extension): unify assertions

--- packages/e2e-tests/src/assert/VotingCenterPageAssert.ts
+++ packages/e2e-tests/src/assert/VotingCenterPageAssert.ts
@@ -7,15 +7,15 @@ class VotingCenterPageAssert {
   async assertSeeVotingCenterBanner() {
     await VotingCenterPage.banner.waitForDisplayed();
     await VotingCenterPage.title.waitForDisplayed();
-    expect(await VotingCenterPage.title.getText()).to.be.eq(await t('browserView.voting-beta.modal.title'));
+    expect(await VotingCenterPage.title.getText()).to.equal(await t('browserView.voting-beta.modal.title'));
     await VotingCenterPage.description.waitForDisplayed();
     const expectedDescriptionKey = (await isPopupMode())
       ? 'browserView.voting-beta.modal.description-popup'
       : 'browserView.voting-beta.modal.description';
     const expectedDescription = (await t(expectedDescriptionKey)).replaceAll('<br /><br />', '\n\n');
-    expect(await VotingCenterPage.description.getProperty('innerText')).to.be.eq(expectedDescription);
+    expect(await VotingCenterPage.description.getProperty('innerText')).to.equal(expectedDescription);
     await VotingCenterPage.govToolButton.waitForClickable();
-    expect(await VotingCenterPage.govToolButton.getText()).to.be.eq(await t('browserView.voting-beta.modal.cta'));
+    expect(await VotingCenterPage.govToolButton.getText()).to.equal(await t('browserView.voting-beta.modal.cta'));
   }
 }
 
