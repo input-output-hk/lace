7b586ed8fa8c8ca9e2b3d120af36ca243642e6be
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-02-24 16:45:57 +0000

    chore: bump manifest version v1.20.1 (#1739)

---
+++ apps/browser-extension-wallet/src/release-notes/1_20_1.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_20_1 = (): React.ReactElement => (
+  <>
+    <p>This release:</p>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Adjusts Lace's logging configuration</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_20_1;
