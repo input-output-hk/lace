02b56b7cc92f28659559269f2a342df58f7b3d73
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-02-25 10:32:05 +0000

    fixup! fix(extension): remove invalid lookup causing range error

--- apps/browser-extension-wallet/src/utils/PaperWallet/paper-wallet.ts
+++ apps/browser-extension-wallet/src/utils/PaperWallet/paper-wallet.ts
@@ -50,7 +50,7 @@ export const generatePaperWalletPdf = async ({
 
     const paperWalletPdf = await paperWallet({
       pgpRef: pgpInfo.pgpKeyReference,
-      creationDate: new Date().toLocaleDateString(navigator.language ?? 'en-US', {
+      creationDate: new Date().toLocaleDateString(navigator.language, {
         year: 'numeric',
         month: 'long',
         day: 'numeric'
