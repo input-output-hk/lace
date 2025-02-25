5638c28f84a46e0e9920dd5c6a8cdaf509c8acff
vetalcore <vetalcore@gmail.com>
2025-02-25 10:03:05 +0200

    fix: [lw-10100] show exit confirmation for staking drawer (#1733)

--- packages/staking/src/features/staking/StakingView.tsx
+++ packages/staking/src/features/staking/StakingView.tsx
@@ -1,16 +1,23 @@
 import { Box, Text } from '@input-output-hk/lace-ui-toolkit';
 import { Activity } from 'features/activity';
-import { useOutsideHandles } from 'features/outside-handles-provider';
+import { ExitStakingModal } from 'features/modals/ExitStakingModal';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BrowsePools } from '../BrowsePools';
 import { Drawer } from '../Drawer';
 import { ChangingPreferencesModal } from '../modals';
+import { useOutsideHandles } from '../outside-handles-provider';
 import { Overview } from '../overview';
 import { DrawerManagementStep, DrawerStep } from '../store';
 import { Navigation } from './Navigation';
 import { OneTimeModals } from './OneTimeModals';
 import { StakingPage } from './types';
+
+const stepsWithExitConfirmation = new Set<DrawerStep>([
+  DrawerManagementStep.Confirmation,
+  DrawerManagementStep.Sign,
+  DrawerManagementStep.Preferences,
+]);
 
 export const StakingView = () => {
   const { t } = useTranslation();
@@ -49,8 +56,13 @@ export const StakingView = () => {
           </>
         )}
       </Navigation>
-      <Drawer showCloseIcon showBackIcon={(step: DrawerStep): boolean => stepsWithBackBtn.has(step)} />
+      <Drawer
+        showCloseIcon
+        showBackIcon={(step: DrawerStep): boolean => stepsWithBackBtn.has(step)}
+        showExitConfirmation={(step: DrawerStep): boolean => stepsWithExitConfirmation.has(step)}
+      />
       <ChangingPreferencesModal />
+      <ExitStakingModal />
       {!isSharedWallet && <OneTimeModals />}
     </>
   );
