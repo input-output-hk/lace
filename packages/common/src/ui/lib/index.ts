f17897c5fd813e80d8ea694c062668c9fc3053d1
Przemysław Włodek <przem.wlodek.github@gmail.com>
2025-02-27 15:38:23 +0100

    feat(common): capture relevant AppLogger errors in Sentry

--- packages/common/src/ui/lib/index.ts
+++ packages/common/src/ui/lib/index.ts
@@ -4,3 +4,4 @@ export * from './get-random-icon';
 export * from './text-formatters';
 export * from './get-number-unit';
 export * from './stringify-with-fallback';
+export * from './is-network-error';
