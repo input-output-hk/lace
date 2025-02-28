d29ac45d15243532ae975cfba8fa7ad1a91a8d7c
vetalcore <vetalcore@gmail.com>
2025-02-28 14:31:03 +0200

    fix: [lw-12362] view cbor in dapp confirmation (#1747)

---
+++ packages/core/src/ui/components/ActivityDetail/components/TxDetailsCBOR/TxDetailsCBOR.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from 'react';
+import { TxDetailsGroup } from '../../TxDetailsGroup';
+import { useTranslation } from 'react-i18next';
+import { Flex, Text } from '@input-output-hk/lace-ui-toolkit';
+import { ReactComponent as CopyIcon } from '../../../../assets/icons/copy-icon.svg';
+import styles from './TxDetailsCBOR.module.scss';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { toast } from '@lace/common';
+
+interface TxDetailsCBORProps {
+  cbor: string;
+}
+
+export const TxDetailsCBOR = ({ cbor }: TxDetailsCBORProps): React.ReactElement => {
+  const { t } = useTranslation();
+
+  const doToast = useCallback(() => {
+    toast.notify({
+      text: t('core.addressCard.handle.copy.notification'),
+      icon: CopyIcon
+    });
+  }, [t]);
+
+  return (
+    <TxDetailsGroup title={t('core.activityDetails.CBOR')} testId="cbor-detail" withSeparatorLine>
+      <Flex flexDirection="column" gap="$20">
+        <Flex w="$fill" justifyContent="flex-end">
+          <CopyToClipboard text={cbor}>
+            <CopyIcon className={styles.copyButton} data-testid="copy-address-btn" onClick={doToast} />
+          </CopyToClipboard>
+        </Flex>
+        <Text.Body.Normal data-testid="cbor" className={styles.cbor} weight="$medium">
+          {cbor}
+        </Text.Body.Normal>
+      </Flex>
+    </TxDetailsGroup>
+  );
+};
