e28d540a1f1707ceea15d5402e902aded732e7b2
Szymon Masłowski <szymon.maslowski@iohk.io>
2025-05-08 10:30:31 +0200

    tmp

--- apps/browser-extension-wallet/src/popup.tsx
+++ apps/browser-extension-wallet/src/popup.tsx
@@ -1,11 +1,6 @@
 /* eslint-disable sonarjs/cognitive-complexity */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import { walletRoutePaths } from '@routes';
-import { StoreProvider } from '@stores';
-import { CurrencyStoreProvider } from '@providers/currency';
-import { AppSettingsProvider, DatabaseProvider, ThemeProvider, AnalyticsProvider } from '@providers';
 import '@lib/i18n';
 import 'antd/dist/antd.css';
 import './styles/index.scss';
@@ -14,120 +9,14 @@ import 'normalize.css';
 // https://github.com/import-js/eslint-plugin-import/issues/1810
 // eslint-disable-next-line import/no-unresolved
 import '@lace/staking/index.css';
-import { BackgroundServiceAPIProvider } from '@providers/BackgroundServiceAPI';
-import { ExternalLinkOpenerProvider } from '@providers/ExternalLinkOpenerProvider';
-import { APP_MODE_POPUP, TRACK_POPUP_CHANNEL } from './utils/constants';
-import { MigrationContainer } from '@components/MigrationContainer';
-import { DataCheckContainer } from '@components/DataCheckContainer';
-import { PostHogClientProvider } from '@providers/PostHogClientProvider';
-import { BackgroundPageProvider } from '@providers/BackgroundPageProvider';
-import { AddressesDiscoveryOverlay } from 'components/AddressesDiscoveryOverlay';
-import { getBackgroundStorage } from '@lib/scripts/background/storage';
-import { runtime, Storage, storage } from 'webextension-polyfill';
-import { NamiMigrationGuard } from './features/nami-migration/NamiMigrationGuard';
+import { TRACK_POPUP_CHANNEL } from './utils/constants';
+import { runtime } from 'webextension-polyfill';
 import { createNonBackgroundMessenger } from '@cardano-sdk/web-extension';
 import { logger } from '@lace/common';
-import { AppVersionGuard } from './utils/AppVersionGuard';
-import { ErrorBoundary } from '@components/ErrorBoundary';
-import { BlockchainProvider, useCurrentBlockchain, Blockchain } from './multichain/BlockchainProvider';
 import { Popup } from '@src/poc/Popup';
 
-const CARDANO_LACE = 'lace';
-const BITCOIN_LACE = 'lace-bitcoin';
-
-const App = (): React.ReactElement => {
-  const [_, setMode] = useState<'lace' | 'nami' | 'lace-bitcoin'>('lace');
-  const { setBlockchain } = useCurrentBlockchain();
-
-  useEffect(() => {
-    const handleStorageChange = async (changes: Record<string, Storage.StorageChange>) => {
-      const oldModeValue = changes.BACKGROUND_STORAGE?.oldValue?.namiMigration;
-      const newModeValue = changes.BACKGROUND_STORAGE?.newValue?.namiMigration;
-      const activeBlockchainOldValue = changes.BACKGROUND_STORAGE?.oldValue?.activeBlockchain;
-      const activeBlockchainNewValue = changes.BACKGROUND_STORAGE?.newValue?.activeBlockchain;
-
-      if (activeBlockchainOldValue !== activeBlockchainNewValue) {
-        const isCardano = !activeBlockchainNewValue || activeBlockchainNewValue === 'cardano';
-        setMode(isCardano ? CARDANO_LACE : BITCOIN_LACE);
-        setBlockchain(isCardano ? Blockchain.Cardano : Blockchain.Bitcoin);
-        return;
-      }
-
-      if (oldModeValue?.mode !== newModeValue?.mode) {
-        setMode(newModeValue?.mode || CARDANO_LACE);
-        // Force back to original routing unless it is staking route
-        if (window.location.hash.split('#')[1] !== walletRoutePaths.earn) {
-          window.location.hash = '#';
-        }
-      }
-    };
-
-    storage.onChanged.addListener(handleStorageChange);
-
-    const getWalletMode = async () => {
-      const { namiMigration, activeBlockchain } = await getBackgroundStorage();
-      if (activeBlockchain === 'bitcoin') {
-        setMode(BITCOIN_LACE);
-        setBlockchain(Blockchain.Bitcoin);
-      } else {
-        setMode(namiMigration?.mode || CARDANO_LACE);
-        setBlockchain(Blockchain.Cardano);
-      }
-    };
-
-    getWalletMode();
-
-    return () => {
-      storage.onChanged.removeListener(handleStorageChange);
-    };
-  }, [setBlockchain]);
-
-  return (
-    <ErrorBoundary>
-      <BackgroundServiceAPIProvider>
-        <AppSettingsProvider>
-          <DatabaseProvider>
-            <StoreProvider appMode={APP_MODE_POPUP}>
-              <CurrencyStoreProvider>
-                <HashRouter>
-                  <PostHogClientProvider>
-                    <AnalyticsProvider>
-                      <ThemeProvider>
-                        <ExternalLinkOpenerProvider>
-                          <MigrationContainer appMode={APP_MODE_POPUP}>
-                            <DataCheckContainer appMode={APP_MODE_POPUP}>
-                              <AddressesDiscoveryOverlay>
-                                <NamiMigrationGuard>
-                                  <BackgroundPageProvider>
-                                    <AppVersionGuard>
-                                      <Popup />
-                                    </AppVersionGuard>
-                                  </BackgroundPageProvider>
-                                </NamiMigrationGuard>
-                              </AddressesDiscoveryOverlay>
-                            </DataCheckContainer>
-                          </MigrationContainer>
-                        </ExternalLinkOpenerProvider>
-                      </ThemeProvider>
-                    </AnalyticsProvider>
-                  </PostHogClientProvider>
-                </HashRouter>
-              </CurrencyStoreProvider>
-            </StoreProvider>
-          </DatabaseProvider>
-        </AppSettingsProvider>
-      </BackgroundServiceAPIProvider>
-    </ErrorBoundary>
-  );
-};
-
 const mountNode = document.querySelector('#lace-popup');
-ReactDOM.render(
-  <BlockchainProvider>
-    <App />
-  </BlockchainProvider>,
-  mountNode
-);
+ReactDOM.render(<Popup />, mountNode);
 
 // not exposing any API; used to keep track of connection with SW to determine whether popup is open
 createNonBackgroundMessenger({ baseChannel: TRACK_POPUP_CHANNEL }, { logger, runtime });
--- apps/browser-extension-wallet/src/views/browser-view/index.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/index.tsx
@@ -1,17 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-// import { BrowserViewRoutes } from '@views/browser/routes';
-// import { BitcoinBrowserViewRoutes } from '../bitcoin-mode/BitcoinBrowserViewRoutes';
-import {
-  CurrencyStoreProvider,
-  DatabaseProvider,
-  AppSettingsProvider,
-  ThemeProvider,
-  AnalyticsProvider,
-  UIThemeProvider
-} from '@providers';
-import { StoreProvider } from '@stores';
 import 'antd/dist/antd.css';
 import '../../styles/index.scss';
 import 'normalize.css';
@@ -19,122 +7,8 @@ import 'normalize.css';
 // https://github.com/import-js/eslint-plugin-import/issues/1810
 // eslint-disable-next-line import/no-unresolved
 import '@lace/staking/index.css';
-import { BackgroundServiceAPIProvider } from '@providers/BackgroundServiceAPI';
-import { ExternalLinkOpenerProvider } from '@providers/ExternalLinkOpenerProvider';
-import { BackgroundPageProvider } from '@providers/BackgroundPageProvider';
-import { APP_MODE_BROWSER } from '@src/utils/constants';
-import { MigrationContainer } from '@components/MigrationContainer';
-import { DataCheckContainer } from '@components/DataCheckContainer';
 import '../../lib/scripts/keep-alive-ui';
-import { PostHogClientProvider } from '@providers/PostHogClientProvider';
-import { AddressesDiscoveryOverlay } from 'components/AddressesDiscoveryOverlay';
-import { NamiMigrationGuard } from '@src/features/nami-migration/NamiMigrationGuard';
-import { AppVersionGuard } from '@src/utils/AppVersionGuard';
-import { ErrorBoundary } from '@components/ErrorBoundary';
-import { storage, Storage } from 'webextension-polyfill';
-import { walletRoutePaths } from '@routes';
-import { getBackgroundStorage } from '@lib/scripts/background/storage';
-import { BlockchainProvider, useCurrentBlockchain, Blockchain } from './../../multichain/BlockchainProvider';
 import { Tab } from '@src/poc/Tab';
 
-const CARDANO_LACE = 'lace';
-const BITCOIN_LACE = 'lace-bitcoin';
-
-// const passwordPOC = true;
-
-const App = (): React.ReactElement => {
-  const [_, setMode] = useState<'lace' | 'lace-bitcoin'>('lace');
-  const { setBlockchain } = useCurrentBlockchain();
-
-  /* eslint-disable sonarjs/cognitive-complexity */
-  useEffect(() => {
-    const handleStorageChange = async (changes: Record<string, Storage.StorageChange>) => {
-      const oldModeValue = changes.BACKGROUND_STORAGE?.oldValue?.namiMigration;
-      const newModeValue = changes.BACKGROUND_STORAGE?.newValue?.namiMigration;
-      const activeBlockchainOldValue = changes.BACKGROUND_STORAGE?.oldValue?.activeBlockchain;
-      const activeBlockchainNewValue = changes.BACKGROUND_STORAGE?.newValue?.activeBlockchain;
-
-      if (activeBlockchainOldValue !== activeBlockchainNewValue) {
-        const isCardano = !activeBlockchainNewValue || activeBlockchainNewValue === 'cardano';
-        setMode(isCardano ? CARDANO_LACE : BITCOIN_LACE);
-        setBlockchain(isCardano ? Blockchain.Cardano : Blockchain.Bitcoin);
-        return;
-      }
-
-      if (oldModeValue?.mode !== newModeValue?.mode) {
-        setMode(newModeValue?.mode || CARDANO_LACE);
-        // Force back to original routing unless it is staking route
-        if (window.location.hash.split('#')[1] !== walletRoutePaths.earn) {
-          window.location.hash = '#';
-        }
-      }
-    };
-
-    storage.onChanged.addListener(handleStorageChange);
-
-    const getWalletMode = async () => {
-      const { activeBlockchain } = await getBackgroundStorage();
-      if (activeBlockchain === 'bitcoin') {
-        setMode(BITCOIN_LACE);
-        setBlockchain(Blockchain.Bitcoin);
-      } else {
-        setMode(CARDANO_LACE);
-        setBlockchain(Blockchain.Cardano);
-      }
-    };
-
-    getWalletMode();
-
-    return () => {
-      storage.onChanged.removeListener(handleStorageChange);
-    };
-  }, [setBlockchain]);
-
-  return (
-    <ErrorBoundary>
-      <BackgroundServiceAPIProvider>
-        <AppSettingsProvider>
-          <DatabaseProvider>
-            <StoreProvider appMode={APP_MODE_BROWSER}>
-              <CurrencyStoreProvider>
-                <HashRouter>
-                  <BackgroundPageProvider>
-                    <PostHogClientProvider>
-                      <AnalyticsProvider>
-                        <ThemeProvider>
-                          <UIThemeProvider>
-                            <ExternalLinkOpenerProvider>
-                              <MigrationContainer appMode={APP_MODE_BROWSER}>
-                                <DataCheckContainer appMode={APP_MODE_BROWSER}>
-                                  <AddressesDiscoveryOverlay>
-                                    <NamiMigrationGuard>
-                                      <AppVersionGuard>
-                                        <Tab />
-                                      </AppVersionGuard>
-                                    </NamiMigrationGuard>
-                                  </AddressesDiscoveryOverlay>
-                                </DataCheckContainer>
-                              </MigrationContainer>
-                            </ExternalLinkOpenerProvider>
-                          </UIThemeProvider>
-                        </ThemeProvider>
-                      </AnalyticsProvider>
-                    </PostHogClientProvider>
-                  </BackgroundPageProvider>
-                </HashRouter>
-              </CurrencyStoreProvider>
-            </StoreProvider>
-          </DatabaseProvider>
-        </AppSettingsProvider>
-      </BackgroundServiceAPIProvider>
-    </ErrorBoundary>
-  );
-};
-
 const mountNode = document.querySelector('#lace-app');
-ReactDOM.render(
-  <BlockchainProvider>
-    <App />
-  </BlockchainProvider>,
-  mountNode
-);
+ReactDOM.render(<Tab />, mountNode);
