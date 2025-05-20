c96f363347ae28daaf9075bcd0389e2fe925cbc5
wklos-iohk <wojciech.klos@iohk.io>
2025-05-20 11:40:50 +0200

    test(extension): add missing types to certain POMs

--- packages/e2e-tests/src/elements/glossaryPage.ts
+++ packages/e2e-tests/src/elements/glossaryPage.ts
@@ -1,12 +1,15 @@
+/* global WebdriverIO */
+import type { ChainablePromiseElement } from 'webdriverio';
+
 class GlossaryPage {
   private ACTIVE_ARTICLE = 'div[data-active="true"]';
   private ACTIVE_ARTICLE_TITLE = `${this.ACTIVE_ARTICLE} button`;
 
-  get activeArticle() {
+  get activeArticle(): ChainablePromiseElement<WebdriverIO.Element> {
     return $(this.ACTIVE_ARTICLE);
   }
 
-  get activeArticleTitle() {
+  get activeArticleTitle(): ChainablePromiseElement<WebdriverIO.Element> {
     return $(this.ACTIVE_ARTICLE_TITLE);
   }
 }
