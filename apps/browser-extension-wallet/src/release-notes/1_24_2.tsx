3b2c579b52dc9dc440ac7f9cb5bdb10494273804
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-07-01 21:48:36 +0100

    chore: bump manifest v1.24.2

---
+++ apps/browser-extension-wallet/src/release-notes/1_24_2.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_24_2 = (): React.ReactElement => (
+  <>
+    <p>
+      <strong>Optimized Bitcoin Fee Estimation</strong>
+      <br />
+      discover more competitive rates when sending BTC.
+    </p>
+    <p>
+      <strong>Improved Ledger Message Signing</strong>
+      <br />
+      Large messages are now signed by hash per CIP-8 when using Ledger wallets.
+    </p>
+  </>
+);
+
+export default ReleaseNote_1_24_2;
