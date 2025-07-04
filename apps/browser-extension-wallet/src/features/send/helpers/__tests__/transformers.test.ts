15baa8ef853d79ee48afae232b95696195ecef92
Martynas Kazlauskas <martynas.kazlauskas@iohk.io>
2025-07-04 05:11:56 +0300

    chore: remove unused minimum coin quantity code
    it was probably replaced with something else

--- apps/browser-extension-wallet/src/features/send/helpers/__tests__/transformers.test.ts
+++ apps/browser-extension-wallet/src/features/send/helpers/__tests__/transformers.test.ts
@@ -1,13 +1,8 @@
-import { availableCoinsTransformer, displayedCoinsTransformer } from '../transformers';
-import { mockAvailableBalance, mockDisplayedCoins } from '../../../../utils/mocks/test-helpers';
-import { TxMinimumCoinQuantity } from '../../../../types';
+import { availableCoinsTransformer } from '../transformers';
+import { mockAvailableBalance } from '../../../../utils/mocks/test-helpers';
 import { firstValueFrom } from 'rxjs';
 
 const assetId = '659f2917fb63f12b33667463ee575eeac1845bbc736b9c0bbc40ba8254534c41';
-const minimumCoinQuantity: TxMinimumCoinQuantity = {
-  coinMissing: '2000000',
-  minimumCoin: '2500000'
-};
 
 describe('Testing availableCoinsTransformer function', () => {
   it('should return parsed available coins', async () => {
@@ -25,34 +20,5 @@ describe('Testing availableCoinsTransformer function', () => {
         symbol: '659f2917...534c41'
       }
     ]);
-  });
-});
-
-describe('Testing displayedCoinsTransformer function', () => {
-  it('should return only coins field if no assets are displayed', () => {
-    const result = displayedCoinsTransformer(mockDisplayedCoins);
-    expect(result.coins.toString()).toBe('0');
-  });
-
-  it('should return coins and assets fields when both are displayed', () => {
-    const result = displayedCoinsTransformer([...mockDisplayedCoins, { amount: '5', coinId: assetId }]);
-    expect(result.coins.toString()).toBe('0');
-    expect(result.assets).toEqual(new Map([[assetId, BigInt('5')]]));
-  });
-
-  it(
-    'should return default minimum coin quantity as coins ' +
-      'if displayed coins list only has assets and no minimum is provided',
-    () => {
-      const result = displayedCoinsTransformer([{ amount: '5', coinId: assetId }]);
-      expect(result.coins.toString()).toBe('1000000');
-      expect(result.assets).toEqual(new Map([[assetId, BigInt('5')]]));
-    }
-  );
-
-  it('should return provided minimum coin quantity as coins if displayed coins list only has assets', () => {
-    const result = displayedCoinsTransformer([{ amount: '5', coinId: assetId }], minimumCoinQuantity);
-    expect(result.coins.toString()).toBe('2500000');
-    expect(result.assets).toEqual(new Map([[assetId, BigInt('5')]]));
   });
 });
--- apps/browser-extension-wallet/src/features/send/stores/slices/send-transaction-slice.ts
+++ apps/browser-extension-wallet/src/features/send/stores/slices/send-transaction-slice.ts
@@ -6,10 +6,8 @@ export const sendTransactionSlice: StateCreator<SendTransactionSlice> = (set) =>
   destinationAddress: undefined,
   transactionValue: undefined,
   transaction: undefined,
-  minimumCoinQuantity: { coinMissing: '0', minimumCoin: '0' },
   setDestinationAddress: (address) => set(() => ({ destinationAddress: address })),
   setTransactionValue: (value) => set(() => ({ transactionValue: value })),
   setTransaction: (tx) => set(() => ({ transaction: tx })),
-  setTransactionFeeLovelace: (fee) => set(() => ({ transactionFeeLovelace: fee })),
-  setMinimumCoinQuantity: (coinQty) => set(() => ({ minimumCoinQuantity: coinQty }))
+  setTransactionFeeLovelace: (fee) => set(() => ({ transactionFeeLovelace: fee }))
 });
