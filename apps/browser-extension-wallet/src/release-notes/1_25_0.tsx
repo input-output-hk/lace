b91f378646a8cbc7b8baa8d825c36a03fe7c51b4
Michael <7581002+mchappell@users.noreply.github.com>
2025-07-18 17:57:19 +0100

    chore: bump manifest v1.25.0 (#1954)

---
+++ apps/browser-extension-wallet/src/release-notes/1_25_0.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_25_0 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Coming soon: Print your NFTs with our new @NFTPrintLab partnership</li>
+      <li>
+        Attach metadata to BTC transactions via <pre>OP_RETURN</pre>
+      </li>
+      <li>Improved token display with on-chain metadata support</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_25_0;
