e8462b3d63d1170ecebccacefa7f703a6085a91a
Angel Castillo <angel.castillo@iohk.io>
2025-07-30 16:59:23 -0500

    feat: render metadata field in transaction preview before signing

--- packages/core/src/ui/components/ActivityDetail/components/index.ts
+++ packages/core/src/ui/components/ActivityDetail/components/index.ts
@@ -1,4 +1,5 @@
 export * from './TxDetailsCertificates/TxDetailsCertificates';
 export * from './TxDetailsCBOR/TxDetailsCBOR';
+export * from './TxDetailsAuxiliaryData/TxDetailsAuxiliaryData';
 export * from './TxDetailsProposalProcedures';
 export * from './TxDetailsVotingProcedures';
