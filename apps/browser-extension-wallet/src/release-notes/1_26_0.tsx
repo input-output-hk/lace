c00e599d0da8a147cb811b3d1dff82af432f85df
Michael <7581002+mchappell@users.noreply.github.com>
2025-08-01 16:43:05 +0100

    chore: bump manifest v1.26.0 (#1963)

---
+++ apps/browser-extension-wallet/src/release-notes/1_26_0.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_26_0 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Metadata is now displayed in DApp Connector</li>
+      <li>Improved NFTPrintLab integration</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_26_0;
