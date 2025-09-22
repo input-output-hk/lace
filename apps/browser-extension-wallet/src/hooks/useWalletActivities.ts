8b68b9ca59e8dddfb65a756f719d093baabfc9a0
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-09-22 15:03:16 +0100

    fix: token detail drawer when no token pricing available

--- apps/browser-extension-wallet/src/hooks/useWalletActivities.ts
+++ apps/browser-extension-wallet/src/hooks/useWalletActivities.ts
@@ -29,7 +29,6 @@ export const useWalletActivities = ({
 
   const fetchWalletActivities = useCallback(async () => {
     fiatCurrency &&
-      cardanoFiatPrice &&
       getWalletActivities({
         fiatCurrency,
         cardanoFiatPrice,
