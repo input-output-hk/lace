9399b278f8f1209b2b877f147474b7b4e09d829b
vetalcore <vetalcore@gmail.com>
2025-09-22 14:30:24 +0300

    fix: btc wallet display if pricing data not available

--- apps/browser-extension-wallet/src/views/bitcoin-mode/features/activity/components/ActivityLayout.tsx
+++ apps/browser-extension-wallet/src/views/bitcoin-mode/features/activity/components/ActivityLayout.tsx
@@ -121,14 +121,17 @@ export const ActivityLayout = (): React.ReactElement => {
 
     const walletAddress = addresses[0].address;
 
-    const groups = [...recentTransactions, ...pendingTransaction].reduce((acc, transaction) => {
-      const dateKey = transaction.timestamp === 0 ? 'Pending' : formattedDate(new Date(transaction.timestamp * 1000));
-      if (!acc[dateKey]) {
-        acc[dateKey] = [];
-      }
-      acc[dateKey].push(transaction);
-      return acc;
-    }, {} as { [date: string]: Bitcoin.TransactionHistoryEntry[] });
+    const groups = [...recentTransactions, ...pendingTransaction].reduce(
+      (acc, transaction) => {
+        const dateKey = transaction.timestamp === 0 ? 'Pending' : formattedDate(new Date(transaction.timestamp * 1000));
+        if (!acc[dateKey]) {
+          acc[dateKey] = [];
+        }
+        acc[dateKey].push(transaction);
+        return acc;
+      },
+      {} as { [date: string]: Bitcoin.TransactionHistoryEntry[] }
+    );
 
     const sortedDates = Object.keys(groups).sort((a, b) => {
       if (a === 'Pending') return -1;
@@ -169,9 +172,9 @@ export const ActivityLayout = (): React.ReactElement => {
               ? 'PENDING'
               : formattedTimestamp(new Date(transaction.timestamp * 1000)),
           amount: `${new BigNumber(net.toString()).dividedBy(100_000_000).toFixed(8, BigNumber.ROUND_HALF_UP)} BTC`,
-          fiatAmount: `${computeBalance(Number(net) / SATS_IN_BTC, fiatCurrency.code, bitcoinPrice)} ${
-            fiatCurrency.code === 'ADA' ? 'BTC' : fiatCurrency.code
-          }`,
+          fiatAmount: `${
+            bitcoinPrice ? computeBalance(Number(net) / SATS_IN_BTC, fiatCurrency.code, bitcoinPrice) : '-'
+          } ${fiatCurrency.code === 'ADA' ? 'BTC' : fiatCurrency.code}`,
           status:
             transaction.status === Bitcoin.TransactionStatus.Pending ? ActivityStatus.PENDING : ActivityStatus.SUCCESS,
           type,
