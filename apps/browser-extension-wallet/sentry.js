34287df4b470a665f34257ed27871f007ada81f3
Piotr Czeglik <piotr.czeglik@iohk.io>
2025-09-29 10:18:48 +0200

    docs: use sentry logging in development environment

--- apps/browser-extension-wallet/sentry.js
+++ apps/browser-extension-wallet/sentry.js
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/react';
 import { v4 as uuidv4 } from 'uuid';
 import { storage } from 'webextension-polyfill';
+
 Sentry.init({
   environment: process.env.SENTRY_ENVIRONMENT,
   dsn: process.env.SENTRY_DSN,
