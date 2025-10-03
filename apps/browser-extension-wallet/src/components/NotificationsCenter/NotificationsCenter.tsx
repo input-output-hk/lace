7e97a0dd2c6da44d89d26454152c24cd34e3ef57
vetalcore <vetalcore@gmail.com>
2025-10-03 16:01:53 +0300

    feat: implement notification item component (#2007)

--- apps/browser-extension-wallet/src/components/NotificationsCenter/NotificationsCenter.tsx
+++ apps/browser-extension-wallet/src/components/NotificationsCenter/NotificationsCenter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Box, Flex } from '@input-output-hk/lace-ui-toolkit';
 
@@ -6,6 +6,7 @@ import { Button, NavigationButton } from '@lace/common';
 import { SectionTitle } from '@components/Layout/SectionTitle';
 
 import styles from './NotificationsCenter.module.scss';
+import { WarningModal } from '@src/views/browser-view/components/WarningModal/WarningModal';
 
 export interface NotificationsCenterProps {
   onBack: () => void;
@@ -19,34 +20,54 @@ export const NotificationsCenter = ({
   popupView
 }: NotificationsCenterProps): React.ReactElement => {
   const { t } = useTranslation();
+  const [isRemoveNotificationModalVisible, setIsRemoveNotificationModalVisible] = useState(false);
+
+  const onRemoveNotification = () => {
+    // TODO: implement remove notification
+    // eslint-disable-next-line no-console
+    console.log('remove notification');
+    setIsRemoveNotificationModalVisible(false);
+  };
 
   return (
-    <Box p="$24">
-      <Flex justifyContent="space-between" mb={'$44'}>
-        <Box mb={'$0'}>
-          <SectionTitle
-            sideText={`(${1})`}
-            title={
-              <Flex alignItems="center" gap="$8">
-                <NavigationButton icon="arrow" onClick={onBack} />
-                {t('notificationsCenter.title')}
-              </Flex>
-            }
-          />
-        </Box>
-        {!popupView && (
-          <Button
-            className={styles.button}
-            block
-            color="gradient"
-            data-testid="notifications-bell"
-            onClick={onMarkAllAsRead}
-          >
-            {t('notificationsCenter.markAllAsRead')}
-          </Button>
-        )}
-      </Flex>
-      Notifications Center (Placeholder content)
-    </Box>
+    <>
+      <WarningModal
+        visible={isRemoveNotificationModalVisible}
+        header={t('notificationsCenter.removeNotification')}
+        content={t('notificationsCenter.removeNotification.description')}
+        onCancel={() => setIsRemoveNotificationModalVisible(false)}
+        cancelLabel={t('notificationsCenter.removeNotification.cancel')}
+        confirmLabel={t('notificationsCenter.removeNotification.confirm')}
+        onConfirm={onRemoveNotification}
+        isPopupView={popupView}
+      />
+      <Box p="$24">
+        <Flex justifyContent="space-between" mb={'$44'}>
+          <Box mb={'$0'}>
+            <SectionTitle
+              sideText={`(${1})`}
+              title={
+                <Flex alignItems="center" gap="$8">
+                  <NavigationButton icon="arrow" onClick={onBack} />
+                  {t('notificationsCenter.title')}
+                </Flex>
+              }
+            />
+          </Box>
+          {!popupView && (
+            <Button
+              className={styles.button}
+              block
+              color="gradient"
+              data-testid="notifications-bell"
+              onClick={onMarkAllAsRead}
+            >
+              {t('notificationsCenter.markAllAsRead')}
+            </Button>
+          )}
+        </Flex>
+        Notifications Center (Placeholder content)
+      </Box>
+    </>
   );
 };
