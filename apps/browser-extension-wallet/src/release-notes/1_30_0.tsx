860b680dc55492ef3d7d76d714048fc1ee0b5083
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-10-07 16:11:17 +0200

    chore: bump manifest v1.30.0

---
+++ apps/browser-extension-wallet/src/release-notes/1_30_0.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_30_0 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Spanish language support</li>
+      <li>You can switch your language in Settings → Preferences</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_30_0;
