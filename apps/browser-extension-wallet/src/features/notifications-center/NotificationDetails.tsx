3751d7b1460c033f72b2a12fb591d74e5e7c1e4b
vetalcore <vetalcore@gmail.com>
2025-10-08 17:05:53 +0300

    feat: implement notification details component

--- apps/browser-extension-wallet/src/features/notifications-center/NotificationDetails.tsx
+++ apps/browser-extension-wallet/src/features/notifications-center/NotificationDetails.tsx
@@ -1,38 +1,56 @@
-/* eslint-disable unicorn/no-useless-undefined */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types, promise/catch-or-return, sonarjs/cognitive-complexity, no-magic-numbers, unicorn/no-null */
 import React from 'react';
-import { ContentLayout } from '@src/components/Layout';
-import { useTranslation } from 'react-i18next';
-import { useNotificationsCenter } from '@hooks/useNotificationsCenter';
-import { Flex } from '@input-output-hk/lace-ui-toolkit';
-import { NavigationButton } from '@lace/common';
-import { useHistory } from 'react-router';
-import { SectionTitle } from '@components/Layout/SectionTitle';
 
-export const NotificationDetails = (): React.ReactElement => {
+import { useTranslation } from 'react-i18next';
+
+import { Flex, Button as LaceButton, Text, Box, Divider } from '@input-output-hk/lace-ui-toolkit';
+import TrashOutlineComponent from '../../assets/icons/browser-view/trash-icon.component.svg';
+import { LaceNotification } from '@src/types/notifications-center';
+import styles from './NotificationDetails.module.scss';
+
+export interface NotificationDetailsProps {
+  notification: LaceNotification;
+  onRemoveNotification?: () => void;
+  popupView?: boolean;
+}
+
+export const NotificationDetails = ({
+  notification,
+  onRemoveNotification,
+  popupView
+}: NotificationDetailsProps): React.ReactElement => {
   const { t } = useTranslation();
-  const { unreadNotifications } = useNotificationsCenter();
-  const history = useHistory();
+
+  const PublisherTextComponent = popupView ? Text.Body.Normal : Text.Body.Small;
+  const BodyTextComponent = popupView ? Text.Label : Text.Body.Large;
 
   return (
-    <ContentLayout
-      title={
-        <SectionTitle
-          isPopup
-          title={
-            <Flex alignItems="center" gap="$8">
-              <NavigationButton icon="arrow" onClick={() => history.goBack()} />
-              {t('notificationsCenter.title')}
-            </Flex>
-          }
-          sideText={`(${unreadNotifications})`}
-          data-testid="notifications-details-title"
-        />
-      }
-    >
-      <div>
-        <h1>Notification Details</h1>
-      </div>
-    </ContentLayout>
+    <Flex pt="$20" flexDirection="column" w="$fill">
+      <Flex px={popupView ? '$20' : '$0'} flexDirection="column" w="$fill">
+        <Text.Heading weight="$bold" color="secondary">
+          {notification.message.title}
+        </Text.Heading>
+        <Box mt={popupView ? '$8' : '$18'}>
+          <PublisherTextComponent weight="$semibold">{notification.message.publisher}</PublisherTextComponent>
+        </Box>
+        <Divider w="$fill" mt={popupView ? '$16' : '$18'} mb={popupView ? '$16' : '$32'} />
+        {notification.message.format === 'plain' ? (
+          <BodyTextComponent weight="$semibold">{notification.message.body}</BodyTextComponent>
+        ) : (
+          <pre>{JSON.stringify(notification.message.body)}</pre>
+        )}
+      </Flex>
+      {typeof onRemoveNotification === 'function' && (
+        <Flex mt="$32" className={styles.actions} w="$fill">
+          <LaceButton.Secondary
+            w="$fill"
+            size="medium"
+            color="secondary"
+            label={t('notificationsCenter.removeNotification.confirm')}
+            icon={<TrashOutlineComponent className={styles.icon} />}
+            onClick={onRemoveNotification}
+          />
+        </Flex>
+      )}
+    </Flex>
   );
 };
---
+++ apps/browser-extension-wallet/src/features/notifications-center/NotificationDetailsContainer.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable unicorn/no-useless-undefined */
+import React, { useState, useEffect } from 'react';
+import { WarningModal } from '@src/views/browser-view/components';
+import { useTranslation } from 'react-i18next';
+import { useHistory, useParams } from 'react-router';
+import { useNotificationsCenter } from '@hooks/useNotificationsCenter';
+import { walletRoutePaths } from '@routes';
+import { NotificationDetails } from './NotificationDetails';
+import { Flex } from '@input-output-hk/lace-ui-toolkit';
+import { Button, NavigationButton } from '@lace/common';
+import { ContentLayout } from '@components/Layout';
+
+export const NotificationDetailsContainer = (): React.ReactElement => {
+  const { t } = useTranslation();
+  const history = useHistory();
+  const { notifications, remove } = useNotificationsCenter();
+  const [notificationIdToRemove, setNotificationIdToRemove] = useState<string | undefined>();
+  const { id: notificationId } = useParams<{ id: string }>();
+  const notification = notifications?.find(({ message }) => message.id === notificationId);
+
+  const onBack = () => {
+    history.goBack();
+  };
+
+  const onViewAllNotification = () => {
+    history.push(walletRoutePaths.notifications);
+  };
+
+  useEffect(() => {
+    if (!notification && notifications?.length > 0) {
+      history.push(walletRoutePaths.notifications);
+    }
+  }, [notification, history, notifications?.length]);
+
+  if (!notification) {
+    return <></>;
+  }
+
+  return (
+    <>
+      <WarningModal
+        visible={!!notificationIdToRemove}
+        header={t('notificationsCenter.removeNotification')}
+        content={t('notificationsCenter.removeNotification.description')}
+        onCancel={() => setNotificationIdToRemove(undefined)}
+        cancelLabel={t('notificationsCenter.removeNotification.cancel')}
+        confirmLabel={t('notificationsCenter.removeNotification.confirm')}
+        onConfirm={() => {
+          remove(notificationIdToRemove);
+          setNotificationIdToRemove(undefined);
+        }}
+      />
+      <ContentLayout>
+        <Flex justifyContent="space-between" alignItems="center" gap="$8">
+          <NavigationButton icon="arrow" onClick={onBack} />
+          <Button color="gradient" data-testid="view-all-button" onClick={onViewAllNotification}>
+            {t('notificationsCenter.viewAll')}
+          </Button>
+        </Flex>
+        <NotificationDetails
+          notification={notification}
+          onRemoveNotification={() => setNotificationIdToRemove(notificationId)}
+          popupView
+        />
+      </ContentLayout>
+    </>
+  );
+};
