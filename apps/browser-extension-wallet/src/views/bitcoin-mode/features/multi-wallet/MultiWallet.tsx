a23ea66e66507602e25542f30115488f544721ae
John Oshalusi <john.oshalusi@iohk.io>
2025-10-22 10:50:58 +0100

    feat: restyle multi-wallet onboarding flow (#2020)

--- apps/browser-extension-wallet/src/views/bitcoin-mode/features/multi-wallet/MultiWallet.tsx
+++ apps/browser-extension-wallet/src/views/bitcoin-mode/features/multi-wallet/MultiWallet.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable unicorn/no-null */
 import { NavigationButton } from '@lace/common';
-import cn from 'classnames';
 import { WalletSetupConfirmationDialogProvider, WalletSetupFlow, WalletSetupFlowProvider } from '@lace/core';
 import { useBackgroundPage } from '@providers/BackgroundPageProvider';
 import { walletRoutePaths } from '@routes';
-import { Modal } from 'antd';
 import React, { useCallback } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import styles from './MultiWallet.module.scss';
 import { WalletOnboardingFlows } from './WalletOnboardingFlows';
 import { postHogMultiWalletActions } from '@providers/AnalyticsProvider/analyticsTracker';
 import { Home } from './Home';
 import { usePostHogClientContext } from '@providers/PostHogClientProvider';
+import { WalletSetupLayout } from '@views/browser/components';
+import { Portal } from '@views/browser/features/wallet-setup/components/Portal';
 
 export const MultiWallet = (): JSX.Element => {
   const history = useHistory();
-  const location = useLocation();
   const posthogClient = usePostHogClientContext();
   const { page, setBackgroundPage } = useBackgroundPage();
 
@@ -30,33 +29,27 @@ export const MultiWallet = (): JSX.Element => {
     [history, page, setBackgroundPage]
   );
 
-  const isBitcoinModeRootPath = location.pathname === walletRoutePaths.newBitcoinWallet.root;
-
   return (
     <WalletSetupFlowProvider flow={WalletSetupFlow.ADD_WALLET}>
       <WalletSetupConfirmationDialogProvider>
-        {({ isDialogOpen, withConfirmationDialog, shouldShowDialog$ }) => (
-          <Modal
-            centered
-            closable
-            footer={null}
-            open={!isDialogOpen}
-            width="100%"
-            className={cn(styles.modal, { [styles.flex]: isBitcoinModeRootPath })}
-            onCancel={() => handleOnCancel(withConfirmationDialog)}
-          >
-            <div className={styles.closeButton}>
-              <NavigationButton icon="cross" onClick={() => handleOnCancel(withConfirmationDialog)} />
-            </div>
-            <WalletOnboardingFlows
-              mergeEventRequired
-              postHogActions={postHogMultiWalletActions}
-              renderHome={() => <Home />}
-              setFormDirty={(dirty) => shouldShowDialog$.next(dirty)}
-              urlPath={walletRoutePaths.newBitcoinWallet}
-              flowsEnabled={!!posthogClient.featureFlagsByNetwork}
-            />
-          </Modal>
+        {({ withConfirmationDialog, shouldShowDialog$ }) => (
+          <Portal>
+            <WalletSetupLayout>
+              <div className={styles.contentWrapper}>
+                <div className={styles.closeButton}>
+                  <NavigationButton icon="cross" onClick={() => handleOnCancel(withConfirmationDialog)} />
+                </div>
+                <WalletOnboardingFlows
+                  mergeEventRequired
+                  postHogActions={postHogMultiWalletActions}
+                  renderHome={() => <Home />}
+                  setFormDirty={(dirty) => shouldShowDialog$.next(dirty)}
+                  urlPath={walletRoutePaths.newBitcoinWallet}
+                  flowsEnabled={!!posthogClient.featureFlagsByNetwork}
+                />
+              </div>
+            </WalletSetupLayout>
+          </Portal>
         )}
       </WalletSetupConfirmationDialogProvider>
     </WalletSetupFlowProvider>
