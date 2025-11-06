1d4ca41276c2851534b3c1044d78911a95e3e302
Rhys Bartels-Waller <rhys.bartelswaller@iohk.io>
2025-11-06 19:42:35 +0000

    fix(extension): filter undefined values in getSwapQuoteSources
    Add filtering to remove undefined values that could result from
    missing pools in split groups. This prevents 'undefined' from
    appearing in the quote sources string.

--- apps/browser-extension-wallet/src/views/browser-view/features/swaps/util.ts
+++ apps/browser-extension-wallet/src/views/browser-view/features/swaps/util.ts
@@ -1,4 +1,7 @@
 import { SplitGroup } from './types';
 
 export const getSwapQuoteSources = (splitGroup: SplitGroup[]): string =>
-  splitGroup.flatMap((groups) => groups.flatMap((group) => group.pools?.map((pool) => pool.dex))).join(', ');
+  splitGroup
+    .flatMap((groups) => groups.flatMap((group) => group.pools?.map((pool) => pool.dex) ?? []))
+    .filter((dex): dex is string => dex !== undefined)
+    .join(', ');
