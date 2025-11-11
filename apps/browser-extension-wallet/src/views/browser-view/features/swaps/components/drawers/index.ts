a29ce83a55532f5154799c1e6932f38feb7b2dd2
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-11-11 17:50:18 +0000

    feat(extension): remove redundant swap success drawer

--- apps/browser-extension-wallet/src/views/browser-view/features/swaps/components/drawers/index.ts
+++ apps/browser-extension-wallet/src/views/browser-view/features/swaps/components/drawers/index.ts
@@ -3,4 +3,3 @@ export * from './LiquiditySourcesDrawer';
 export * from './SlippageDrawer';
 export * from './SwapReview';
 export * from './TokenSelectDrawer';
-export * from './SwapSuccessDrawer';
