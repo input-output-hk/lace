7151c205fed36a59cf2bc918e2b608bc96884069
Michael <7581002+mchappell@users.noreply.github.com>
2025-11-13 09:10:02 +0000

    chore: bump manifest v1.31.0 (#2045)

---
+++ apps/browser-extension-wallet/src/release-notes/1_31_0.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_31_0 = (): React.ReactElement => (
+  <>
+    <p>
+      <strong>New Features</strong>
+    </p>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>
+        <strong>Token Swaps</strong>
+      </li>
+      <li>Swap Cardano native tokens directly in-app using SteelSwap. No more platform hopping.</li>
+      <li>
+        <strong>Bitcoin general availability</strong>
+      </li>
+      <li>Bitcoin wallets are now out of beta.</li>
+    </ul>
+    <p>
+      <strong>Improvements</strong>
+    </p>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>API Request Optimizations</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_31_0;
