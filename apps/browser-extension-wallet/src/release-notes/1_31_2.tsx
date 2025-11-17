53c541304fac93f3c8dafeed4b0acc5d3f2660aa
Michael Chappell <7581002+mchappell@users.noreply.github.com>
2025-11-17 19:51:14 +0000

    chore(extension): bump manifest v1.31.2

---
+++ apps/browser-extension-wallet/src/release-notes/1_31_2.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_31_2 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Improved swap analytics</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_31_2;
