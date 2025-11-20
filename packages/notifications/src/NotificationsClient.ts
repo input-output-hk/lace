a54c1c07747db156dc79999d5f4b125703be3c73
Daniele Ricci <daniele.ricci@iohk.io>
2025-11-20 10:45:56 +0100

    fix: save the topics on init as well

--- packages/notifications/src/NotificationsClient.ts
+++ packages/notifications/src/NotificationsClient.ts
@@ -252,6 +252,8 @@ export class NotificationsClient {
       }
     }
 
+    await this.updateTopics();
+
     this.isInitialized = true;
     this.unsubscribedTopics = [...subscribedTopics, ...unsubscribedTopics];
 
