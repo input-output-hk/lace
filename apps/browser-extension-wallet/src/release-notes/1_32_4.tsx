91e527d5be35749302e8061d4fd9474bcbe30bcc
mirceahasegan <mircea.hasegan@iohk.io>
2025-12-10 17:28:45 +0100

    chore: manifest version bump for 1.32.4 release (#2106) (#2107)

---
+++ apps/browser-extension-wallet/src/release-notes/1_32_4.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_32_4 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Improve token price updates</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_32_4;
