c804f0891e04eaa8bc6f60163820716e54ac00ef
Mircea Hasegan <mircea.hasegan@iohk.io>
2025-12-17 19:09:29 +0200

    chore: manifest version bump for 1.32.5 release

---
+++ apps/browser-extension-wallet/src/release-notes/1_32_5.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_32_5 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Performance improvements</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_32_5;
