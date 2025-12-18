5749a7661a2e034cb9c6b1c385e47426a1da4770
mirceahasegan <mircea.hasegan@iohk.io>
2025-12-18 12:21:42 +0100

    chore: cherry-pick release 1.32.x to main (#2120)
    * fix(extension): stop setting subscribed_topics properties [LW-14095]
    
    * chore: manifest version bump for 1.32.5 release
    
    * chore: release notes 1.32.5 update

---
+++ v1/apps/browser-extension-wallet/src/release-notes/1_32_5.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+const ReleaseNote_1_32_5 = (): React.ReactElement => (
+  <>
+    <ul style={{ listStyleType: 'disc', margin: 0 }}>
+      <li>Reduce network traffic related to notifications feature</li>
+    </ul>
+  </>
+);
+
+export default ReleaseNote_1_32_5;
