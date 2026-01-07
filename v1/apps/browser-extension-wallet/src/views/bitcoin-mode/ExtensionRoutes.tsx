48ae41059dbd7d5f13a4b07f1474b4ed2866e85a
mirceahasegan <mircea.hasegan@iohk.io>
2026-01-07 10:28:41 +0100

    fix(extension): show notification center in bitcoin wallet (#2118)

--- v1/apps/browser-extension-wallet/src/views/bitcoin-mode/ExtensionRoutes.tsx
+++ v1/apps/browser-extension-wallet/src/views/bitcoin-mode/ExtensionRoutes.tsx
@@ -7,16 +7,28 @@ import { PopupAssets } from './features/assets';
 import { SendContainer } from './features/send/components';
 import { walletRoutePaths } from './wallet-paths';
 import { Settings } from '../../features/settings';
+import { useNotificationsCenterConfig } from '../../hooks/useNotificationsCenterConfig';
+import { NotificationDetailsContainer, NotificationsCenter } from '../../features/notifications-center';
 
-export const ExtensionRoutes = (): React.ReactElement => (
-  <MainLayout>
-    <Switch>
-      <Route exact path={walletRoutePaths.assets} component={PopupAssets} />
-      <Route exact path={walletRoutePaths.receive} component={ReceiveInfoContainer} />
-      <Route exact path={walletRoutePaths.activity} component={Activity} />
-      <Route exact path={walletRoutePaths.send} component={SendContainer} />
-      <Route exact path={walletRoutePaths.settings} component={Settings} />
-      <Route path="*" render={() => <Redirect to={walletRoutePaths.assets} />} />
-    </Switch>
-  </MainLayout>
-);
+export const ExtensionRoutes = (): React.ReactElement => {
+  const { isNotificationsCenterEnabled } = useNotificationsCenterConfig();
+
+  return (
+    <MainLayout>
+      <Switch>
+        <Route exact path={walletRoutePaths.assets} component={PopupAssets} />
+        <Route exact path={walletRoutePaths.receive} component={ReceiveInfoContainer} />
+        <Route exact path={walletRoutePaths.activity} component={Activity} />
+        <Route exact path={walletRoutePaths.send} component={SendContainer} />
+        {isNotificationsCenterEnabled && (
+          <Route exact path={walletRoutePaths.notifications} component={NotificationsCenter} />
+        )}
+        {isNotificationsCenterEnabled && (
+          <Route exact path={walletRoutePaths.notification} component={NotificationDetailsContainer} />
+        )}
+        <Route exact path={walletRoutePaths.settings} component={Settings} />
+        <Route path="*" render={() => <Redirect to={walletRoutePaths.assets} />} />
+      </Switch>
+    </MainLayout>
+  );
+};
