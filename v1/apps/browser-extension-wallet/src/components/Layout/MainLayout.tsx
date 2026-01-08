20a701d0754fbcad2c246049f28b5ce473ca3ffe
Piotr Czeglik <piotr.czeglik@iohk.io>
2026-01-08 10:38:00 +0100

    feat: remove release notes announcement

--- v1/apps/browser-extension-wallet/src/components/Layout/MainLayout.tsx
+++ v1/apps/browser-extension-wallet/src/components/Layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import debounce from 'lodash/debounce';
 import { useTranslation } from 'react-i18next';
 import { toast } from '@lace/common';
@@ -6,16 +6,12 @@ import { MainFooter, MainHeader } from '../MainMenu';
 import styles from './MainLayout.module.scss';
 import { SimpleHeader } from '../MainMenu/SimpleHeader';
 import { useNetworkError } from '@hooks/useNetworkError';
-import { Announcement } from '@components/Announcement/Announcement';
-import { storage } from 'webextension-polyfill';
-import { ABOUT_EXTENSION_KEY, ExtensionUpdateData } from '@lib/scripts/types';
 import { PrivacyPolicyUpdate } from '../PrivacyPolicyUpdate/PrivacyPolicyUpdate';
 
 interface MainLayoutProps {
   children: React.ReactNode;
   useSimpleHeader?: boolean;
   hideFooter?: boolean;
-  showAnnouncement?: boolean;
   showBetaPill?: boolean;
 }
 
@@ -31,12 +27,9 @@ export const MainLayout = ({
   children,
   useSimpleHeader = false,
   hideFooter,
-  showAnnouncement = false,
   showBetaPill = false
 }: MainLayoutProps): React.ReactElement => {
   const { t } = useTranslation();
-  const [aboutExtension, setAboutExtension] = useState({} as ExtensionUpdateData);
-  const { version, acknowledged, reason } = aboutExtension;
 
   const debouncedToast = useMemo(() => debounce(toast.notify, toastThrottle), []);
   const showNetworkError = useCallback(
@@ -45,23 +38,6 @@ export const MainLayout = ({
   );
 
   useNetworkError(showNetworkError);
-
-  const getAboutExtensionData = useCallback(async () => {
-    const data = await storage.local.get(ABOUT_EXTENSION_KEY);
-    setAboutExtension(data?.[ABOUT_EXTENSION_KEY] || {});
-  }, []);
-
-  useEffect(() => {
-    getAboutExtensionData();
-  }, [getAboutExtensionData]);
-
-  const onUpdateAcknowledge = useCallback(async () => {
-    const data = { version, acknowledged: true, reason };
-    await storage.local.set({
-      [ABOUT_EXTENSION_KEY]: data
-    });
-    setAboutExtension(data);
-  }, [reason, version]);
 
   return (
     <div className={styles.layoutContainer}>
@@ -73,12 +49,6 @@ export const MainLayout = ({
           </div>
         </div>
       </div>
-      <Announcement
-        visible={showAnnouncement && version && !acknowledged}
-        onConfirm={onUpdateAcknowledge}
-        version={version}
-        reason={reason}
-      />
       <PrivacyPolicyUpdate />
       {!hideFooter && <MainFooter />}
     </div>
