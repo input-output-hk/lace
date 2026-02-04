9a470481eea0bd4a8243430d3730d8772036cfaa
Mircea Hasegan <mircea.hasegan@iohk.io>
2026-02-04 18:19:05 +0100

    refactor: remove unused notifications lib

--- v1/packages/notifications/src/providers/PubNub/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './PubNubProvider';
-export * from './PubNubPollingProvider';
-export * from './types';
--- v1/packages/notifications/src/providers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './providers';
-export * from './types';
-export * from './PubNub';
