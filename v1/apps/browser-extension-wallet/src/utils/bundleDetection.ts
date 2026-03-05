0a4d2123bb63d82dae05261f539ecc7d9a2ebe4f
John Oshalusi <john.oshalusi@iohk.io>
2026-03-05 10:01:22 +0100

    feat: improve bundle type detection (#2183)

--- v1/apps/browser-extension-wallet/src/utils/bundleDetection.ts
+++ v1/apps/browser-extension-wallet/src/utils/bundleDetection.ts
@@ -1,5 +1,1 @@
-import { runtime } from 'webextension-polyfill';
-
-export const isV2Bundle = (): boolean =>
-  // eslint-disable-next-line camelcase
-  (runtime.getManifest() as { bundle_type?: string }).bundle_type === 'v2';
+export const isV2Bundle = (): boolean => process.env.BUNDLE_TYPE === 'v2';
