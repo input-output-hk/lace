914663da2de573ccffcc79804d7f795c08c8db6e
Dmytro Iakymenko <dmytro.iakymenko@iohk.io>
2026-04-02 16:20:38 -0300

    ci: lace bundle (v1+v2+lmp)

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ const MergeManifestPlugin = require("./merge-manifest-plugin");
 const target = process.env.BUILD_TARGET || "lmp";
 
 const TARGET_PATHS = {
-  lmp: "v2/apps/midnight-extension",
-  v2: "v2/apps/lace-extension",
+  lmp: ["v2/apps/midnight-extension"],
+  v2: ["v2/apps/lace-extension"],
+  "v2-lmp": ["v2/apps/lace-extension", "v2/apps/midnight-extension"],
 };
 
 if (!TARGET_PATHS[target]) {
@@ -15,8 +16,6 @@ if (!TARGET_PATHS[target]) {
     `Invalid BUILD_TARGET "${target}". Must be one of: ${Object.keys(TARGET_PATHS).join(", ")}`
   );
 }
-
-const targetPath = TARGET_PATHS[target];
 
 module.exports = {
   mode: "development",
@@ -41,15 +40,15 @@ module.exports = {
           from: path.join(__dirname, "assets"),
           to: path.join(__dirname, "dist"),
         },
-        // Copy entire target (LMP or v2) dist
+        // Copy entire target (LMP, V2 or both) dist
         // Luckily, there are no conflicting filenames so we can copy both to root dist/
-        {
+        ...TARGET_PATHS[target].map((targetPath) => ({
           from: path.join(__dirname, targetPath, "dist"),
           to: path.join(__dirname, "dist"),
           globOptions: {
             ignore: ["**/manifest.json", "**/popup.html"],
           },
-        },
+        })),
         // Copy entire V1 dist
         {
           from: path.join(
